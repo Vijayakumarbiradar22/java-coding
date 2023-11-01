2f1ca2eb039aa47aa73251644aabdf07c70fc932
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-01 21:22:03 +0530

    recursion for increasenum

---
+++ Recursion/increasenum/numin.java
@@ -0,0 +1,18 @@
+package Recursion.increasenum;
+
+public class numin {
+    public static void printNum(int n){
+        if(n==6){
+            System.out.println(n);
+            return;
+        }
+
+        System.out.println(n);
+        printNum(n+1);
+    }
+    public static void main(String args[]){
+        int n=1;
+        printNum(n);
+    }
+    
+}
