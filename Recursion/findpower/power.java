9fa755f8f3467625bd3da7591db09ee764bcbf2d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-02 21:10:34 +0530

    completed code

---
+++ Recursion/findpower/power.java
@@ -0,0 +1,18 @@
+package Recursion.findpower;
+
+public class power {
+    public static int findPower(int x, int n){
+
+        int powm1 = findPower(x,n-1);
+        int pow = n*powm1;
+        return pow;
+    }
+    public static void main(String args){
+        int x=2;
+        int n=5;
+        int result = findPower(x,n);
+
+        System.out.println(result);
+    }
+    
+}
