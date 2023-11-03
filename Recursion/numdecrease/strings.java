b884a01d4fd4adbe4ba3135c62c2383a7f2a21a2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-03 21:55:48 +0530

    modified code

--- Recursion/numdecrease/strings.java
+++ Recursion/numdecrease/strings.java
@@ -11,10 +11,11 @@ public class strings {
         printNum(n-1);
     }
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of n:");
-        int n = sc.nextInt();
-        printNum(n);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the value of n:");
+            int n = sc.nextInt();
+            printNum(n);
+        }
     }
     
 }
