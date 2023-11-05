5b1010caabe52484b3c7720f0345cc59f3d39591
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-05 18:04:04 +0530

    first and last occur modified

---
+++ Recursion/eleoccurance/occur.java
@@ -0,0 +1,29 @@
+package Recursion.eleoccurance;
+
+public class occur {
+    public static int first=-1;
+    public static int last=-1;
+    public static void occurfl(String str,int indx,char element){
+        if(indx==str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+
+        char currChar=str.charAt(indx);
+        if(currChar==element){
+            if(first==-1){
+                first=indx;
+            } else {
+                last = indx;
+            }
+        }
+        occurfl(str, indx+1, element);
+    }
+    public static void main(String args[]){
+        String str = "abcaadefaaaf";
+        occurfl(str, 0, 'a');
+
+    }
+    
+}
