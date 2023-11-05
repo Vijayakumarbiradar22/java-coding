ec34557f08cff55cedaf6b2e5bc5ff5c175f0835
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-05 17:53:21 +0530

    modified code

---
+++ Recursion/uniquesubsequence/usub.java
@@ -0,0 +1,25 @@
+package Recursion.uniquesubsequence;
+import java.util.HashSet;
+public class usub {
+    public static void usubseq(String str,int indx,String newString,HashSet<String> set){
+        if(indx==str.length()){
+            if(set.contains(newString)){
+               return;
+            } else {
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
+        }
+
+        char currChar = str.charAt(indx);
+        usubseq(str, indx+1, newString+currChar, set);
+        usubseq(str, indx+1, newString, set);
+    }
+    public static void main(String args[]){
+        String str = "aaa";
+        HashSet<String> set = new HashSet<>();
+        usubseq(str, 0, "", set);
+    }
+    
+}
