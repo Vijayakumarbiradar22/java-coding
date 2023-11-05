55a20841dd535f2e4d0d1f3f2bc34c422dd7a2d4
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-05 17:07:42 +0530

    removed duplicate

---
+++ Recursion/removedup/dup.java
@@ -0,0 +1,26 @@
+package Recursion.removedup;
+
+public class dup {
+    public static boolean map[]=new boolean[26];
+    public static void removeDup(String str,int indx,String newString){
+        if(indx==str.length()){
+            System.out.println(newString);
+            return;
+        }
+
+        char currChar=str.charAt(indx);
+        if(map[currChar-'a']==true){
+            removeDup(str, indx+1, newString);
+        } else {
+            newString +=currChar;
+            map[currChar-'a']=true;
+            removeDup(str, indx+1, newString);
+        }
+    }
+    public static void main(String args[]){
+        String str="viiijaay";
+        removeDup(str, 0, "");
+
+    }
+    
+}
