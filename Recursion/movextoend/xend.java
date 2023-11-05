75d148ae534a34dc468dffddc0b112ec41cd494b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-05 17:28:18 +0530

    move x to end  code

---
+++ Recursion/movextoend/xend.java
@@ -0,0 +1,28 @@
+package Recursion.movextoend;
+
+public class xend {
+    public static void moveallx(String str,int indx,int count,String newString){
+        if(indx==str.length()){
+            for(int i=0;i<count;i++){
+                newString +='x';
+            }
+            System.out.println(newString);
+                return;
+        }
+
+        char currChar=str.charAt(indx);
+        if(currChar=='x'){
+            count++;
+            moveallx(str, indx+1, count, newString);
+        } else {
+            newString +=currChar;
+            moveallx(str, indx+1, count, newString); 
+        }
+    }
+    public static void main(String args[]){
+        String str = "axbxxcxdx";
+        moveallx(str, 0, 0, "");
+
+    }
+    
+}
