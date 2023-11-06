081ef18f3f4efeb83feceac52fd391ec08d8f93c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-06 18:58:22 +0530

    reverse num

---
+++ Numbers/reversenumber/rev.java
@@ -0,0 +1,21 @@
+package Numbers.reversenumber;
+
+public class rev {
+    public static void main(String args[]){
+        int rem,revNum=0;
+        int num=1234;
+        int temp=num;
+        while(num>0){
+            rem=num%10;
+            revNum=revNum*10+rem;
+            num=num/10;
+        }
+        System.out.println(revNum);
+        if(revNum==temp){
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not palindrome");
+        }
+    }
+    
+}
