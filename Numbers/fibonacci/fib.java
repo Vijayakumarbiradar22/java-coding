4be1f57a1ec4b1d9eed27ca9abf8ae2150052558
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-06 19:16:29 +0530

    modified code

---
+++ Numbers/fibonacci/fib.java
@@ -0,0 +1,20 @@
+package Numbers.fibonacci;
+
+public class fib {
+    public static void findFib(int a,int b,int n){
+        if(n==0){
+           return;
+        }
+
+        int c=a+b;
+        System.out.println(c);
+        findFib(b,c,n-1);
+    }
+    public static void main(String args[]){
+        int n=7;
+        int a=0;
+        int b=1;
+        findFib(a,b,n-2);
+    }
+    
+}
