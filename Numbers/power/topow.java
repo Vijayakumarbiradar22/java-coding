e490378b2458f9ffbaef624bc7884f30fb8de94c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-06 18:48:53 +0530

    power code

---
+++ Numbers/power/topow.java
@@ -0,0 +1,34 @@
+package Numbers.power;
+import java.util.Scanner;
+public class topow {
+    public static int findPower(int x, int n){
+        if(n==0){
+            return 1;
+        }
+
+        if(x==0){
+            return 0;
+        }
+
+        if(n%2==0){
+            return findPower(x,n/2) * findPower(x,n/2);
+        }
+        else {
+            return findPower(x,n/2) * findPower(x,n/2) *x;
+        }
+        
+
+    }
+    public static void main(String args[]){
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter x:");
+            int x = sc.nextInt();
+            System.out.println("Enter n:");
+            int n = sc.nextInt();
+            findPower(x,n);
+        }
+    }
+    
+    
+    
+}
