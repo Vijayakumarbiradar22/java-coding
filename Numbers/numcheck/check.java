15f68a1c01fe613739219da8e7d69b10ec675ba0
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-06 18:34:01 +0530

    odd even code

---
+++ Numbers/numcheck/check.java
@@ -0,0 +1,16 @@
+package Numbers.numcheck;
+import java.util.Scanner;
+public class check {
+    public static void main(String args[]){
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the number:");
+            int num=sc.nextInt();
+            if(num%2==0){
+                System.out.println("Even");
+            } else {
+                System.out.println("Odd");
+            }
+        }
+    }
+    
+}
