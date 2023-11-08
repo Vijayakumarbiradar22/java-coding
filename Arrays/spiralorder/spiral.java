28d18ae5274a6aafef9bcac661855bf9bd7f6df2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-08 14:35:18 +0530

    new code

---
+++ Arrays/spiralorder/spiral.java
@@ -0,0 +1,52 @@
+package Arrays.spiralorder;
+import java.util.Scanner;
+public class spiral {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no.of rows:");
+        int r=sc.nextInt();
+        System.out.println("Enter the no.of cols");
+        int c=sc.nextInt();
+        int[][] mat = new int[r][c];
+        System.out.println("Enter the element:");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                mat[i][j]=sc.nextInt();
+            }
+        }
+
+        System.out.println("The spiral order is:");
+        int rowstart=0;
+        int rowend=r-1;
+        int colstart=0;
+        int colend=c-1;
+
+        while(rowstart<=rowend && colstart<=colend){
+
+            for(int col=colstart;col<=colend;col++){
+                System.out.print(mat[rowstart][col]+" ");
+            }
+            rowstart++;
+
+            for(int row=rowstart;row<=rowend;row++){
+                System.out.print(mat[row][colend]+" ");
+            }
+            colend--;
+
+            for(int col=colend;col>=colstart;col--){
+                System.out.print(mat[rowend][col]+" ");
+            }
+            rowend--;
+
+            for(int row=rowend;row>=rowstart;row--){
+                System.out.print(mat[row][colstart]+" ");
+            }
+            colstart++;
+
+        }
+
+
+
+    }
+    
+}
