6825cfedceae59d6af669ce62ff5f936c02ddacc
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-08 13:51:48 +0530

    added new code

--- Patterns/Stars/butterfly/fly.java
+++ Patterns/Stars/butterfly/fly.java
@@ -3,29 +3,13 @@ package Patterns.Stars.butterfly;
 public class fly {
     public static void main(String args[]){
         int n=4;
+        //Upper part
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
 
-            int sp=2*(n-i);
-            for(int s=1;s<=sp;s++){
-                System.out.print(" ");
-            } 
-
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-
-         System.out.println();   
-        }
-
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-
-            int sp=2*(n-i);
+            int sp = 2*(n-i);
             for(int s=1;s<=sp;s++){
                 System.out.print(" ");
             }
@@ -33,8 +17,30 @@ public class fly {
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
+
             System.out.println();
         }
+
+            //lower part
+
+            for(int i=n;i>=1;i--){
+                for(int j=1;j<=i;j++){
+                    System.out.print("*");
+                }
+                
+                int sp=2*(n-i);
+                for(int s=1;s<=sp;s++){
+                    System.out.print(" ");
+                }
+
+                for(int j=1;j<=i;j++){
+                    System.out.print("*");
+                }
+
+                System.out.println();
+            }
+
+        
     }
     
 }
