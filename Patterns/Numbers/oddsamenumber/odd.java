be6e586460607cd37400d4f1b9789a2b963399e5
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-08 13:56:34 +0530

    added new code

---
+++ Patterns/Numbers/oddsamenumber/odd.java
@@ -0,0 +1,17 @@
+package Patterns.Numbers.oddsamenumber;
+
+public class odd {
+    public static void main(String arg[]){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
