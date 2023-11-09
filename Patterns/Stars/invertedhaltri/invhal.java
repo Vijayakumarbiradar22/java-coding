58da6d5d33725bf1656da736d0edbe757bb3b1e0
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-09 22:05:51 +0530

    new code added

--- Patterns/Stars/invertedhaltri/invhal.java
+++ Patterns/Stars/invertedhaltri/invhal.java
@@ -2,19 +2,21 @@ package Patterns.Stars.invertedhaltri;
 import java.util.Scanner;
 public class invhal {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the rows:");
-        int n=sc.nextInt();
-        for(int i=n;i>0;i++){
-            for(int j=1;j<=i;j++){
-                if(j==n-i+1 || j==n+i-1 || i==n){
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the rows:");
+            int n=sc.nextInt();
+            for(int i=n;i>0;i++){
+                for(int j=1;j<=i;j++){
+                    if(j==n-i+1 || j==n+i-1 || i==n){
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
-            }
-            System.out.println();
+                System.out.println();
+            
         }
+    }
 
     }
     
