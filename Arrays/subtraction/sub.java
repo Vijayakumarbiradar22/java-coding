958202b6819c90f232bd23e8b3fc5e609944d5d1
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-10 19:34:17 +0530

    added new code

---
+++ Arrays/subtraction/sub.java
@@ -0,0 +1,45 @@
+package Arrays.subtraction;
+import java.util.Scanner;
+public class sub {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no.of rows:");
+        int r = sc.nextInt();
+        System.out.println("Enter the no.of cols:");
+        int c = sc.nextInt();
+        int[][] arr1=new int[r][c];
+        int[][] arr2=new int[r][c];
+        System.out.println("Enter the array 1:");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr1[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println(arr1.length);
+
+        System.out.println("Enter the array 2:");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr2[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println(arr2.length);
+
+        int arr3[][]=new int[arr1.length][arr2.length];
+
+        System.out.println("Array1 minus Array2 is:");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr3[i][j]=0;
+                for(int k=0;k<arr3.length;k++){
+                    arr3[i][j]=arr1[i][j]-arr2[i][j];
+                }
+                System.out.println(arr3[i][j]+" ");
+            }
+            System.out.println();
+        }
+        
+
+    }
+    
+}
