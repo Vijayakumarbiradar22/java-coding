727afef4eee0c0367e464e63a58744cab4920009
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-11 21:43:09 +0530

    new code added

---
+++ Patterns/Alpha/alphaseries/ab.java
@@ -0,0 +1,14 @@
+package Patterns.Alpha.alphaseries;
+
+public class ab {
+    public static void main(String args[]){
+        char chr='E';
+        for(char ch='A';ch<=chr;ch++){
+            for(char j='A';j<=chr;j++){
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
