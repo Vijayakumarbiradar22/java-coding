b9e82cc1b2eae5ba1d790a08ce7fa808c0071933
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-11 21:55:04 +0530

    new code added

--- Patterns/Alpha/nextalpha/next.java
+++ Patterns/Alpha/nextalpha/next.java
@@ -1,13 +1,19 @@
 package Patterns.Alpha.nextalpha;
-
+import java.util.Scanner;
 public class next {
     public static void main(String args[]){
+        try(Scanner sc = new Scanner(System.in)){
+        //System.out.println("Enter the end character:");
         char ch='E';
         for(char i='A';i<=ch;i++){
+            for(int s=1;s<=ch-i;s++){
+                System.out.print(" ");
+            }
             for(char j='A';j<=i;j++){
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.println();
+        }
         }
     }
     
