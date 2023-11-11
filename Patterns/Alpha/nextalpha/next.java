d02b8ac10cd0d6a85bea09a7e67d203ea82e8ba0
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-11 21:48:11 +0530

    new code added

---
+++ Patterns/Alpha/nextalpha/next.java
@@ -0,0 +1,14 @@
+package Patterns.Alpha.nextalpha;
+
+public class next {
+    public static void main(String args[]){
+        char ch='E';
+        for(char i='A';i<=ch;i++){
+            for(char j='A';j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
