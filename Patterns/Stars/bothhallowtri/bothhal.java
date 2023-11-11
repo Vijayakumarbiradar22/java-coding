92ecb577ea346720cdfad9568c076d2edeadbbf2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-11 21:35:27 +0530

    new code added

--- Patterns/Stars/bothhallowtri/bothhal.java
+++ Patterns/Stars/bothhallowtri/bothhal.java
@@ -1,8 +1,10 @@
 package Patterns.Stars.bothhallowtri;
-
+import java.util.Scanner;
 public class bothhal {
     public static void main(String args[]){
-        int n=5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
         for(int i=n;i>=1;i--){
             for(int j=1;j<=2*n-1;j++){
                 if(j==n+i-1 || j==n-i+1 || i==n){
