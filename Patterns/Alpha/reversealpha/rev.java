20f538fab6a29a3be0e75eab031406f1317c7e65
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-11 22:17:59 +0530

    addednew code

---
+++ Patterns/Alpha/reversealpha/rev.java
@@ -0,0 +1,14 @@
+package Patterns.Alpha.reversealpha;
+
+public class rev {
+    public static void main(String args[]){
+        char ch='G';
+        for(char i='A';i<=ch;i++){
+            for(char j='A';j<=ch;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
