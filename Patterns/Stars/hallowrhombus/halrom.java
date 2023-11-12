2cba7de227ffc4fc63fd2d7b40b1987579392095
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-12 22:06:20 +0530

    added new code

--- Patterns/Stars/hallowrhombus/halrom.java
+++ Patterns/Stars/hallowrhombus/halrom.java
@@ -2,29 +2,30 @@ package Patterns.Stars.hallowrhombus;
 import java.util.Scanner;
 public class halrom {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the rows:");
-        int n=sc.nextInt();
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=2*n-1;j++){
-                if(j==n+i-1 || j==n-i+1 || i==n){
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the rows:");
+            int n=sc.nextInt();
+            for(int i=1;i<n;i++){
+                for(int j=1;j<=2*n-1;j++){
+                    if(j==n+i-1 || j==n-i+1 || i==n){
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
+                System.out.println();
             }
-            System.out.println();
-        }
 
-        for(int i=n-2;i>=1;i--){
-            for(int j=1;j<=2*n-1;j++){
-                if(j==n+i-1 || j==n-i+1 || i==n){
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
+            for(int i=n-2;i>=1;i--){
+                for(int j=1;j<=2*n-1;j++){
+                    if(j==n+i-1 || j==n-i+1 || i==n){
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
+                System.out.println();
             }
-            System.out.println();
         }
     }
     
