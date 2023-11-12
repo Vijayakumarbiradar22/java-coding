9b6459260b23da3e0503e5b8b5995216fb05ddae
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-12 22:06:06 +0530

    added new code

--- Patterns/Stars/hallowrhombus/halrom.java
+++ Patterns/Stars/hallowrhombus/halrom.java
@@ -1,8 +1,10 @@
 package Patterns.Stars.hallowrhombus;
-
+import java.util.Scanner;
 public class halrom {
     public static void main(String args[]){
-        int n=6;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
         for(int i=1;i<n;i++){
             for(int j=1;j<=2*n-1;j++){
                 if(j==n+i-1 || j==n-i+1 || i==n){
