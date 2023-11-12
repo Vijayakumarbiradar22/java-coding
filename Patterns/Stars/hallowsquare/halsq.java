111a4960f22a7c91e363e40fb2b8f2baefd59a60
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-12 22:05:19 +0530

    added new code

--- Patterns/Stars/hallowsquare/halsq.java
+++ Patterns/Stars/hallowsquare/halsq.java
@@ -2,18 +2,19 @@ package Patterns.Stars.hallowsquare;
 import java.util.Scanner;
 public class halsq {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the rows:");
-        int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(i==1 || i==n || j==1 || j==n){
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the rows:");
+            int n=sc.nextInt();
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n;j++){
+                    if(i==1 || i==n || j==1 || j==n){
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
+                System.out.println();
             }
-            System.out.println();
         }
     }
     
