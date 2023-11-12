2a0426ad81441ac818f8881e3231ba8b2be60a60
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-12 22:03:42 +0530

    added new code

--- Patterns/Stars/hallowtriangle/haltri.java
+++ Patterns/Stars/hallowtriangle/haltri.java
@@ -1,8 +1,10 @@
 package Patterns.Stars.hallowtriangle;
-
+import java.util.Scanner;
 public class haltri {
     public static void main(String args[]){
-        int n=4;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=2*n-1;j++){
                 if(j==n-i+1 || j==n+i-1 || i==n ){
