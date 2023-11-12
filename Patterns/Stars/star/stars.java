81b40c06f9ac12d70919cad1c390507a73db78bf
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-12 22:07:59 +0530

    added new code

--- Patterns/Stars/star/stars.java
+++ Patterns/Stars/star/stars.java
@@ -2,17 +2,18 @@ package Patterns.Stars.star;
 import java.util.Scanner;
 public class stars {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the rows:");
-        int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int s=1;s<=n-i;s++){
-                System.out.print(" ");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the rows:");
+            int n=sc.nextInt();
+            for(int i=1;i<=n;i++){
+                for(int s=1;s<=n-i;s++){
+                    System.out.print(" ");
+                }
+                for(int j=1;j<=i;j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
             }
-            for(int j=1;j<=i;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
         }
     }
     
