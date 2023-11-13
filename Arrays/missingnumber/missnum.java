9a5a8c4ecb0938f68b2ef628bf6351aafe825fd0
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-13 12:13:44 +0530

    new code added

--- Arrays/missingnumber/missnum.java
+++ Arrays/missingnumber/missnum.java
@@ -10,16 +10,17 @@ public class missnum {
         return sum;
     }
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array:");
-        int size=sc.nextInt();
-        int[] arr = new int[size];
-        System.out.println("Enter the elements");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the size of array:");
+            int size=sc.nextInt();
+            int[] arr = new int[size];
+            System.out.println("Enter the elements");
+            for(int i=0;i<size;i++){
+                arr[i]=sc.nextInt();
+            }
+            int result = findMissing(arr,size);
+            System.out.println(result);
         }
-        int result = findMissing(arr,size);
-        System.out.println(result);
     }
     
 }
