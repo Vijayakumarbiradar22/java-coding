63df90fa942896caf7cdaefe05935c2c5920ca31
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-13 09:31:30 +0530

    new code added

---
+++ Arrays/leftrotate/lrotate.java
@@ -0,0 +1,35 @@
+package Arrays.leftrotate;
+import java.util.Scanner;
+public class lrotate {
+    public static void leftRotate(int arr[],int size, int rt){
+        int p=1;
+        while(p<=rt){
+            int last=arr[0];
+            for(int i=0;i<size-1;i++){
+                arr[i]=arr[i+1];
+            }
+            arr[size-1]=last;
+            p++;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int size=sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the elements of array:");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the rotation times");
+        int rt=sc.nextInt();
+
+        leftRotate(arr,size,rt);
+
+        for(int i=0;i<size;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+    
+}
