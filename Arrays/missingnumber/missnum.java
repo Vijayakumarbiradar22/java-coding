df17a4c77da34e8df325041135d6dd262dfd0b55
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-13 12:13:24 +0530

    new code added

---
+++ Arrays/missingnumber/missnum.java
@@ -0,0 +1,25 @@
+package Arrays.missingnumber;
+import java.util.Scanner;
+public class missnum {
+    public static int findMissing(int arr[], int size){
+        int sum=((size+1)*(size+2))/2;
+
+        for(int i=0;i<size;i++){
+            sum=sum-arr[i];
+        }
+        return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int size=sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the elements");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        int result = findMissing(arr,size);
+        System.out.println(result);
+    }
+    
+}
