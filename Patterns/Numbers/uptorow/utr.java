d81b01f7e48b817c1c1212489e7b1724a63d8966
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-14 21:56:34 +0530

    new code added

--- Patterns/Numbers/uptorow/utr.java
+++ Patterns/Numbers/uptorow/utr.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class utr {
     public static void main(String args[]){
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the number:");
+            System.out.println("Enter the number of rows:");
             int n=sc.nextInt();
             //int n=5;
             for(int i=1;i<=n;i++){
