ea779542d062d739d47e3d04faaca2fc4742a263
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-14 22:00:20 +0530

    new code added

--- Patterns/Numbers/oddsamenumber/odd.java
+++ Patterns/Numbers/oddsamenumber/odd.java
@@ -2,17 +2,18 @@ package Patterns.Numbers.oddsamenumber;
 import java.util.Scanner;
 public class odd {
     public static void main(String arg[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no.of rows:");
-        int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int s=1;s<=n-i;s++){
-                System.out.print(" ");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the no.of rows:");
+            int n=sc.nextInt();
+            for(int i=1;i<=n;i++){
+                for(int s=1;s<=n-i;s++){
+                    System.out.print(" ");
+                }
+                for(int j=1;j<=i;j++){
+                    System.out.print(i+" ");
+                }
+                System.out.println();
             }
-            for(int j=1;j<=i;j++){
-                System.out.print(i+" ");
-            }
-            System.out.println();
         }
     }
     
