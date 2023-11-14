6ff8ddecf5a0f5f22e7133f53acc80e5a77d583f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-14 21:57:49 +0530

    new code added

--- Patterns/Numbers/samenumber/sn.java
+++ Patterns/Numbers/samenumber/sn.java
@@ -2,14 +2,15 @@ package Patterns.Numbers.samenumber;
 import java.util.Scanner;
 public class sn {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no.of rows:");
-        int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(i);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the no.of rows:");
+            int n=sc.nextInt();
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=i;j++){
+                    System.out.print(i);
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
     
