30bd2a6a26dc3ff6e96ddaa8adf8f6a3cebe7342
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-14 21:59:09 +0530

    new code added

--- Patterns/Numbers/palindromenum/palin.java
+++ Patterns/Numbers/palindromenum/palin.java
@@ -2,21 +2,22 @@ package Patterns.Numbers.palindromenum;
 import java.util.Scanner;
 public class palin {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no.of rows:");
-        int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int s=1;s<=n-i;s++){
-                System.out.print(" ");
-            }
-            for(int j=i;j>=1;j--){
-                System.out.print(j);
-            }
-            for(int j=2;j<=i;j++){
-                System.out.print(j);
-            }
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the no.of rows:");
+            int n=sc.nextInt();
+            for(int i=1;i<=n;i++){
+                for(int s=1;s<=n-i;s++){
+                    System.out.print(" ");
+                }
+                for(int j=i;j>=1;j--){
+                    System.out.print(j);
+                }
+                for(int j=2;j<=i;j++){
+                    System.out.print(j);
+                }
 
-            System.out.println();
+                System.out.println();
+            }
         }
     }
     
