67357aaa9416803b2efc2c88cd597be008b597ed
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-14 22:01:55 +0530

    new code added

--- Patterns/Numbers/binary/bn.java
+++ Patterns/Numbers/binary/bn.java
@@ -2,20 +2,21 @@ package Patterns.Numbers.binary;
 import java.util.Scanner;
 public class bn {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no.of rows:");
-        int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                int sum=i+j;
-                if(sum%2==0){
-                    System.out.print("1");
-                } else {
-                    System.out.print("0");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the no.of rows:");
+            int n=sc.nextInt();
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=i;j++){
+                    int sum=i+j;
+                    if(sum%2==0){
+                        System.out.print("1");
+                    } else {
+                        System.out.print("0");
+                    }
+                    
                 }
-                
+                System.out.println();
             }
-            System.out.println();
         }
     }
     
