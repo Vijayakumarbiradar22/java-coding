6b99767f6264ccff09f7babc365ad3445bc9ca3a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-14 21:55:14 +0530

    new code added

--- Patterns/Numbers/uptorow/utr.java
+++ Patterns/Numbers/uptorow/utr.java
@@ -1,8 +1,11 @@
 package Patterns.Numbers.uptorow;
-
+import java.util.Scanner;
 public class utr {
     public static void main(String args[]){
-        int n=5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n=sc.nextInt();
+        //int n=5;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(j);
