2e8d043de03df200d91b2ec0aa730e16953ba8de
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-15 21:13:32 +0530

    added new code

--- ArrayList/AL.java
+++ ArrayList/AL.java
@@ -20,7 +20,7 @@ public class AL {
         System.out.println(size);
 
         for(int i=0;i<list.size();i++){
-            // System.out.println(list.get(i));
+            System.out.println(list.get(i));
         }
     }
     
