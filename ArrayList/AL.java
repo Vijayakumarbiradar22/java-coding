5ee68f3cdaebbf366d68df9a0f6ca4b496e9595a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-15 21:05:37 +0530

    added new code

---
+++ ArrayList/AL.java
@@ -0,0 +1,20 @@
+package ArrayList;
+import java.util.ArrayList;
+import java.util.Collections;
+public class AL {
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+
+        int get2=list.get(1);
+        System.out.println(get2);
+
+        // Collections.sort(list);
+        // System.out.println(list);
+    }
+    
+}
