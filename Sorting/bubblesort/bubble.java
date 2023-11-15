132f7dbdfb1e1b9d89628c12f6427050207687e1
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-15 21:35:23 +0530

    added new code

--- Sorting/bubblesort/bubble.java
+++ Sorting/bubblesort/bubble.java
@@ -14,6 +14,8 @@ public class bubble {
         }
     }
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
         int[] array ={1,7,3,6,5};
         int size=array.length;
         bubbleSort(array,size);
