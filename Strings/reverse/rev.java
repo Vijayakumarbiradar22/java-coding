da557a1c37d4061b918a67ebe35ebadbed0b38fa
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-16 22:09:48 +0530

    added new code

---
+++ Strings/reverse/rev.java
@@ -0,0 +1,25 @@
+package Strings.reverse;
+import java.util.Scanner;
+public class rev {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str=sc.nextLine();
+
+        StringBuilder sb = new StringBuilder(str);
+
+        for(int i=0;i<sb.length()/2;i++){
+            int front=i;
+            int back =sb.length()-1-i;
+
+            char frontchar = sb.charAt(front);
+            char backchar = sb.charAt(back);
+
+            sb.setCharAt(front, backchar);
+            sb.setCharAt(back, frontchar);
+        }
+        System.out.println(sb);
+
+    }
+    
+}
