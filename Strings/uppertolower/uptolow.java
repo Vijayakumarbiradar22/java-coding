20e6cf7a2de80cbbef950fbaadc069180eaef571
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-16 21:53:10 +0530

    added new code

---
+++ Strings/uppertolower/uptolow.java
@@ -0,0 +1,17 @@
+package Strings.uppertolower;
+import java.util.Scanner;
+public class uptolow {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str = sc.nextLine();
+        String res="";
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)>='A'&& str.charAt(i)<='Z'){
+                res+=str.charAt(i)+('a'-'A');
+            }
+        }
+        System.out.println(res);
+    }
+    
+}
