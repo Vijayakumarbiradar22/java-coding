72509e0c64b0b7b351da41c9f02b90953ce8b6fb
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-16 21:31:02 +0530

    added new code

--- Strings/countvowels/vowel.java
+++ Strings/countvowels/vowel.java
@@ -2,23 +2,23 @@ package Strings.countvowels;
 import java.util.Scanner;
 public class vowel {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string");
-        String str=sc.nextLine();
-        int count=0;
-
-        for(int i=0;i<str.length();i++){
-            if(str.toLowerCase().charAt(i)=='a' ||
-            str.toLowerCase().charAt(i)=='e' ||
-            str.toLowerCase().charAt(i)=='i' ||
-            str.toLowerCase().charAt(i)=='o' ||
-            str.toLowerCase().charAt(i)=='u'){
-                if(str.charAt(i)!=' '){
-                    count+=1;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the string");
+            String str=sc.nextLine();
+            int count=0;
+            for(int i=0;i<str.length();i++){
+                if(str.toLowerCase().charAt(i)=='a' ||
+                str.toLowerCase().charAt(i)=='e' ||
+                str.toLowerCase().charAt(i)=='i' ||
+                str.toLowerCase().charAt(i)=='o' ||
+                str.toLowerCase().charAt(i)=='u'){
+                    if(str.charAt(i)!=' '){
+                        count+=1;
+                    }
                 }
             }
+            System.out.println(count);
         }
-        System.out.println(count);
     }
     
 }
