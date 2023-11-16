d511ee30d1148732fd5be725457a9e7ad33fec89
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-16 22:27:19 +0530

    added new code

---
+++ Strings/reversebyword/revbyword.java
@@ -0,0 +1,20 @@
+package Strings.reversebyword;
+import java.util.Scanner;
+public class revbyword {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str=sc.nextLine();
+        String ns[]=str.split(" ");
+        String nstr="";
+
+        for(int i=0;i<ns.length;i++){
+            for(int j=ns[i].length()-1;j>=0;j--){
+                nstr+=ns[i].charAt(j);
+            }
+            nstr+=" ";
+        }
+        System.out.println(nstr);
+    }
+    
+}
