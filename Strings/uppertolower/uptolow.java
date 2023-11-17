9d9c92cf7682fc329a559c59bdb6ea020be8458f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-17 19:50:24 +0530

    modified

--- Strings/uppertolower/uptolow.java
+++ Strings/uppertolower/uptolow.java
@@ -2,7 +2,7 @@ package Strings.uppertolower;
 import java.util.Scanner;
 public class uptolow {
     public static void main(String args[]){
-        try (Scanner sc = new Scanner(System.in)) {
+        Scanner sc = new Scanner(System.in);
             System.out.println("Enter the string:");
             String str = sc.nextLine();
             String res="";
@@ -12,7 +12,9 @@ public class uptolow {
                 }
             }
             System.out.println(res);
-        }
+            sc.close();
+        
+        
     }
     
 }
