a32d722b05015fd87781fb3b1633438bfa7fff2b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-20 19:29:28 +0530

    added new code

---
+++ Arrays/betweenrange/nums.java
@@ -0,0 +1,23 @@
+package Arrays.betweenrange;
+
+public class nums {
+    public static void main(String args[]){
+        int arr[]= {1,12,14,20,25,37,65,72};
+        int start=2;
+        int end=40;
+        int count=0;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>=start && arr[i]<=end){
+                count+=1;
+            }
+        }
+
+        if(count>0){
+            System.out.println(count+" numbers available");
+        } else {
+            System.out.println("No numbers available");
+        }
+    }
+    
+}
