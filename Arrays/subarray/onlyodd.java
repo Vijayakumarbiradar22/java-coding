03d422ac294756332fe76df5b6e84da28724735d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-20 19:08:45 +0530

    added new code

--- Arrays/subarray/onlyodd.java
+++ Arrays/subarray/onlyodd.java
@@ -1,11 +1,19 @@
 package Arrays.subarray;
-
+import java.util.Scanner;
 public class onlyodd {
     public static void main(String args[]){
-        int[] arr = {1,2,3,4,5,6,7,8,9};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the elements of array:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        //int[] arr = {1,2,3,4,5,6,7,8,9};
         int od=0;
 
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<n;i++){
             if(arr[i]%2!=0){
                 od+=1;
             }
@@ -14,7 +22,7 @@ public class onlyodd {
         int[] arr1=new int[od];
         int x=0;
 
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<n;i++){
             if(arr[i]%2!=0){
                 arr1[x]=arr[i];
                 x++;
