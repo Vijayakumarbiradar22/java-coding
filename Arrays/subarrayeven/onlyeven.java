83becbd6c2c917612cda7d82d56b72d20a6c52c7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-20 19:06:25 +0530

    added new code

--- Arrays/subarrayeven/onlyeven.java
+++ Arrays/subarrayeven/onlyeven.java
@@ -1,11 +1,19 @@
 package Arrays.subarrayeven;
-
+import java.util.Scanner;
 public class onlyeven {
     public static void main(String args[]){
-        int[] arr = {1,2,3,4,5,6,7,8,9};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the element of array:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        //int[] arr = {1,2,3,4,5,6,7,8,9};
         int even=0;
-        
-        for(int i=0;i<arr.length;i++){
+
+        for(int i=0;i<n;i++){
             if(arr[i]%2==0){
                 even+=1;
             }
@@ -14,7 +22,7 @@ public class onlyeven {
         int arr1[]=new int[even];
         int x=0;
 
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<n;i++){
             if(arr[i]%2==0){
                 arr1[x]=arr[i];
                 x++;
