7aa5c981b6a01a5541902ac79076e44d45ed2ef2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-21 21:52:32 +0530

    added new code

--- Patterns/Numbers/nextnumber/nextnum.java
+++ Patterns/Numbers/nextnumber/nextnum.java
@@ -1,8 +1,11 @@
 package Patterns.Numbers.nextnumber;
-
+import java.util.Scanner;
 public class nextnum {
     public static void main(String args[]){
-        int n=4,num=1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
+        int num=1;
         for(int i=1;i<=n;i++){
             for(int s=1;s<=n-i;s++){
                 System.out.print(" ");
