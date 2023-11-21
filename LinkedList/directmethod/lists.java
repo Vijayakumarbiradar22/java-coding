87d437a3e0383d8b1e50539605d6173d2c8d3154
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-21 12:37:39 +0530

    added new code

---
+++ LinkedList/directmethod/lists.java
@@ -0,0 +1,22 @@
+package LinkedList.directmethod;
+import java.util.*;
+public class lists {
+    public static void main(String args[]){
+        LinkedList<String> list =new LinkedList<>();
+        list.addFirst("10");
+        list.addLast("20");
+        list.addLast("30");
+        list.addLast("40");
+
+        list.removeFirst();
+        list.removeLast();
+
+        System.out.println(list.size());
+
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+"->");
+        }
+        System.out.println("NULL");
+    }
+    
+}
