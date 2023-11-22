bef74092c0e1451f7b4ed0ec0caa97eac3ee1d82
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-22 07:55:05 +0530

    added new code

---
+++ Numbers/primenumber/prime.java
@@ -0,0 +1,30 @@
+package Numbers.primenumber;
+import java.util.Scanner;
+public class prime {
+    public static boolean isPrime(int num){
+        if(num<2){
+            return false;
+        }
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the start number:");
+        int start=sc.nextInt();
+        System.out.println("Enter thhe end number:");
+        int end = sc.nextInt();
+        for(int i=start;i<end;i++){
+            while(!isPrime(i)){
+                System.out.print(i+" ");
+            }
+        }
+        //sc.close();
+
+    }
+    
+}
