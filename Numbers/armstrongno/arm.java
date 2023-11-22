940b9b897a58a9304db0f3c78f0ed3dae18f9ff7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-22 19:34:35 +0530

    added new code

---
+++ Numbers/armstrongno/arm.java
@@ -0,0 +1,31 @@
+package Numbers.armstrongno;
+import java.util.Scanner;
+public class arm {
+    public static void main(String args[]){
+        int count=0;
+        double rem,sum=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=sc.nextInt();
+        int original=num;
+        while(num!=0){
+            num=num/10;
+            count++;
+        }
+       
+        num=original;
+        while(num!=0){
+            rem=num%10;
+            sum=sum+Math.pow(rem,count);
+            num=num/10;
+        }
+        if(original==sum){
+            System.out.println(original+" is Armstrong number.");
+        } else {
+            System.out.println(original+" is not Armstrong number.");
+        }
+
+
+    }
+    
+}
