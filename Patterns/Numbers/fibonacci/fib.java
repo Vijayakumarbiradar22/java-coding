f9190a246f518da6ac97b59f0e4f075a85311758
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-22 07:39:43 +0530

    added new code

--- Patterns/Numbers/fibonacci/fib.java
+++ Patterns/Numbers/fibonacci/fib.java
@@ -16,10 +16,14 @@ public class fib {
         int currIndex=0;
 
         for(int i=1;i<=n;i++){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
             for(int j=1;j<=i;j++){
-                System.out.print(fibseries[currIndex]);
+                System.out.print(fibseries[currIndex]+" ");
                 currIndex++;
             }
+            System.out.println();
         }
     }
     public static void main(String args[]){
