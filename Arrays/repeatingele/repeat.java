b7c8415da7d1b591051342e90ee5ea20d6d12527
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-23 22:05:14 +0530

    added new code

--- Arrays/repeatingele/repeat.java
+++ Arrays/repeatingele/repeat.java
@@ -5,7 +5,7 @@ public class repeat {
         for(int i=0;i<size-1;i++){
             for(int j=i+1;j<size;j++){
                 if(arr[i]==arr[j]){
-                    System.out.println(arr[i]+" ");
+                    System.out.println("Repeating elements are:"+arr[i]+" ");
                 }
             }
         }
