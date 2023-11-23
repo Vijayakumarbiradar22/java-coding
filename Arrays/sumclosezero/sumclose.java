15b754f788fcb32a1e01e04ede0e789cb726e526
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-23 21:51:46 +0530

    added new code

---
+++ Arrays/sumclosezero/sumclose.java
@@ -0,0 +1,36 @@
+package Arrays.sumclosezero;
+import java.util.Scanner;
+public class sumclose {
+    public static void sumClose(int arr[],int size){
+        //int inv_count=0;
+        int l,r,min_sum,sum,min_l,min_r;
+        min_l=0;
+        min_r=1;
+        min_sum=arr[0]+arr[1];
+
+        for(l=0;l<size-1;l++){
+            for(r=l+1;r<size;r++){
+                sum=arr[l]+arr[r];
+                if(Math.abs(min_sum)>Math.abs(sum)){
+                    min_sum=sum;
+                    min_l=l;
+                    min_r=r;
+                }
+            }
+        }
+        System.out.println("The 2 elements whose sum is minimum are "+arr[min_l]+" and "+arr[min_r]);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the elements:");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        sumClose(arr,size);
+        sc.close();
+    }
+    
+}
