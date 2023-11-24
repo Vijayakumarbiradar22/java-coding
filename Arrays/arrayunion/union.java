2e078032041f8d018c17bdb925c26c045ae86f36
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-24 19:43:15 +0530

    added new code

---
+++ Arrays/arrayunion/union.java
@@ -0,0 +1,44 @@
+package Arrays.arrayunion;
+import java.util.Scanner;
+import java.util.*;
+public class union {
+    public static ArrayList<Integer> Unionarray(int arr1[],int arr2[],int size1,int size2){
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int i:arr1){
+            set.add(i);
+        }
+
+        for(int i:arr2){
+            set.add(i);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i:set){
+            list.add(i);
+        }
+        return list;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array1:");
+        int size1=sc.nextInt();
+        int arr1[]=new int[size1];
+        System.out.println("Enter the elements of array1:");
+        for(int i=0;i<size1;i++){
+            arr1[i]=sc.nextInt();
+        }
+        System.out.println("Enter the size of array2:");
+        int size2=sc.nextInt();
+        int arr2[]=new int[size2];
+        System.out.println("Enter the elements of array2:");
+        for(int i=0;i<size2;i++){
+            arr2[i]=sc.nextInt();
+        }
+
+        ArrayList<Integer> uni = Unionarray(arr1,arr2,size1,size2);
+        for(int i:uni){
+            System.out.print(i+" ");
+        }
+    }
+    
+}
