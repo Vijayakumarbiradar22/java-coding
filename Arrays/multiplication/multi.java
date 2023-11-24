0146aac2da1f39494d9c820eb6999924dc00e996
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-24 10:26:15 +0530

    added new code

--- Arrays/multiplication/multi.java
+++ Arrays/multiplication/multi.java
@@ -1,6 +1,20 @@
 package Arrays.multiplication;
 import java.util.Scanner;
 public class multi {
+    public static void mulMat(int mat1[][],int mat2[][],int R1,int C1,int R2,int C2){
+        int res[][]=new int[R1][C2];
+        System.out.println("Multiplication of matrix is:");
+        for(int i=0;i<R1;i++){
+        for(int j=0;j<C2;j++){
+            for(int k=0;k<R2;k++){
+                res[i][j]+=mat1[i][k]*mat2[k][j];
+            }
+            System.out.print(res[i][j]+" ");
+        }
+        System.out.println();
+       }
+
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the no.of rows and cols in mat1:");
@@ -11,7 +25,6 @@ public class multi {
         int C2=sc.nextInt();
         int mat1[][]=new int[R1][C1];
         int mat2[][]=new int[R2][C2];
-         int res[][]=new int[R1][C2];
         System.out.println("Enter the elements of mat1:");
         for(int i=0;i<R1;i++){
             for(int j=0;j<C1;j++){
@@ -28,17 +41,7 @@ public class multi {
         if(C1!=R2){
             System.out.println("Matrices cannot be multiplied.");
         }
-       
-        System.out.println("Multiplication of matrix is:");
-        for(int i=0;i<R1;i++){
-        for(int j=0;j<C2;j++){
-            for(int k=0;k<R2;k++){
-                res[i][j]+=mat1[i][k]*mat2[k][j];
-            }
-            System.out.print(res[i][j]+" ");
-        }
-        System.out.println();
-       }
+        mulMat(mat1,mat2,R1,C1,R2,C2);
         sc.close();
     }
     
