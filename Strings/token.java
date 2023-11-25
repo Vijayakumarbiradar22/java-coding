3c7b230b1de2b3013722bfcb91154a3f3c8d27a9
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-25 11:27:45 +0530

    added new code

---
+++ Strings/token.java
@@ -0,0 +1,17 @@
+package Strings;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class token {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str=sc.nextLine();
+
+        StringTokenizer st1 = new StringTokenizer(str," ");
+
+        while(st1.hasMoreTokens()){
+            System.out.println(st1.nextToken());
+        }
+    }
+    
+}
