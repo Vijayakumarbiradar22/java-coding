0e61fc1e19aba3e2f5d39ba24b62b9956af44bf8
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-26 22:34:56 +0530

    added new code

---
+++ Patterns/Numbers/oddseries/odd.java
@@ -0,0 +1,19 @@
+package Patterns.Numbers.oddseries;
+import java.util.Scanner;
+public class odd {
+    public static void main(String args[]){
+        int num=1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num);
+                num+=2;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
