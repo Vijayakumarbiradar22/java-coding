38899a1f5a89fb04400cd47b5c9d1126ee8e3369
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-26 22:27:14 +0530

    added new code

--- Patterns/Stars/ultahalfrhombus/ulta.java
+++ Patterns/Stars/ultahalfrhombus/ulta.java
@@ -14,6 +14,16 @@ public class ulta {
             }
             System.out.println();
         }
+
+        for(int i=n;i>=1;i--){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         sc.close();
     }
     
