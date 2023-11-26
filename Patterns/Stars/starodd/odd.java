d2d608ec9e0d5976d298aac8cdfb10fe47e01873
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-26 14:54:44 +0530

    added new code

---
+++ Patterns/Stars/starodd/odd.java
@@ -0,0 +1,18 @@
+package Patterns.Stars.starodd;
+import java.util.Scanner;
+public class odd {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no.of rows:");
+        int n=sc.nextInt();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
