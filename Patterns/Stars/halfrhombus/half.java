1216209d771af3d8f71404418808afa7904f752e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-26 22:20:01 +0530

    added new code

--- Patterns/Stars/halfrhombus/half.java
+++ Patterns/Stars/halfrhombus/half.java
@@ -12,6 +12,13 @@ public class half {
             }
             System.out.println();
         }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
     }
     
 }
