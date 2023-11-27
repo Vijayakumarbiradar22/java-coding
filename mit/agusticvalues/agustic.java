1a80a18d9badc4987ccc5919207e3aeb9512fa82
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-27 21:06:04 +0530

    added new code

--- mit/agusticvalues/agustic.java
+++ mit/agusticvalues/agustic.java
@@ -2,16 +2,16 @@ package mit.agusticvalues;
 import java.util.Scanner;
 public class agustic {
     public static String findSumOfAgusticValues(String s){
-        long result=calculateAgusticValue(s,1,s.charAt(0)-'0',0);
+        long result=calculateAgusticValues(s,1,s.charAt(0)-'0',0);
         return Long.toString(result);
     }
 
-    public static long calculateAgusticValue(String s,int index,long current,long currentSum){
+    public static long calculateAgusticValues(String s,int index,long current,long currentSum){
         if(index==s.length()){
             return currentSum+current;
         }
-        long result=calculateAgusticValue(s,index+1,current*10+(s.charAt(index)-'0'),currentSum);
-        result+=calculateAgusticValue(s,index+1,s.charAt(index)-'0',currentSum+current);
+        long result=calculateAgusticValues(s,index+1,current*10+(s.charAt(index)-'0'),currentSum);
+        result+=calculateAgusticValues(s,index+1,s.charAt(index)-'0',currentSum+current);
         return result;
     }
     public static void main(String args[]){
