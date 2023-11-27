6c73b9d3a7c70ee45ef6a4f4d7652768268486db
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-27 23:13:45 +0530

    added new code

---
+++ mit/maxtoys/toys.java
@@ -0,0 +1,36 @@
+package mit.maxtoys;
+import java.util.Scanner;
+public class toys {
+    public static int getMaxToys(int prices[],int money){
+        int sum=0,count=0,max=0;
+
+        for(int i=0;i<prices.length;i++){
+            for(int j=i;j<prices.length;j++){
+                if(sum+prices[j]<=money){
+                    sum=sum+prices[j];
+                    count=count+1;
+                    max=Math.max(count,max);
+                } else {
+                    count=0;
+                    sum=0;
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n = sc.nextInt();
+        int[] prices=new int[n];
+        System.out.println("Enter the prices of toys:");
+        for(int i=0;i<n;i++){
+            prices[i]=sc.nextInt();
+        }
+        System.out.println("Enter the money:");
+        int money=sc.nextInt();
+        System.out.println(getMaxToys(prices,money));
+    }
+    
+}
