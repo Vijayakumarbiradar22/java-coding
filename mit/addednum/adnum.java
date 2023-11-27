37a09e7fa1c1eb7cb7b858a030c169e2b38ab35f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-27 22:42:28 +0530

    added new code

--- mit/addednum/adnum.java
+++ mit/addednum/adnum.java
@@ -1,34 +1,22 @@
 package mit.addednum;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 public class adnum {
     public static int findAddedValue(int n,int[] A,int[] G){
-        Map<Integer,Integer> countMap=new HashMap<>();
+        Arrays.sort(A);
+        Arrays.sort(G);
 
-        for(int num:A){
-            countMap.put(num,countMap.getOrDefault(num,0)+1);
-        }
+        int addedValue = 0;
 
-        for(int num:G){
-            countMap.put(num,countMap.get(num)-1);
-        }
-
-        int removeElement1=0;
-        int removeElement2=0;
-        for(Map.Entry<Integer,Integer>entry:countMap.entrySet()){
-            if(entry.getValue()>0){
-                if(removeElement1==0){
-                    removeElement1=entry.getKey();
-                } else {
-                    removeElement2=entry.getKey();
-                    break;
-                }
+        for (int i = 0; i < n - 1; i++) {
+            if (A[i] != G[i]) {
+                addedValue = G[i] - A[i];
+                break;
             }
         }
 
-        int addedValue=Math.min(removeElement1,removeElement2);
         return addedValue;
+
+        
     }
     public static void main(String args[]){
         int n=4;
