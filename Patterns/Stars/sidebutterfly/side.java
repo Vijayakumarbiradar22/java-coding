d681e0da985b429c20139bd693aea69dcd062eb9
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-29 22:27:02 +0530

    added new code

--- Patterns/Stars/sidebutterfly/side.java
+++ Patterns/Stars/sidebutterfly/side.java
@@ -5,7 +5,12 @@ public class side {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the rows:");
         int n=sc.nextInt();
-        
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
     
