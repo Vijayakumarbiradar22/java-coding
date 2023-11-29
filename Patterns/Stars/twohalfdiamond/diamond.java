ab590ee88d18ebd67da805a8a10ccef0f5ba5fec
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-29 22:10:40 +0530

    added new code

--- Patterns/Stars/twohalfdiamond/diamond.java
+++ Patterns/Stars/twohalfdiamond/diamond.java
@@ -1,8 +1,11 @@
 package Patterns.Stars.twohalfdiamond;
-
+import java.util.Scanner;
 public class diamond {
     public static void main(String rgs[]){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
+
     }
     
 }
