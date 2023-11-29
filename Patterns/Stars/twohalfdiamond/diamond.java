52edafbbc7edcd9881f7dec74a02b55c83178149
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-29 22:40:27 +0530

    added new code

--- Patterns/Stars/twohalfdiamond/diamond.java
+++ Patterns/Stars/twohalfdiamond/diamond.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class diamond {
     public static void main(String rgs[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the rows:");
+        System.out.println("Enter the no.of rows:");
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
