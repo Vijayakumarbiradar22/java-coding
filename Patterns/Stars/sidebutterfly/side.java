e140eba1009dc499277120ee94f4707aa3aa6ed6
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-29 22:24:43 +0530

    added new code

---
+++ Patterns/Stars/sidebutterfly/side.java
@@ -0,0 +1,12 @@
+package Patterns.Stars.sidebutterfly;
+import java.util.Scanner;
+public class side {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
+        
+
+    }
+    
+}
