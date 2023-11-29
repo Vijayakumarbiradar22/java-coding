8a9845a5418a0edd593bf14d96b98030b94a23b4
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-29 22:14:29 +0530

    added new code

--- Patterns/Stars/twohalfdiamond/diamond.java
+++ Patterns/Stars/twohalfdiamond/diamond.java
@@ -11,7 +11,12 @@ public class diamond {
             }
             System.out.println();
         }
-        
+        for(int i=n;i>=0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
     
 }
