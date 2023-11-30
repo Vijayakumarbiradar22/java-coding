5588caed035db8db6e71759018b974ce34da0f57
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-30 22:23:06 +0530

    added new code

--- Patterns/Stars/leftdiamond/left.java
+++ Patterns/Stars/leftdiamond/left.java
@@ -16,7 +16,7 @@ public class left {
             System.out.println();
         }
 
-        for(int i=n;i>=1;i--){
+        for(int i=n-1;i>=1;i--){
             for(int s=1;s<=n-i;s++){
                 System.out.print(" ");
             }
