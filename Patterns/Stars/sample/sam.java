4ff3367fc26df60321f4342375d694d55659c653
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-30 22:32:21 +0530

    added new code

---
+++ Patterns/Stars/sample/sam.java
@@ -0,0 +1,27 @@
+package Patterns.Stars.sample;
+import java.util.Scanner;
+public class sam {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no.of roes:");
+        int n=sc.nextInt();
+         for(int i=1;i<=n;i++){
+            
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+         }
+
+         for(int i=1;i<=n;i++){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+         }
+    }
+    
+}
