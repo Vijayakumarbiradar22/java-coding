c9adefec9c397fdb848904656fb29cc96f8d0a6a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-11-30 22:19:09 +0530

    added new code

---
+++ Patterns/Stars/leftdiamond/left.java
@@ -0,0 +1,31 @@
+package Patterns.Stars.leftdiamond;
+import java.util.Scanner;
+public class left {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n;i>=1;i--){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
