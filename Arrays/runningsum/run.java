64344bddc6c7a079613dd489047a0d9d2438ce84
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-01 07:42:12 +0530

    added new code

---
+++ Arrays/runningsum/run.java
@@ -0,0 +1,24 @@
+package Arrays.runningsum;
+import java.util.Scanner;
+public class run {
+    public static int[] findRunningSum(int num[],int n){
+        int[] result=new int[n];
+        result[0]=num[0];
+        for(int i=1;i<n;i++){
+            result[i]=num[i]+result[i-1];
+        }
+        return result;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+        int num[]=new int[n];
+        System.out.println("Enter the elements of array:");
+        for(int i=0;i<n;i++){
+            num[i]=sc.nextInt();
+        }
+        findRunningSum(num,n);
+    }
+    
+}
