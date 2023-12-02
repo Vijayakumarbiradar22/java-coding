ae798198fe29a9a6e15c40805d30ebf70679efa1
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-02 22:17:16 +0530

    added new code

---
+++ Arrays/targetsum/target.java
@@ -0,0 +1,34 @@
+package Arrays.targetsum;
+import java.util.Scanner;
+import java.util.*;
+public class target {
+    public static int[] twoSum(int[] nums, int target,int n){
+        Map<Integer,Integer> map=new HashMap<>();
+        int[] result=new int[2];
+        for(int i=0;i<n;i++){
+            if(map.containsKey(target-nums[i])){
+                result[1]=i;
+                result[0]=map.get(target-nums[i]);
+                return result;
+            }
+            map.put(nums[i],i);
+        }
+        return result;
+
+    }
+    public static void main(String rgs[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+        int[] nums=new int[n];
+        System.out.println("Enter the elements:");
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        System.out.println("Enter the target num:");
+        int target=sc.nextInt();
+
+        twoSum(nums,target,n);
+    }
+    
+}
