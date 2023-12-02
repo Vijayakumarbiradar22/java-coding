fb2b854df6fa24a8ea5e85c3d8f8a3869eb2f60a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-02 22:25:50 +0530

    added new code

--- Patterns/Numbers/reversenum/rev.java
+++ Patterns/Numbers/reversenum/rev.java
@@ -2,11 +2,18 @@ package Patterns.Numbers.reversenum;
 import java.util.Scanner;
 public class rev {
     public static void main(String args[]){
+        int num=10;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the rows:");
         int n=sc.nextInt();
 
-        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num--;
+            }
+            System.out.println();
+        }
     }
     
 }
