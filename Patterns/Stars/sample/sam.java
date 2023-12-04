c6bc1fd60017ba6c3d388bbc3ca01dc3f3514c52
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-04 22:30:24 +0530

    added new code

--- Patterns/Stars/sample/sam.java
+++ Patterns/Stars/sample/sam.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class sam {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no.of roes:");
+        System.out.println("Enter the no.of rows:");
         int n=sc.nextInt();
          for(int i=1;i<=n;i++){
             
