c3b03ce6bc1e880a27df86911fcb46f03db876b2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-04 22:19:51 +0530

    added new code

--- Patterns/Stars/oddstar/ostar.java
+++ Patterns/Stars/oddstar/ostar.java
@@ -6,7 +6,10 @@ public class ostar {
         System.out.println("Enter the rows:");
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;i<=2*i-1;j++){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
                 System.out.print("*");
             }
             System.out.println();
