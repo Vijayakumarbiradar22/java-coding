78bbbe64555839a9e3d2e62429985af066335ca7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-04 22:16:25 +0530

    added new code

---
+++ Patterns/Stars/oddstar/ostar.java
@@ -0,0 +1,11 @@
+package Patterns.Stars.oddstar;
+import java.util.Scanner;
+public class ostar {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n = sc.nextInt();
+        
+    }
+    
+}
