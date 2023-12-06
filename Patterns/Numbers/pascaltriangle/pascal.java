f933d2f6b36991c0f969509f860b1461278a180e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-06 22:14:00 +0530

    added new code

--- Patterns/Numbers/pascaltriangle/pascal.java
+++ Patterns/Numbers/pascaltriangle/pascal.java
@@ -2,7 +2,15 @@ package Patterns.Numbers.pascaltriangle;
 import java.util.Scanner;
 public class pascal {
     public static void main(String args[]){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=n;j++){
+                System.out.print(fact(i)/(fact(j)*fact(i-j)));
+            }
+            System.out.println();
+        }
     }
     
 }
