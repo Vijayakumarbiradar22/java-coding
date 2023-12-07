53de737c71883ae5cf6d895e26b9b570f5a4bb10
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-07 21:50:45 +0530

    added new code

--- Numbers/binomialcoef/binomial.java
+++ Numbers/binomialcoef/binomial.java
@@ -1,6 +1,13 @@
 package Numbers.binomialcoef;
 import java.util.Scanner;
 public class binomial {
+    public static int fact(int n){
+        int factorial=1;
+        for(int i=2;i<n;i++){
+            factorial=factorial*i;
+        }
+        return factorial;
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the rows:");
