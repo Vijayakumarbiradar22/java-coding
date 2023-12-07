ed09aca07430b08c60e36d982521c8fc8e6e6189
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-07 22:04:02 +0530

    added new code

--- Numbers/nCr/combi.java
+++ Numbers/nCr/combi.java
@@ -1,5 +1,15 @@
 package Numbers.nCr;
-
+import java.util.Scanner;
 public class combi {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n:");
+        int n=sc.nextInt();
+        System.out.println("Enter r:");
+        int r=sc.nextInt();
+
+        int result=fact(n)/(fact(r)*fact(n-r));
+        System.out.println(result);
+    }
     
 }
