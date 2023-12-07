df8e243e7f2820e3d28df23021e489e3d15b5393
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-07 22:05:28 +0530

    added new code

--- Numbers/nCr/combi.java
+++ Numbers/nCr/combi.java
@@ -1,6 +1,13 @@
 package Numbers.nCr;
 import java.util.Scanner;
 public class combi {
+    public static int fact(int l){
+        int factorial=1;
+        for(int i=2;i<l;i++){
+            factorial*=i;
+        }
+        return factorial;
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter n:");
