457ce039c18c7156c32260338062ea3ed00dc26c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-07 21:54:54 +0530

    added new code

--- Numbers/binomialcoef/binomial.java
+++ Numbers/binomialcoef/binomial.java
@@ -13,7 +13,7 @@ public class binomial {
         System.out.println("Enter the rows:");
         int n=sc.nextInt();
         for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
+            for(int j=1;j<=i;j++){
                 System.out.print(fact(i)/(fact(j)*fact(i-j)));
             }
             System.out.println();
