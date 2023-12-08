efacdaf8a17a338157d6481e0a2f5cde853245e4
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-08 22:03:27 +0530

    added new code

---
+++ Bitmanupulation/updatebit/ub.java
@@ -0,0 +1,11 @@
+package Bitmanupulation.updatebit;
+import java.util.Scanner;
+public class ub {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the oper:");
+        int oper=sc.nextInt(); //if oper is 1 then set and if oper is 0 then clear
+        
+    }
+    
+}
