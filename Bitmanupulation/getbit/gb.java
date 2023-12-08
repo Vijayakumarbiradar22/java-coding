5f5bd020bbd97e81d35d2e8344c2070fccaeb689
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-08 21:52:41 +0530

    added new code

---
+++ Bitmanupulation/getbit/gb.java
@@ -0,0 +1,16 @@
+package Bitmanupulation.getbit;
+
+public class gb {
+    public static void main(String args[]){
+        int n=5;
+        int pos=2;
+        int bitMask=1<<pos;
+
+        if((bitMask&n)==0){
+            System.out.println("Bit was zero");
+        } else {
+            System.out.println("Bit was one");
+        }
+    }
+    
+}
