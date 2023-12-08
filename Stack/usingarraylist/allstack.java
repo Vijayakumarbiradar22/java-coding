ad590b920dce5895559b25456b526d29a363907d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-08 19:51:12 +0530

    added new code

--- Stack/usingarraylist/allstack.java
+++ Stack/usingarraylist/allstack.java
@@ -1,46 +1,46 @@
 package Stack.usingarraylist;
 import java.util.*;
 public class allstack {
-     static class Stack{
+    static class Stack{
         static ArrayList<Integer> list = new ArrayList<>();
-        public static boolean isEmpty() {
-            return list.size()==0;
+        
+        public static boolean isEmpty(){
+            return list.size()==0;   
         }
 
-        public static void push(int data){
+        public void push(int data){
             list.add(data);
         }
 
-        public static int pop(){
+        public int pop(){
             if(isEmpty()){
                 return -1;
             }
-            int top=list.get(list.size()-1);
+            int top = list.get(list.size()-1);
             list.remove(list.size()-1);
             return top;
         }
 
-        public static int peek(){
+        public int peek(){
             if(isEmpty()){
                 return -1;
             }
             return list.get(list.size()-1);
         }
+
     }
     public static void main(String args[]){
-         Stack s = new Stack();
-            s.push(1);
-            s.push(2);
-            s.push(3);
-            s.push(4);
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
 
-            while(s.isEmpty()){
-                System.out.println(s.peek());
-                s.peek();
+        while(s.isEmpty()){
+            System.out.println(s.peek());
+            s.peek();
             }
 
     }
-   
-
     
 }
