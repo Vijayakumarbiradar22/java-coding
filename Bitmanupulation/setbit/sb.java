c6dad37dfc374b2c7b3e9c3babc55e25542b2139
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-08 21:55:30 +0530

    added new code

--- Bitmanupulation/setbit/sb.java
+++ Bitmanupulation/setbit/sb.java
@@ -2,7 +2,12 @@ package Bitmanupulation.setbit;
 
 public class sb {
     public static void main(String args[]){
-        
+       int n=5;
+       int pos=1;
+       int bitMask=1<<pos;
+       
+       int newNumber=bitMask|n;
+       System.out.println(newNumber);
     }
     
 }
