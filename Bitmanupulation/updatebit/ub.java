91edbf2684f1f547675be19db59fe07664872d72
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-08 22:05:25 +0530

    added new code

--- Bitmanupulation/updatebit/ub.java
+++ Bitmanupulation/updatebit/ub.java
@@ -5,7 +5,18 @@ public class ub {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the oper:");
         int oper=sc.nextInt(); //if oper is 1 then set and if oper is 0 then clear
-        
+        int n=5;
+        int pos=2;
+        int bitMask=1<<pos;
+
+        if(oper==1){
+            int newNumber=bitMask|n;
+            System.out.println(newNumber);
+        } else {
+            int newBitmask=~(bitMask);
+            int newNumber=newBitmask&n;
+            System.out.println(newNumber);
+        }
     }
     
 }
