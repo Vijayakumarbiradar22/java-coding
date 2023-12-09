cead5c4acc69e471056a2e39d146fb60fcdeb2a7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-09 11:27:35 +0530

    added new code

---
+++ Tree/preorder/pre.java
@@ -0,0 +1,48 @@
+package Tree.preorder;
+
+public class pre {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    static class BinaryTree{
+        static int indx=-1;
+        public Node buildTree(int nodes[]){
+            indx++;
+            if(nodes[indx]==-1){
+                return null;
+            }
+
+            Node newNode=new Node(nodes[indx]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void main(String args[]){
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+
+        Node root=tree.buildTree(nodes);
+        preorder(root);
+
+    }
+    
+}
