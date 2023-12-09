77f5af88bfa2e0bfdf8670eadf89c3ff26c3c60a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-09 14:44:50 +0530

    added new code

---
+++ Tree/countofnodes/countnodes.java
@@ -0,0 +1,48 @@
+package Tree.countofnodes;
+
+public class countnodes {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    
+    static class BinaryTree{
+        static int indx=-1;
+        public Node buildTree(int nodes[]){
+            indx++;
+            if(nodes[indx]==-1){
+                return null;
+            }
+
+            Node newNode= new Node(nodes[indx]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static int countofnodes(Node root){
+        if(root==null){
+            return 0;
+        }
+        int leftNodes=countofnodes(root.left);
+        int rightNodes=countofnodes(root.right);
+        return leftNodes+rightNodes+1;
+    }
+
+    public static void main(String args[]){
+        int Nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root=tree.buildTree(Nodes);
+        System.out.println(countofnodes(root));
+
+    }
+    
+}
