52321a359cc24513d35392e3b67f24d04c0dca25
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-09 14:25:12 +0530

    added new code

--- Tree/levelorder/level.java
+++ Tree/levelorder/level.java
@@ -1,5 +1,5 @@
 package Tree.levelorder;
-
+import java.util.*;
 public class level {
     static class Node{
         int data;
@@ -29,15 +29,36 @@ public class level {
     }
 
     public static void levelorder(Node root){
-        
+      Queue<Node> q=new LinkedList<>(); 
+      q.add(root);
+      q.add(null);
+      
+      while(!q.isEmpty()){
+        Node currNode=q.remove();
+        if(currNode==null){
+            System.out.println();
+            if(q.isEmpty()){
+                break;
+            } else {
+                q.add(null);
+            }
+        } else {
+            System.out.print(currNode.data+" ");
+            if(currNode.left!=null){
+                q.add(currNode.left);
+            }
+            if(currNode.right!=null){
+                q.add(currNode.right);
+            }
+        }
+      }
     }
     public static void main(String args[]){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
 
         Node root=tree.buildTree(nodes);
-
-
+        levelorder(root);
     }
     
 }
