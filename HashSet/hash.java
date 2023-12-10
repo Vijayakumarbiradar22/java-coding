abbd5d5b284f96e1d7a3c360bf9d803745b8f2c7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-10 12:11:21 +0530

    added new code

--- HashSet/hash.java
+++ HashSet/hash.java
@@ -3,6 +3,7 @@ import java.util.HashSet;
 public class hash {
     public static void main(String args[]){
         HashSet<Integer> set = new HashSet<>();
+        //Insert
         set.add(1);
         set.add(2);
         set.add(3);
@@ -10,6 +11,15 @@ public class hash {
 
         System.out.println(set);
 
+        //search
+        if(set.contains(1)){
+            System.out.println("Set has 1");
+        }
+        if(!set.contains(7)){
+            System.out.println("Set does not contains 7");
+        }
+
+        //delete
         
     }
     
