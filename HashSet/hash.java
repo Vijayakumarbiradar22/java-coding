2c3f13803def5feeb4d9c52cc50644f3d3a02089
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-10 12:08:42 +0530

    added new code

--- HashSet/hash.java
+++ HashSet/hash.java
@@ -3,6 +3,12 @@ import java.util.HashSet;
 public class hash {
     public static void main(String args[]){
         HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(2);
+
+        System.out.println(set);
     }
     
 }
