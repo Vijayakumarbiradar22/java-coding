558b26d74e5426bad38fddbcd5f1e56f0e1f1092
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-11 19:40:39 +0530

    added new code

--- Hashing/itenerary/iten.java
+++ Hashing/itenerary/iten.java
@@ -1,12 +1,34 @@
 package Hashing.itenerary;
 import java.util.*;
 public class iten {
+    public static String getStart(HashMap<String,String> tick){
+        HashMap<String,String> revMap=new HashMap<>();
+
+        for(String key:tick.keySet()){
+            revMap.put(tick.get(key),key);
+        }
+
+        for(String key:tick.keySet()){
+            if(!revMap.containsKey(key)){
+                return key;
+            }
+        }
+        return null;
+    }
     public static void main(String args[]){
         HashMap<String,String> tickets=new HashMap<>();
         tickets.put("Chennai","Bengalore");
         tickets.put("Mumbai","Delhi");
         tickets.put("Goa","Chennai");
         tickets.put("Delhi","Goa");
+
+        String start=getStart(tickets);
+
+        while(tickets.containsKey(start)){
+            System.out.println(start);
+            start=tickets.get(start);
+        }
+        System.out.println(start);
     }
     
 }
