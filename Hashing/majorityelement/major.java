11822b3aaf4b1516355d6c8ecb8ddb6370a80949
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-11 18:42:21 +0530

    added new code

--- Hashing/majorityelement/major.java
+++ Hashing/majorityelement/major.java
@@ -7,20 +7,21 @@ public class major {
 
         for(int i=0;i<n;i++){
             if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i]+1));
+                map.put(nums[i],map.get(nums[i])+1);
             } else {
                 map.put(nums[i],1);
             }
         }
 
         for(int key:map.keySet()){
-            if(key>n/3){
+            if(map.get(key)>n/3){
                 System.out.println(key);
             }
         }
     }
     public static void main(String args[]){
         int nums[]={1,3,2,5,1,3,1,5,1};
+        //int nums[]={1,2};
         majorityElement(nums);
     }
     
