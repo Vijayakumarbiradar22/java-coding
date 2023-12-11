f1502211ef592ba10ef231f2d8c3a765de5c0d36
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-11 19:33:44 +0530

    added new code

---
+++ Hashing/itenerary/iten.java
@@ -0,0 +1,12 @@
+package Hashing.itenerary;
+import java.util.*;
+public class iten {
+    public static void main(String args[]){
+        HashMap<String,String> tickets=new HashMap<>();
+        tickets.put("Chennai","Bengalore");
+        tickets.put("Mumbai","Delhi");
+        tickets.put("Goa","Chennai");
+        tickets.put("Delhi","Goa");
+    }
+    
+}
