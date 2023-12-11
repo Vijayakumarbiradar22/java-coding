2753209b7f2b6a60b573fecd743c72f625ac7529
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-11 18:56:09 +0530

    added new code

--- Hashing/unionofarray/union.java
+++ Hashing/unionofarray/union.java
@@ -1,8 +1,22 @@
 package Hashing.unionofarray;
-
+import java.util.*;
 public class union {
-    public static void main(String args[]){
-        
+    public static int findUnion(int arr1[],int arr2[]){
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<arr1.length;i++){
+            set.add(arr1[i]);
+        }
+
+        for(int i=0;i<arr2.length;i++){
+            set.add(arr2[i]);
+        }
+
+        return set.size();
     }
+    public static void main(String args[]){
+        int arr1[]={7,3,9};
+        int arr2[]={6,3,9,2,9,4};
+        System.out.println(findUnion(arr1,arr2));
+    }   
     
 }
