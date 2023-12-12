0cecfb6c776c21f9fc7615f8c2e42cc64c34183e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-12 19:37:30 +0530

    added new code

---
+++ Sorting/quicksort/quick.java
@@ -0,0 +1,20 @@
+package Sorting.quicksort;
+import java.util.Scanner;
+public class quick {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the elements:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        quicksort(arr,0,n-1);
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
