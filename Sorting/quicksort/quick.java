25f7683b4847d554ac20c59940a61463d2e9d096
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-12 19:45:18 +0530

    added new code

--- Sorting/quicksort/quick.java
+++ Sorting/quicksort/quick.java
@@ -1,6 +1,33 @@
 package Sorting.quicksort;
 import java.util.Scanner;
 public class quick {
+    public static void quicksort(int arr[],int low,int high){
+        if(low<high){
+            int pindx=partition(arr,low,high);
+
+            quicksort(arr,low,pindx-1);
+            quicksort(arr,pindx+1,high);
+        }
+    }
+
+    public static int partition(int arr[],int low,int high){
+        int pivot=high;
+        int i=low-1;
+
+        for(int j=low;j<high;j++){
+            if(arr[j]<arr[pivot]){
+                i++;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        i++;
+        int temp=arr[i];
+        arr[i]=arr[pivot];
+        arr[high]=temp;
+        return i;
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
