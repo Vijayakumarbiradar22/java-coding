2d269913c1b0f8d6dd36c09b3d294778b2d5d3f8
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-12 22:14:04 +0530

    added new code

--- Sorting/insertionsort/insertion.java
+++ Sorting/insertionsort/insertion.java
@@ -5,7 +5,7 @@ public class insertion {
         for(int i=1;i<n;i++){
             int current=arr[i];
             int j=i-1;
-            while(j>=0&&current<arr[j]){
+            while(j>=0 && current<arr[j]){
                 arr[j+1]=arr[j];
                 j--;
             }
