9f0b18babc36a349dca184ec5d92ae706593edaf
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-12 16:03:34 +0530

    added new code

--- Sorting/bubblesort/bubble.java
+++ Sorting/bubblesort/bubble.java
@@ -4,7 +4,7 @@ public class bubble {
     public static void bubbleSort(int array[], int size){
         int temp;
         for(int i=0;i<size;i++){
-            for(int j=i+1;j<=size;j++){
+            for(int j=i+1;j<size;j++){
                 if(array[i]>array[j]){
                     temp=array[i];
                     array[i]=array[j];
