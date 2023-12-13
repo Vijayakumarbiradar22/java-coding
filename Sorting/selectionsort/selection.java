f8ae34f0383aa9ecee9e531ae19c761e272a54a7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-13 21:52:34 +0530

    added new code

--- Sorting/selectionsort/selection.java
+++ Sorting/selectionsort/selection.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class selection {
     public static void main(String args[]){
         Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        
     }
     
 }
