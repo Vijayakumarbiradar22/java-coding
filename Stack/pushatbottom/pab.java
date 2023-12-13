2f7b317288b66bf470547592f5e09627c1a64dd7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-13 22:17:22 +0530

    added new code

---
+++ Stack/pushatbottom/pab.java
@@ -0,0 +1,12 @@
+package Stack.pushatbottom;
+import java.util.*;
+public class pab {
+    public static void main(String args[]){
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        
+    }
+    
+}
