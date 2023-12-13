7a5fbd04c8bf6ea3e209f4daf633ec5fdf0d83c5
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-13 21:55:16 +0530

    added new code

--- Sorting/selectionsort/selection.java
+++ Sorting/selectionsort/selection.java
@@ -9,7 +9,22 @@ public class selection {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        
+
+        for(int i=0;i<n-1;i++){
+            int smallest = i;
+            for(int j=i+1;j<n;j++){
+                if(arr[smallest]>arr[j]){
+                    smallest=j;
+                }
+            }
+            int temp=arr[smallest];
+            arr[smallest]=arr[i];
+            arr[i]=temp;
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
     
 }
