092511ba241ca8e74ef11362862e078014d96241
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-13 21:51:11 +0530

    added new code

---
+++ Sorting/selectionsort/selection.java
@@ -0,0 +1,8 @@
+package Sorting.selectionsort;
+import java.util.Scanner;
+public class selection {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+    }
+    
+}
