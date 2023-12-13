2242fdc46b736e8bbab355e3c548d7c150dfe674
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-13 22:02:35 +0530

    added new code

---
+++ Stack/reverse/rev.java
@@ -0,0 +1,11 @@
+package Stack.reverse;
+import java.util.*;
+public class rev {
+    public static void main(String args[]){
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+    }
+    
+}
