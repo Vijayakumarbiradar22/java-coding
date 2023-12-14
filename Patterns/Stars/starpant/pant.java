3cb6c083982b301482904e0c514642eef8e45b03
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-14 21:47:47 +0530

    added new code

--- Patterns/Stars/starpant/pant.java
+++ Patterns/Stars/starpant/pant.java
@@ -9,8 +9,8 @@ public class pant {
         int y=n;
 
         for(int i=1;i<=n;i++){
-            for(int j=1;j<2*n;j++){
-                if(j<x&&j>y){
+            for(int j=1;j<n*2;j++){
+                if(j>x&&j<y){
                     System.out.print(" ");
                 } else {
                     System.out.print("*");
