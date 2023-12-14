750700d4c853b63ac4f2ba74e8c7277b8ea38aa9
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-14 21:35:40 +0530

    added new code

--- Patterns/Alpha/reversealpha/rev.java
+++ Patterns/Alpha/reversealpha/rev.java
@@ -2,14 +2,14 @@ package Patterns.Alpha.reversealpha;
 
 public class rev {
     public static void main(String args[]){
+        
         char ch='G';
         for(char i='A';i<=ch;i++){
+             
             for(char j='A';j<=ch;j++){
                 System.out.print(j);
             }
-            // for(int s=1;s<2*i-2;s++){
-            //     System.out.print(" ");
-            // }
+        
             for(char k=ch;k>='A';k--){
                 System.out.print(k);
             }
