c1388371c408f3b4f3e8613ea67d42465bf7f87d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-14 19:44:25 +0530

    added new code

--- Stack/usingarraylist/allstack.java
+++ Stack/usingarraylist/allstack.java
@@ -30,16 +30,16 @@ public class allstack {
 
     }
     public static void main(String args[]){
-        Stack s = new Stack();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        s.push(4);
+        //Stack s = new Stack();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+        // s.push(4);
 
-        while(s.isEmpty()){
-            System.out.println(s.peek());
-            s.peek();
-            }
+        // while(s.isEmpty()){
+        //     System.out.println(s.peek());
+        //     s.peek();
+        //     }
 
     }
     
