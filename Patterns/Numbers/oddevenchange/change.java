883402344ab8f7095b18a3573a203f8e5564b2fa
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-14 22:16:15 +0530

    added new code

---
+++ Patterns/Numbers/oddevenchange/change.java
@@ -0,0 +1,9 @@
+package Patterns.Numbers.oddevenchange;
+import java.util.Scanner;
+public class change {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+}
