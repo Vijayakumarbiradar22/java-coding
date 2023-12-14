2ffc16dd91bd790d474fe86affe27f9387ed36b1
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-14 19:41:34 +0530

    added new code

--- Stack/usingcollection/stacks.java
+++ Stack/usingcollection/stacks.java
@@ -1,5 +1,11 @@
 package Stack.usingcollection;
-
+import java.util.*;
 public class stacks {
-    
+    public static void main(String args[]){
+    Stack<Integer> s = new Stack<>();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    }
 }
