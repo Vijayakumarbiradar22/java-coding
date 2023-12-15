7f089576ef79a658ad1f83bed5775c3f649336c0
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-15 10:44:41 +0530

    added new code

---
+++ Patterns/Numbers/hallowdiamond/halnum.java
@@ -0,0 +1,24 @@
+package Patterns.Numbers.hallowdiamond;
+import java.util.Scanner;
+public class halnum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*n-i;j++){
+                if(j==n+i-1 || j==n-i+1 || i==n){
+                    System.out.print("i");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
