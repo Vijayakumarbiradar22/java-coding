3fd99a3d450cd221debd9e61595d432ddf6d602d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-15 10:37:24 +0530

    added new code

--- Patterns/Numbers/diamondnumber/dinum.java
+++ Patterns/Numbers/diamondnumber/dinum.java
@@ -15,6 +15,16 @@ public class dinum {
             }
             System.out.println();
         }
+
+        for(int i=n;i>=1;i--){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
     }
     
 }
