46b07de75c7c9c31864796cc3f610ef4225ca555
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-15 10:34:29 +0530

    added new code

---
+++ Patterns/Numbers/diamondnumber/dinum.java
@@ -0,0 +1,20 @@
+package Patterns.Numbers.diamondnumber;
+import java.util.Scanner;
+public class dinum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    
+}
