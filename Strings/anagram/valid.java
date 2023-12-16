b57918e7dd5a49b8415cbe1e85e6f9e9961806bc
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-16 19:05:22 +0530

    added new code

--- Strings/anagram/valid.java
+++ Strings/anagram/valid.java
@@ -1,6 +1,19 @@
 package Strings.anagram;
 import java.util.Scanner;
 public class valid {
+    public static boolean isAnagram(String s, String t){
+        var chars=new int[26];
+        for(var c:s.toCharArray()) chars[c-'a']++;
+        for(var c:t.toCharArray()) chars[c-'a']--;
+
+        for(int i=0;i<26;i++){
+            if(chars[i]!=0){
+                return false;
+            }
+        }
+        return true;
+
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String s:");
