ba6e190525ffa3bd262b3010eeedce2e0a9df3ea
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-16 21:12:59 +0530

    added new code

---
+++ Strings/groupanagram/group.java
@@ -0,0 +1,17 @@
+package Strings.groupanagram;
+import java.util.Scanner;
+public class group {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+        String strs[]=new String[n];
+        System.out.println("Enter the array of strings:");
+        for(int i=0;i<n;i++){
+            strs[i]=sc.nextLine();
+        }
+
+        
+    }
+    
+}
