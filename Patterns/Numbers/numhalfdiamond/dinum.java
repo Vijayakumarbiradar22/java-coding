e9b07cb7770d176cccde3ac669df9aa7827515c6
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-16 22:03:23 +0530

    added new code

--- Patterns/Numbers/numhalfdiamond/dinum.java
+++ Patterns/Numbers/numhalfdiamond/dinum.java
@@ -2,6 +2,10 @@ package Patterns.Numbers.numhalfdiamond;
 import java.util.Scanner;
 public class dinum {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
+        
         
     }
     
