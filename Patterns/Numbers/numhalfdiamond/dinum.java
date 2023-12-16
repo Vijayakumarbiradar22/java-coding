39ab4fe80dd10f564c1d9f58cafaa34811df914a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-16 22:04:31 +0530

    added new code

--- Patterns/Numbers/numhalfdiamond/dinum.java
+++ Patterns/Numbers/numhalfdiamond/dinum.java
@@ -5,6 +5,13 @@ public class dinum {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the rows:");
         int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
         
         
     }
