346c4ef387fdbe1c7dd59906be17e53bca8ce1db
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-16 21:29:27 +0530

    added new code

--- Strings/groupanagram/group.java
+++ Strings/groupanagram/group.java
@@ -1,6 +1,26 @@
 package Strings.groupanagram;
 import java.util.Scanner;
+import java.util.*;
 public class group {
+    public static List<List<String>> groupAnagrams(String[] strs){
+        if(strs.length==0){
+            return new ArrayList<>();
+        }
+        Map<String,List<String>> map = new HashMap<>();
+
+        for(String i:strs){
+            char[] chars = i.toCharArray();
+            Arrays.sort(chars);
+
+            String sortedStr=String.valueOf(chars);
+
+            if(!map.containsKey(sortedStr)){
+                map.put(sortedStr,new ArrayList<>());
+            }
+            map.get(sortedStr).add(i);
+        }
+        return new ArrayList<>(map.values());
+        }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
@@ -10,7 +30,11 @@ public class group {
         for(int i=0;i<n;i++){
             strs[i]=sc.nextLine();
         }
-
+        List<List<String>> result = groupAnagrams(strs);
+        for(List<String> s:result){
+            System.out.println(s);
+        }
+        sc.close();
         
     }
     
