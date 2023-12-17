38f43f09d88c2794fb2a21dc981087f5d4be1377
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-17 13:09:45 +0530

    added new code

--- Neetcode/ArraysandHashing/longestconsequence/longseq.java
+++ Neetcode/ArraysandHashing/longestconsequence/longseq.java
@@ -4,7 +4,12 @@ import java.util.*;
 public class longseq {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("Enter the size of array:");
+        int n = sc.nextInt();
+        int nums[]=new int[n];
+        System.out.println("Enter the elements:");{
+
+        }
     }
     
 }
