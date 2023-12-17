bfd21486fde1cf85fa0092e3b7a46f030f67f7a3
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-17 11:49:16 +0530

    added new code

--- Neetcode/ArraysandHashing/topkelement/kfreq.java
+++ Neetcode/ArraysandHashing/topkelement/kfreq.java
@@ -20,6 +20,7 @@ public class kfreq {
         System.out.println("Enter the size of array:");
         int n=sc.nextInt();
         int nums[]=new int[n];
+        System.out.println("Enter the elements:");
         for(int i=0;i<n;i++){
             nums[i]=sc.nextInt();
         }
