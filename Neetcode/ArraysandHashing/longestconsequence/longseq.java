d8283548786bac95b1dce6522caa83b9beb9c0bb
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-17 13:07:51 +0530

    added new code

---
+++ Neetcode/ArraysandHashing/longestconsequence/longseq.java
@@ -0,0 +1,10 @@
+package Neetcode.ArraysandHashing.longestconsequence;
+import java.util.Scanner;
+import java.util.*;
+public class longseq {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+}
