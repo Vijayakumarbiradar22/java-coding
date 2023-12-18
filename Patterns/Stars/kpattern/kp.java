165d1593d7acc5668c90b5272eb85630062c8bad
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-18 21:56:15 +0530

    added new code

--- Patterns/Stars/kpattern/kp.java
+++ Patterns/Stars/kpattern/kp.java
@@ -5,6 +5,13 @@ public class kp {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the rows:");
         int n=sc.nextInt();
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
     
 }
