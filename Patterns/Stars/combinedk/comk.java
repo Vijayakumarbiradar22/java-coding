bd12737978655b8f28cbf8ecb2f6d126e14322d7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-19 19:53:02 +0530

    added new code

---
+++ Patterns/Stars/combinedk/comk.java
@@ -0,0 +1,43 @@
+package Patterns.Stars.combinedk;
+import java.util.Scanner;
+public class comk {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int s=1;s<n-i;s++){
+                System.out.print(" ");
+            }
+
+            for(int s=1;s<=n+1-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
