6896d4fce1daf6e338b37d1cf6c0b48902af8b53
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-19 19:37:27 +0530

    added new code

--- Patterns/Stars/invertkpattern/invertk.java
+++ Patterns/Stars/invertkpattern/invertk.java
@@ -2,7 +2,9 @@ package Patterns.Stars.invertkpattern;
 import java.util.Scanner;
 public class invertk {
     public static void main(String args[]){
-        
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the rows:");
+       int n=sc.nextInt(); 
     }
     
 }
