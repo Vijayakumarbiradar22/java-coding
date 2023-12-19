3e00d50e3db4078c5f1ce6ce8d61d2ef24feaa3b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-19 19:42:17 +0530

    added new code

--- Patterns/Stars/invertkpattern/invertk.java
+++ Patterns/Stars/invertkpattern/invertk.java
@@ -15,6 +15,17 @@ public class invertk {
         }
         System.out.println();
        }
+
+       for(int i=1;i<=n;i++){
+        for(int s=1;s<=n-i;s++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+       }
+
     }
     
 }
