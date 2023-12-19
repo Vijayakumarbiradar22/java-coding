d62d8fec0ea155a39cde88cad66b20694aedb400
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-19 20:19:48 +0530

    added new code

--- Patterns/Stars/combinedk/comk.java
+++ Patterns/Stars/combinedk/comk.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class comk {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no.of rows:");
+        System.out.println("Enter the number of rows:");
         int n=sc.nextInt();
         for(int i=n;i>=1;i--){
             for(int j=1;j<=i;j++){
