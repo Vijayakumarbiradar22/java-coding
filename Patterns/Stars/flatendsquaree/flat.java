a84cf2ede4bba789f85e3877c98b08d1317a9c0f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-20 21:20:21 +0530

    added new code

--- Patterns/Stars/flatendsquaree/flat.java
+++ Patterns/Stars/flatendsquaree/flat.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class flat {
     public static void main(String args[]){
        Scanner sc = new Scanner(System.in); 
-       System.out.println("Enter the rpws:");
+       System.out.println("Enter the rows:");
        int n=sc.nextInt();
        for(int i=1;i<=n;i++){
         for(int s=1;s<=n-i;s++){
