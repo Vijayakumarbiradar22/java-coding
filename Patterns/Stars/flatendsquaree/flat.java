7cdad5dc79ce25d1b82a02c7c231e6950ee1fa53
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-20 21:12:27 +0530

    added new code

--- Patterns/Stars/flatendsquaree/flat.java
+++ Patterns/Stars/flatendsquaree/flat.java
@@ -2,7 +2,7 @@ package Patterns.Stars.flatendsquaree;
 import java.util.Scanner;
 public class flat {
     public static void main(String args[]){
-        
+       Scanner sc = new Scanner(System.in); 
     }
     
 }
