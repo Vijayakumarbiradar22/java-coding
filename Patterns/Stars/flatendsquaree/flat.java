c157d966adccae17866bb0008a0e8e8c949e64b5
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-20 21:17:20 +0530

    added new code

--- Patterns/Stars/flatendsquaree/flat.java
+++ Patterns/Stars/flatendsquaree/flat.java
@@ -17,6 +17,12 @@ public class flat {
         }
         System.out.println();
        }
+
+       for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+       }
     }
     
 }
