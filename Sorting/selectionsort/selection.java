6af2efa272060192f426db64321333e6924f9e82
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-21 22:07:54 +0530

    added new code

--- Sorting/selectionsort/selection.java
+++ Sorting/selectionsort/selection.java
@@ -6,6 +6,7 @@ public class selection {
         System.out.println("Enter the size of array:");
         int n=sc.nextInt();
         int arr[]=new int[n];
+        System.out.println("Enter the elements of array:");
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
