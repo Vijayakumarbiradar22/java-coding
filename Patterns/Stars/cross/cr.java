fca485ceb68e05b4267c4c7fc817cb7e49762ed1
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-21 19:41:03 +0530

    added new code

---
+++ Patterns/Stars/cross/cr.java
@@ -0,0 +1,10 @@
+package Patterns.Stars.cross;
+import java.util.Scanner;
+public class cr {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
+        int n=sc.nextInt();
+    }
+    
+}
