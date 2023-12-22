c73054624445d9181e5a6640d96f6d0ea9ad6018
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-22 06:02:45 +0530

    added new code

--- Searchalgo/binarysearch/binary.java
+++ Searchalgo/binarysearch/binary.java
@@ -1,6 +1,24 @@
 package Searchalgo.binarysearch;
 import java.util.Scanner;
 public class binary {
+    public static int binarySearch(int nums[],int target){
+        int length=nums.length;
+        int left=0;
+        int right=length-1;
+
+        while(left<=right){
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            } else if (nums[mid]<target){
+                left=mid+1;
+            } else {
+                right=mid-1;
+            }
+        }
+        return -1;
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
