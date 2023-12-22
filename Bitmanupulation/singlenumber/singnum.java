5dddaf0aeb57c24d402e836134b93aed31937062
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-22 06:22:22 +0530

    added new code

---
+++ Bitmanupulation/singlenumber/singnum.java
@@ -0,0 +1,11 @@
+package Bitmanupulation.singlenumber;
+import java.util.Scanner;
+public class singnum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+        
+    }
+    
+}
