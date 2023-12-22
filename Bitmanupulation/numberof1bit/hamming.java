4d3c0ce2b249076d646b5c24db434c7ae7c8d479
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-22 06:33:59 +0530

    added new code

--- Bitmanupulation/numberof1bit/hamming.java
+++ Bitmanupulation/numberof1bit/hamming.java
@@ -1,5 +1,15 @@
 package Bitmanupulation.numberof1bit;
-
+import java.util.Scanner;
 public class hamming {
+    public static int hammingWeight(int n){
+        return Integer.bitCount(n);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the bits n:");
+        int n=sc.nextInt();
+        int res = hammingWeight(n);
+        System.out.println(res);
+    }
     
 }
