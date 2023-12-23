a840b1653a46d506ca3075a23afbc7ac7d030b09
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-23 21:38:33 +0530

    added new code

--- Bitmanupulation/bitcount/bit.java
+++ Bitmanupulation/bitcount/bit.java
@@ -1,10 +1,18 @@
 package Bitmanupulation.bitcount;
 import java.util.Scanner;
 public class bit {
+    public static int countOnes(int n){
+        
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number:");
         int n= sc.nextInt();
+
+        int count=countOnes(n);
+        System.out.println(count);
+
+
     }
     
 }
