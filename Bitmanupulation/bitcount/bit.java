088459c0d2c2b9a5e3bf5c2d786d0a59720c259c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-23 21:35:42 +0530

    added new code

--- Bitmanupulation/bitcount/bit.java
+++ Bitmanupulation/bitcount/bit.java
@@ -1,5 +1,9 @@
 package Bitmanupulation.bitcount;
-
+import java.util.Scanner;
 public class bit {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
     
 }
