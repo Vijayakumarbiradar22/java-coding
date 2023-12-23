f9b1ae23a2b710cd02ab3b8caca0372022def3b9
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-23 21:41:14 +0530

    added new code

--- Bitmanupulation/bitcount/bit.java
+++ Bitmanupulation/bitcount/bit.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class bit {
     public static int countOnes(int n){
       int bitMask=1;
-      int count=0;  
+      int count=0; 
+      while(bitMask<=n){
+        if((n & bitMask)== bitMask){
+            count++;
+        }
+        
+      } 
+      return count;
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
