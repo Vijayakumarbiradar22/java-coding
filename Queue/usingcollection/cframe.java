7f94dc1c0e043919c793dc278a792b5016b0bd1f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-24 09:59:48 +0530

    added new code

--- Queue/usingcollection/cframe.java
+++ Queue/usingcollection/cframe.java
@@ -5,6 +5,13 @@ public class cframe {
         Queue<Integer> q = new LinkedList<>();
         q.add(1);
         q.add(2);
+        q.add(3);
+        q.add(4);
+
+        while(q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
     }
     
 }
