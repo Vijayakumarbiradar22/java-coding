919f0c427953a8ee98d824e9216d840f6d5c85d2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-24 10:04:20 +0530

    added new code

---
+++ Queue/usingcollection/usingdequeue/UDQ.java
@@ -0,0 +1,10 @@
+package Queue.usingcollection.usingdequeue;
+import java.util.*;
+public class UDQ {
+    public static void main(String args[]){
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(1);
+        q.add(2);
+    }
+    
+}
