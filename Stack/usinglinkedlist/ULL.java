11ed4e2b14dcf4ee75d4b7193008ad34f7eb8538
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-24 09:47:34 +0530

    added new code

--- Stack/usinglinkedlist/ULL.java
+++ Stack/usinglinkedlist/ULL.java
@@ -1,8 +1,7 @@
 package Stack.usinglinkedlist;
 import java.util.*;
 public class ULL {
-    public static void main(String args[]){
-      static class Node {
+    static class Node {
         int data;
         Node next;
 
@@ -21,13 +20,32 @@ public class ULL {
         public static void push(int data){
           Node newNode =new Node(data);
           if(isEmpty()){
-             head==newNode;
+             head=newNode;
              return;
           }
           newNode.next=head;
           head=newNode;  
         }
+      
+
+      public static int pop(){
+        if(isEmpty()){
+            return -1;
+        }
+        int top=head.data;
+        head=head.next;
+        return top;
       }
+
+      public static int peek(){
+        if(isEmpty()){
+            return -1;
+        }
+        return head.data;
+      }
+    }
+    public static void main(String args[]){
+      
     }
     
 }
