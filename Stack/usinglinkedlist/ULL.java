b126c789d6c1f1ca4936ec0558d175946530ed16
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-24 09:35:17 +0530

    added new code

--- Stack/usinglinkedlist/ULL.java
+++ Stack/usinglinkedlist/ULL.java
@@ -2,7 +2,15 @@ package Stack.usinglinkedlist;
 import java.util.*;
 public class ULL {
     public static void main(String args[]){
-        
+      static class Node {
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+      }  
     }
     
 }
