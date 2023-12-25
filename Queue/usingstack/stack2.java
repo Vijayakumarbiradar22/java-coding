c8b4fde048c5d72bc1545529019b16d81acdbd1e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-25 21:21:31 +0530

    added new code

---
+++ Queue/usingstack/stack2.java
@@ -0,0 +1,18 @@
+package Queue.usingstack;
+import java.util.*;
+public class stack2 {
+    static class Queue {
+        static Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
+
+        public boolean isEmpty(){
+            return s1.isEmpty();
+        }
+
+        
+    }
+    public static void main(String args[]){
+
+    }
+    
+}
