9942e60478e0fb64c9a9f383630436100b48f45e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-25 20:47:49 +0530

    added new code

---
+++ Queue/usingarray/que.java
@@ -0,0 +1,25 @@
+package Queue.usingarray;
+//import java.util.*;
+public class que {
+    static int arr[];
+    static int size;
+    static int rear=-1;
+
+    que(int n){
+        arr=new int[n];
+        this.size=n;
+    }
+
+    public static boolean isEmpty(){
+        return rear==-1;
+    }
+
+    public static void add (int data){
+        
+    }
+
+    public static void main(String args[]){
+
+    }
+    
+}
