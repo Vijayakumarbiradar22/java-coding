fdd00e71a6527ff6a6d04f4b44095fb922575b42
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-25 21:54:37 +0530

    added new code

---
+++ Backtracking/permutation/perm.java
@@ -0,0 +1,11 @@
+package Backtracking.permutation;
+import java.util.Scanner;
+public class perm {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str=sc.nextLine();
+        
+    }
+    
+}
