8235eea80cdd9abeae40b9fdf2054fabc88091dc
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-25 22:04:22 +0530

    added new code

--- Backtracking/permutation/perm.java
+++ Backtracking/permutation/perm.java
@@ -1,6 +1,18 @@
 package Backtracking.permutation;
 import java.util.Scanner;
 public class perm {
+    public static void printPerm(String str,String perm,int indx){
+        if(str.length()==0){
+            System.out.println(perm);
+            return;
+        }
+
+        for(int i=0;i<str.length();i++){
+            char currChar=str.charAt(i);
+            String newStr=str.subString(0,i)+str.subString(i+1);
+            printPerm(newStr,perm+currChar,indx+1);
+        }
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the string:");
