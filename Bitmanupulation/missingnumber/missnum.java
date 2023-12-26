58fb61457330a5471e6b55ffe3fd2961c4df01bc
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-26 20:56:48 +0530

    added new code

--- Bitmanupulation/missingnumber/missnum.java
+++ Bitmanupulation/missingnumber/missnum.java
@@ -5,7 +5,11 @@ public class missnum {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
         int n = sc.nextInt();
-        
+        int nums[]=new int[n];
+        System.out.println("Enter the elements:");
+        for(int i=0;i<n;i++){
+
+        }
     }
     
 }
