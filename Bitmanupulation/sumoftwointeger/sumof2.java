8d3daeb803ea6acbef88fe5273b9039910638ceb
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-26 21:10:30 +0530

    added new code

---
+++ Bitmanupulation/sumoftwointeger/sumof2.java
@@ -0,0 +1,9 @@
+package Bitmanupulation.sumoftwointeger;
+import java.util.Scanner;
+public class sumof2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+}
