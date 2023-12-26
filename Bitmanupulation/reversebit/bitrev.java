f4ea8f6470db5e14fbcabe72b8cd0b08ede2162b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-26 20:44:39 +0530

    added new code

--- Bitmanupulation/reversebit/bitrev.java
+++ Bitmanupulation/reversebit/bitrev.java
@@ -2,7 +2,13 @@ package Bitmanupulation.reversebit;
 import java.util.Scanner;
 public class bitrev {
     public static int reverseBit(int n){
-        
+        int ans=0;
+        for(int i=0;i<32;i++){
+            ans<<=1;
+            ans |=(n&1);
+            n>>=1;
+        }
+        return ans;
     }
     public static void  main(String args[]){
        Scanner sc = new Scanner(System.in);
