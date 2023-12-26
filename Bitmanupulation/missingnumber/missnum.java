93b16d727993806ec81a2d04b35572217cb1f4bd
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-26 20:57:29 +0530

    added new code

--- Bitmanupulation/missingnumber/missnum.java
+++ Bitmanupulation/missingnumber/missnum.java
@@ -8,7 +8,7 @@ public class missnum {
         int nums[]=new int[n];
         System.out.println("Enter the elements:");
         for(int i=0;i<n;i++){
-
+            nums[i]=sc.nextInt();
         }
     }
     
