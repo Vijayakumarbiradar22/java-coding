403f42331036acc4d6f19fd3bd64c9764f5b804d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-27 21:26:57 +0530

    added new code

--- Neetcode/twopointers/sellstock/stock.java
+++ Neetcode/twopointers/sellstock/stock.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class stock {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n = sc.nextInt();
+        int nums[]=new int[n];
+        System.out.println("Enter the elements:");
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
         
     }
     
