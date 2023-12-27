014a680391a26b38251abb0de358f6080fbf739f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-27 21:25:01 +0530

    added new code

---
+++ Neetcode/twopointers/sellstock/stock.java
@@ -0,0 +1,9 @@
+package Neetcode.twopointers.sellstock;
+import java.util.Scanner;
+public class stock {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+}
