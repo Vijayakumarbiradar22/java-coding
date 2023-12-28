2de1c4943f5dc75f1104dc9f4ce0a0b94ffcb426
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-28 22:26:58 +0530

    added new code

--- Patterns/Numbers/negativenumber/negative.java
+++ Patterns/Numbers/negativenumber/negative.java
@@ -4,7 +4,7 @@ public class negative {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int num=-1;
-        System.out.println("Enter the  row:");
+        System.out.println("Enter the number of row:");
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
