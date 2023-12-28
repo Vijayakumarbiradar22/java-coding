16a9ebcf9dbae73b5b9ee603ae6385ca09f4cf95
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-28 22:17:23 +0530

    added new code

---
+++ Patterns/Numbers/negativenumber/negative.java
@@ -0,0 +1,9 @@
+package Patterns.Numbers.negativenumber;
+import java.util.Scanner;
+public class negative {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+}
