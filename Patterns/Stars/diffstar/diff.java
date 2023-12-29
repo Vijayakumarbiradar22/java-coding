ca00ff2e64b84e081dea1d10d432ad27164d18be
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-29 23:31:06 +0530

    added new code

--- Patterns/Stars/diffstar/diff.java
+++ Patterns/Stars/diffstar/diff.java
@@ -6,7 +6,10 @@ public class diff {
         System.out.println("Enter the rows:");
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
-            
+          for(int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+          }
+          System.out.println();  
         }
     }
     
