63885fa45ede0fdf9e568514288b128d9c49285e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-29 23:36:41 +0530

    added new code

--- Patterns/Stars/diffstar/diff.java
+++ Patterns/Stars/diffstar/diff.java
@@ -9,8 +9,7 @@ public class diff {
             for(int s=1;s<=n-i;s++){
                 System.out.print(" ");
             }
-          for(int j=1;j<=2*i-1;j++){
-            
+          for(int j=1;j<=2*i-1;j++){ 
             System.out.print("*");
           }
           System.out.println();  
