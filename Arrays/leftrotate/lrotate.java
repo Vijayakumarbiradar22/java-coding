95fd28410242effdc8b601103f69d6263f43d8a9
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-29 23:27:11 +0530

    added new code

--- Arrays/leftrotate/lrotate.java
+++ Arrays/leftrotate/lrotate.java
@@ -14,7 +14,7 @@ public class lrotate {
     }
     public static void main(String args[]){
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the array size:");
+            System.out.println("Enter the size of array:");
             int size=sc.nextInt();
             int[] arr = new int[size];
             System.out.println("Enter the array elements:");
