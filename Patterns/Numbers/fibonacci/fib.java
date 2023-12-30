8d2c4cb76f31f0da4da221fff0459513c56bd7a8
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-30 20:22:33 +0530

    added new code

--- Patterns/Numbers/fibonacci/fib.java
+++ Patterns/Numbers/fibonacci/fib.java
@@ -28,7 +28,7 @@ public class fib {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the rows:");
+        System.out.println("Enter the total rows:");
         int n=sc.nextInt();
         printFib(n);
         sc.close();
