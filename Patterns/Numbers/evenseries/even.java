862d8b3a88042c0ecb84e8d011cf8ce35ff3a631
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-30 20:16:40 +0530

    added new code

--- Patterns/Numbers/evenseries/even.java
+++ Patterns/Numbers/evenseries/even.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class even {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows:");
         
     }
     
