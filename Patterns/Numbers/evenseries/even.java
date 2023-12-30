6368ef2c9a99667936d1c36e25585525b3b9b4f9
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-30 20:16:20 +0530

    added new code

--- Patterns/Numbers/evenseries/even.java
+++ Patterns/Numbers/evenseries/even.java
@@ -2,6 +2,7 @@ package Patterns.Numbers.evenseries;
 import java.util.Scanner;
 public class even {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         
     }
     
