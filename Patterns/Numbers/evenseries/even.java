4e986fe6d5cc938e8a48e4357895919af51e3875
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-30 20:17:18 +0530

    added new code

--- Patterns/Numbers/evenseries/even.java
+++ Patterns/Numbers/evenseries/even.java
@@ -4,7 +4,10 @@ public class even {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the rows:");
-        
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+
+        }
     }
     
 }
