622632d733fedb652c1083ba6d499c5e84bb1e6e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-30 20:21:57 +0530

    added new code

--- Patterns/Numbers/diamondnumber/dinum.java
+++ Patterns/Numbers/diamondnumber/dinum.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class dinum {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no.of rows:");
+        System.out.println("Enter the total rows:");
         int n=sc.nextInt();
 
         for(int i=1;i<=n;i++){
