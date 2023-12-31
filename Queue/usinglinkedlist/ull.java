bfa258de6e417f7086438488c19ef59078d4ba64
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-31 08:57:19 +0530

    added new code

--- Queue/usinglinkedlist/ull.java
+++ Queue/usinglinkedlist/ull.java
@@ -1,8 +1,19 @@
 package Queue.usinglinkedlist;
-import java.util.*;
+//import java.util.*;
 public class ull {
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            next=null;
+        }
+    }
+
+    
     public static void main(String args[]){
-        
+
     }
     
 }
