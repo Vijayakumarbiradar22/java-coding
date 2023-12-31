ff180017ce99fa5f0c8b19ee01a36bd6e8d74ffd
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2023-12-31 09:01:04 +0530

    added new code

--- Queue/usinglinkedlist/ull.java
+++ Queue/usinglinkedlist/ull.java
@@ -14,6 +14,11 @@ public class ull {
     static Node head=null;
     static Node tail=null;
 
+    public static boolean isEmpty(){
+        return head==null & tail==null;
+    }
+
+    //enqueue
     
     public static void main(String args[]){
 
