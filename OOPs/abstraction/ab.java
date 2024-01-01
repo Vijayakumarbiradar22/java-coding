7df719ca01d00affb7a585ad8d4b2e9eaa1ee8f1
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-01 19:26:26 +0530

    added new code

--- OOPs/abstraction/ab.java
+++ OOPs/abstraction/ab.java
@@ -13,10 +13,15 @@ package OOPs.abstraction;
     }
  }
 
- 
+ class Hen extends Animal{
+    public void walk(){
+        System.out.println("Walks on 2 leg.");
+    }
+ }
 
 public class ab {
     public static void main(String args[]){
+        
 
     }
     
