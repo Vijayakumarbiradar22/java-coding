bc2d359b7872cd0249e83d854b8e751801428511
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-01 19:36:18 +0530

    added new code

--- OOPs/classandobject/cao.java
+++ OOPs/classandobject/cao.java
@@ -1,8 +1,21 @@
 package OOPs.classandobject;
 
+class Pen {
+    String color;
+    String type;
+
+    public void write(){
+        System.out.println("Writing something");
+    }
+
+    public void printColor(){
+        System.out.println(this.color);
+    }
+}
+
 public class cao {
     public static void main(String args[]){
-        
+
     }
     
 }
