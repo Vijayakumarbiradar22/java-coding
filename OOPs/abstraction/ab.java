39e0618f18efc6f371d5e3b7e29adb4ddd55f1fe
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-01 19:24:23 +0530

    added new code

---
+++ OOPs/abstraction/ab.java
@@ -0,0 +1,17 @@
+package OOPs.abstraction;
+
+ abstract class Animal{
+    abstract void walk();
+    public void eats(){
+        System.out.println("Animal eats");
+    }
+ }
+
+ 
+
+public class ab {
+    public static void main(String args[]){
+
+    }
+    
+}
