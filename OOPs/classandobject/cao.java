29e756cbe4ba4a21e6796203beeaca5096520346
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-01 19:37:08 +0530

    added new code

--- OOPs/classandobject/cao.java
+++ OOPs/classandobject/cao.java
@@ -15,6 +15,11 @@ class Pen {
 
 public class cao {
     public static void main(String args[]){
+        Pen pen1 = new Pen();
+        pen1.color="blue";
+        pen1.type="gel";
+
+        
 
     }
     
