eb8c7822da5e9d9abcabe9e3a3bf674caad4356e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-01 19:08:30 +0530

    added new code

--- Patterns/Numbers/hallowdiamond/halnum.java
+++ Patterns/Numbers/hallowdiamond/halnum.java
@@ -7,9 +7,6 @@ public class halnum {
         int n=sc.nextInt();
 
         for(int i=1;i<=n;i++){
-            // for(int s=1;s<=n-i;s++){
-            //     System.out.print(" ");
-            // }
             for(int j=1;j<=2*n-1;j++){
                 if(j==n-i+1 || j==n+i-1 || i==n){
                     System.out.print(i+" ");
