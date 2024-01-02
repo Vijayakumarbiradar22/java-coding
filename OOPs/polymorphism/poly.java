1d0ab19e888ec0ad12108e7c138f5c5d980bd0fb
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-02 22:20:39 +0530

    added new code

--- OOPs/polymorphism/poly.java
+++ OOPs/polymorphism/poly.java
@@ -1,8 +1,25 @@
 package OOPs.polymorphism;
 
+class Student {
+    String name;
+    int age;
+
+    public void printInfo(String name){
+        System.out.println(name);
+    }
+
+    public void printInfo(int age){
+        System.out.println(age);
+    }
+
+    public void printInfo(String name, int age){
+        System.out.println(name+" "+" "+ age);
+    }
+}
+
 public class poly {
     public static void main(String args[]){
-        
+
     }
     
 }
