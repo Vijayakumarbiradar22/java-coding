ff888ed3d1ff39fb2bf7689cc204cd4c8e9bb992
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-02 22:07:36 +0530

    added new code

--- OOPs/encapsulation/bank.java
+++ OOPs/encapsulation/bank.java
@@ -1,8 +1,18 @@
 package OOPs.encapsulation;
 
+class Account {
+    public String name;
+    protected String email;
+    private String password;
+
+    public String getPassword(){
+        return this.password;
+    }
+}
+
 public class bank {
     public static void main(String args[]){
-        
+
     }
     
 }
