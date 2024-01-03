7cc91dfa69e2dd071907081fd9c9a32c6eeaae9b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-03 22:11:30 +0530

    added new code

--- Patterns/Stars/starpant/pant.java
+++ Patterns/Stars/starpant/pant.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class pant {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the rows:");
+        System.out.println("Enter the total rows:");
         int n=sc.nextInt();
         int x=n;
         int y=n;
