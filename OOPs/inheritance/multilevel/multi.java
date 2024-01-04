f2f8b330c8af045053f75281058a7a8f14e65cfb
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-04 21:28:51 +0530

    added new code

--- OOPs/inheritance/multilevel/multi.java
+++ OOPs/inheritance/multilevel/multi.java
@@ -13,9 +13,14 @@ class Triangle extends Shape{
 }
 
 class Equilateral extends Triangle{
-    
+    public void area(int l,int h){
+        System.out.println(1/2*l*h);
+    }
 }
 
 public class multi {
+    public static void main(String args[]){
+        
+    }
     
 }
