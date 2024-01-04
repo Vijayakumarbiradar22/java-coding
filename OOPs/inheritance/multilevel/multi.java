973188ca1bc8850005fcc37ef0cc000e1cd18122
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-04 21:27:31 +0530

    added new code

--- OOPs/inheritance/multilevel/multi.java
+++ OOPs/inheritance/multilevel/multi.java
@@ -2,8 +2,18 @@ package OOPs.inheritance.multilevel;
 
 class Shape{
     public void area(){
-        
+      System.out.println("Display area");  
     }
+}
+
+class Triangle extends Shape{
+    public void area(int l,int h){
+        System.out.println("Area="+1/2*l*h);
+    }
+}
+
+class Equilateral extends Triangle{
+    
 }
 
 public class multi {
