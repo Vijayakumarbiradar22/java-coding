6aff7d2c074000e52c083337afa335a2264d38c2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-05 19:55:01 +0530

    added new code

--- Recursion/stackheight/sh.java
+++ Recursion/stackheight/sh.java
@@ -1,6 +1,7 @@
 package Recursion.stackheight;
 import java.util.*;
 public class sh {
+    public static int calcPow
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the x:");
