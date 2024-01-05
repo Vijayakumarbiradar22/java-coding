686382c4b6adef4d2a1f641cba19ce58f18b54f5
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-05 19:46:09 +0530

    added new code

--- OOPs/staticeg/stat.java
+++ OOPs/staticeg/stat.java
@@ -11,7 +11,9 @@ class Student {
 
 public class stat {
     public static void main(String args[]){
-        
+        Student.school="SMVITM";
+        Student student1=new Student();
+       student1.name ="Vijay";
     }
     
 }
