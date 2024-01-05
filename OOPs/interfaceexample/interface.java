60384abfb2714f0a702ff7d4e5b7d68c821f17a1
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-05 19:38:27 +0530

    added new code

--- OOPs/interfaceexample/interface.java
+++ OOPs/interfaceexample/interface.java
@@ -9,9 +9,12 @@ interface Herbivore {
 }
 
 class Horse implements Animal,Herbivore {
-    
+  public void walk(){
+    System.out.println("Walks on 4 legs");
+  } 
 }
 
 public class interface {
+    
     
 }
