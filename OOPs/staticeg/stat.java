7fc590b4a28b8634639cddb6581c42510597d4f4
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-05 19:43:11 +0530

    added new code

--- OOPs/staticeg/stat.java
+++ OOPs/staticeg/stat.java
@@ -5,10 +5,13 @@ class Student {
     static String school;
 
     public static void changeSchool(){
-        
+        school="newschool";
     }
 }
 
 public class stat {
+    public static void main(String args[]){
+        
+    }
     
 }
