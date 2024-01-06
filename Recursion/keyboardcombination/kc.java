400b0db94814ff7391819138afe15f7c8694c7a2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-06 21:47:40 +0530

    added new code

--- Recursion/keyboardcombination/kc.java
+++ Recursion/keyboardcombination/kc.java
@@ -3,7 +3,10 @@ package Recursion.keyboardcombination;
 public class kc {
     public String[] keypad={".","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
     public static void  printCombi(String str,int indx,String combination){
-        
+       if(indx==str.length()){
+        System.out.println(combination);
+        return;
+       } 
     }
     public static void main(String args[]){
 
