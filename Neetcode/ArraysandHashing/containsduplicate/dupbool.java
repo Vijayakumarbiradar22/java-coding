679aa74b9eaad5ad37691fa0f9bf7a55ed03a6cc
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-07 21:53:12 +0530

    added new code

--- Neetcode/ArraysandHashing/containsduplicate/dupbool.java
+++ Neetcode/ArraysandHashing/containsduplicate/dupbool.java
@@ -16,7 +16,7 @@ public class dupbool {
         System.out.println("Enter the array size:");
         int n=sc.nextInt();
         int nums[]= new int[n];
-        System.out.println("Enter the elements of array:");
+        System.out.println("Enter the array elements:");
         for(int i=0;i<n;i++){
             nums[i]=sc.nextInt();
         }
