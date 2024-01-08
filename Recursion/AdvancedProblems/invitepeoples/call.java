34ba2a3a8380aeb175648f64287766c83ac1dc06
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-08 19:51:37 +0530

    added new code

--- Recursion/AdvancedProblems/invitepeoples/call.java
+++ Recursion/AdvancedProblems/invitepeoples/call.java
@@ -6,7 +6,14 @@ public class call {
         if(n<=1){
             return 1;
         }
-        
+
+        //to call single person
+        int ways1 = callGuest(n-1);
+
+        //to call person in pair
+        int ways2 = (n-1)*callGuest(n-2);
+
+        return ways1+ways2;
     }
     public static void main(String args[]){
         int n=4;
