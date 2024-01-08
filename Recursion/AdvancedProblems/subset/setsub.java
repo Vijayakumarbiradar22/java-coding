fd0d5c0596d6df1edac25a0949b31239011c2d8d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-08 19:41:32 +0530

    added new code

--- Recursion/AdvancedProblems/subset/setsub.java
+++ Recursion/AdvancedProblems/subset/setsub.java
@@ -1,8 +1,10 @@
 package Recursion.AdvancedProblems.subset;
-
+import java.util.ArrayList;
 public class setsub {
     public static void main(String args[]){
-        
+      int n=3;
+      ArrayList<Integer> subset = new ArrayList<>();
+      findSubset(n,subset);   
     }
     
 }
