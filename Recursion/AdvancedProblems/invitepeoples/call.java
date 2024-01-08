f2a3520b87162e8a3a8715e62e5ed4930e47dbc5
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-08 19:48:58 +0530

    added new code

--- Recursion/AdvancedProblems/invitepeoples/call.java
+++ Recursion/AdvancedProblems/invitepeoples/call.java
@@ -1,8 +1,11 @@
 package Recursion.AdvancedProblems.invitepeoples;
 
 public class call {
+    
     public static void main(String args[]){
-        
+        int n=4;
+        int totaldays = callGuest(n);
+        System.out.println(totaldays);
     }
     
 }
