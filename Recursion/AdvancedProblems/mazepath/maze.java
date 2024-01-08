da8c2f03689b8f80ec9557760e0ddf8b0540769c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-08 19:54:19 +0530

    added new code

--- Recursion/AdvancedProblems/mazepath/maze.java
+++ Recursion/AdvancedProblems/mazepath/maze.java
@@ -1,8 +1,12 @@
 package Recursion.AdvancedProblems.mazepath;
 
 public class maze {
+    
     public static void main(String args[]){
-        
+      int n=3;
+      int m=3;
+      int totalpath = countPath(0,0,n,m);
+      System.out.println(totalpath);  
     }
     
 }
