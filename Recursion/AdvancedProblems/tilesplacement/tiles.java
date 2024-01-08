c8f67fff4761321172620f270b4423dbd2b22998
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-08 20:00:20 +0530

    added new code

--- Recursion/AdvancedProblems/tilesplacement/tiles.java
+++ Recursion/AdvancedProblems/tilesplacement/tiles.java
@@ -1,7 +1,18 @@
 package Recursion.AdvancedProblems.tilesplacement;
 
 public class tiles {
-    
+    public static int placeTiles(int n, int m){
+        if(n==m){
+            return 2;
+        }
+
+        if(n<m){
+            return 1;
+        }
+
+        //horizontal placement
+        
+    }
     public static void main(String args[]){
         int n=4;
         int m=2;
