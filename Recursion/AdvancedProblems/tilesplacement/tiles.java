23235af01b35c8dc34a15f333e06447be8636a12
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-08 19:59:20 +0530

    added new code

---
+++ Recursion/AdvancedProblems/tilesplacement/tiles.java
@@ -0,0 +1,12 @@
+package Recursion.AdvancedProblems.tilesplacement;
+
+public class tiles {
+    
+    public static void main(String args[]){
+        int n=4;
+        int m=2;
+        int result = placeTiles(n,m);
+        System.out.println(result);
+    }
+    
+}
