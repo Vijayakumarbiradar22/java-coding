9cddee2790f928200447c0f6c0b1cd845ddd6205
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-08 19:35:55 +0530

    added new code

--- Recursion/AdvancedProblems/permutation/permu.java
+++ Recursion/AdvancedProblems/permutation/permu.java
@@ -2,7 +2,12 @@ package Recursion.AdvancedProblems.permutation;
 
 public class permu {
     public static void printPerm(String str,String permutation){
-        
+        if(str.length()==0){
+            System.out.println(permutation);
+            return;
+        }
+
+        for()
     }
     public static void main(String args[]){
         String str = "abc";
