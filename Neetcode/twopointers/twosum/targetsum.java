024e5442a9db062febeded278028cdd04386dd37
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-09 18:15:15 +0530

    added new code

--- Neetcode/twopointers/twosum/targetsum.java
+++ Neetcode/twopointers/twosum/targetsum.java
@@ -5,7 +5,13 @@ public class targetsum {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
         int n= sc.nextInt();
-        
+        int nums[]=new int[n];
+        System.out.println("Enter the elements of array:");
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        int result = findTarget(nums,n);
+        System.out.println(result);
     }
     
 }
