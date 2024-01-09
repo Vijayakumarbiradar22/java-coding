845de400fd123dae5028128841abc7118d24ae19
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-09 19:42:01 +0530

    added new code

--- Neetcode/twopointers/traprainwater/rainwater.java
+++ Neetcode/twopointers/traprainwater/rainwater.java
@@ -2,7 +2,17 @@ package Neetcode.twopointers.traprainwater;
 import java.util.Scanner;
 public class rainwater {
     public static void main(String args[]){
-      Scanner sc = new Scanner(System.in);  
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the size of array:");
+      int n = sc.nextInt();
+      int heights[] = new int[n];
+      System.out.println("Enter the elements:");
+      for(int i=0;i<n;i++){
+        heights[i]=sc.nextInt();
+      }  
+      int result=trap(heights);
+      System.out.println(result);
+      
     }
     
 }
