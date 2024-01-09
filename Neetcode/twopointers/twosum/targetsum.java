8f63065816e5befd23fc53f510ad5f0fa959915e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-09 18:12:02 +0530

    added new code

---
+++ Neetcode/twopointers/twosum/targetsum.java
@@ -0,0 +1,9 @@
+package Neetcode.twopointers.twosum;
+import java.util.Scanner;
+public class targetsum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+}
