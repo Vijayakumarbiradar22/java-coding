9b6a3a1c99a7316d4587bc7e32c3dc994e2fb4b0
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-09 19:05:02 +0530

    added new code

--- Neetcode/twopointers/containermostwater/mostwater.java
+++ Neetcode/twopointers/containermostwater/mostwater.java
@@ -2,7 +2,18 @@ package Neetcode.twopointers.containermostwater;
 import java.util.Scanner;
 public class mostwater {
     public static void main(String args[]){
-        
+       Scanner sc = new  Scanner(System.in);
+       System.out.println("Enter the size of array");
+       int n = sc.nextInt();
+       int heights[]=new int[n];
+       System.out.println("Enter the eleements:");
+       for(int i=0;i<n;i++){
+        heights[i]=sc.nextInt();
+       }
+
+       int result = findAnswer(heights);
+       System.out.println(result);
+
     }
     
 }
