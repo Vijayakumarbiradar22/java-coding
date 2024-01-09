f0232b132a621e7231428b1d79464d2b3c16517b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-09 19:48:55 +0530

    added new code

--- Neetcode/twopointers/traprainwater/rainwater.java
+++ Neetcode/twopointers/traprainwater/rainwater.java
@@ -1,6 +1,28 @@
 package Neetcode.twopointers.traprainwater;
 import java.util.Scanner;
 public class rainwater {
+    public static int trapWater(int[] heights){
+        int n=heights.length;
+        int[] lmax=new int[n];
+        int[] rmax=new int[n];
+        lmax[0]=heights[0];
+        rmax[n-1]=heights[n-1];
+
+        for(int i=1;i<n;i++){
+            lmax[i]=Integer.max(lmax[i-1],heights[i]);
+        }
+
+        for(int i=n-2;i>=0;i--){
+            rmax[i]=Integer.max(rmax[i+1],heights[i]);
+        }
+
+        int sum=0;
+
+        for(int i=1;i<n;i++){
+            sum+=Integer.min(rmax[i],lmax[i+1]-heights[i]);
+        }
+        return sum;
+    }
     public static void main(String args[]){
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter the size of array:");
@@ -10,7 +32,7 @@ public class rainwater {
       for(int i=0;i<n;i++){
         heights[i]=sc.nextInt();
       }  
-      int result=trap(heights);
+      int result=trapWater(heights);
       System.out.println(result);
       sc.close();
     }
