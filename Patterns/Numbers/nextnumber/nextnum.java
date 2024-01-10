d31b29272eb774b6937bf51f1d3b097ed41f0634
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-10 22:18:34 +0530

    added new code

--- Patterns/Numbers/nextnumber/nextnum.java
+++ Patterns/Numbers/nextnumber/nextnum.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class nextnum {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the total rows:");
+        System.out.println("Enter the no.of rows:");
         int n=sc.nextInt();
         int num=1;
         for(int i=1;i<=n;i++){
