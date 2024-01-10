015a3d2596843385b6c98e27aea5c608df5c8b93
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-10 21:54:29 +0530

    added new code

--- Neetcode/twopointers/traprainwater/rainwater.java
+++ Neetcode/twopointers/traprainwater/rainwater.java
@@ -25,7 +25,7 @@ public class rainwater {
     }
     public static void main(String args[]){
       Scanner sc = new Scanner(System.in);
-      System.out.println("Enter the array size:");
+      System.out.println("Enter the size of array:");
       int n = sc.nextInt();
       int heights[] = new int[n];
       System.out.println("Enter the elements:");
