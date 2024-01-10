b0dc2841407217b4bebb66ec9264d8d29eabb120
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-10 22:08:40 +0530

    added new code

--- Recursion/findpower/power.java
+++ Recursion/findpower/power.java
@@ -15,7 +15,7 @@ public class power {
     }
     public static void main(String args[]){
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the value of x:");
+            System.out.println("Enter the x:");
             int x = sc.nextInt();
             System.out.println("Enter the value of n:");
             int n = sc.nextInt();
