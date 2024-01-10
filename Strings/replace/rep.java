aa52b30f72c0879fde055e40fe668b68c61b745e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-10 22:12:58 +0530

    added new code

--- Strings/replace/rep.java
+++ Strings/replace/rep.java
@@ -2,11 +2,11 @@ package Strings.replace;
 
 public class rep {
     public static void main(String args[]){
-        String str="epple";
+        String str="apple";
         String res="";
 
         for(int i=0;i<str.length();i++){
-            if(str.charAt(i)=='e'){
+            if(str.charAt(i)=='a'){
                 res+='i';
             }
             else{
