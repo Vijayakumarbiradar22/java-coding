be8e5921ad3bcb647d39aaccde109755f2e91f05
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-11 21:29:09 +0530

    added new code

--- Patterns/Alpha/samealpha/same.java
+++ Patterns/Alpha/samealpha/same.java
@@ -2,7 +2,11 @@ package Patterns.Alpha.samealpha;
 
 public class same {
     public static void main(String args[]){
+      char chr='E';
+      
+      for(char i='A';i<='E';i++){
         
+      }
     }
     
 }
