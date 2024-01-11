b82854c060c13d1e2d64967db26fdf3763554668
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-11 21:39:20 +0530

    added new code

---
+++ Patterns/Alpha/similerline/simi.java
@@ -0,0 +1,12 @@
+package Patterns.Alpha.similerline;
+
+public class simi {
+    public static void main(String args[]){
+        char chr='E';
+
+        for(char i='A';i<=chr;i++){
+            
+        }
+    }
+    
+}
