8c50dfde716b8dcec01dc9056b36cfd670c19e0d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-12 21:25:33 +0530

    added new code

--- Patterns/Alpha/triangle/at.java
+++ Patterns/Alpha/triangle/at.java
@@ -2,7 +2,13 @@ package Patterns.Alpha.triangle;
 
 public class at {
     public static void main(String args[]){
-        
+       char chr='E';
+       
+       for(char i='A';i<=chr;i++){
+        for(char j='A';j<=i;j++){
+            System.out.print(j);
+        }
+       }
     }
     
 }
