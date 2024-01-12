d31d6d0da4e12dd86fdc53b9ecda19fbd6424e7c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-12 21:33:49 +0530

    added new code

--- Patterns/Alpha/righttriangle/ralpha.java
+++ Patterns/Alpha/righttriangle/ralpha.java
@@ -5,6 +5,9 @@ public class ralpha {
         char chr='E';
 
         for(char i='A';i<=chr;i++){
+            for(char s='A';s<=chr-i;s++){
+                System.out.print(" ");
+            }
             for(char j='A';j<=i;j++){
                 System.out.print(j);
             }
