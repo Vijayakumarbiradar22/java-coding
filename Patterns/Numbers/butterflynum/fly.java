d7adbf99b3f182a5e26a4867d618bddf5559d27c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-13 22:17:14 +0530

    added new code

--- Patterns/Numbers/butterflynum/fly.java
+++ Patterns/Numbers/butterflynum/fly.java
@@ -5,7 +5,7 @@ public class fly {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
         int n=sc.nextInt();
-       //int n=5;
+      
        for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
             System.out.print(j);
