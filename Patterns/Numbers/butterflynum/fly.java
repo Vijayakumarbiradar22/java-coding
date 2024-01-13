cede67010ede8dad98591959260799798612f023
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-13 22:14:03 +0530

    added new code

--- Patterns/Numbers/butterflynum/fly.java
+++ Patterns/Numbers/butterflynum/fly.java
@@ -1,7 +1,8 @@
 package Patterns.Numbers.butterflynum;
-
+import java.util.*;
 public class fly {
     public static void main(String args[]){
+        
        int n=5;
        for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
