0d1d7e114d2008d5ad8e56a9b24e9d86b77735cc
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-13 22:10:44 +0530

    added new code

--- Patterns/Numbers/butterflynum/fly.java
+++ Patterns/Numbers/butterflynum/fly.java
@@ -22,6 +22,11 @@ public class fly {
         for(int j=1;j<=i;j++){
             System.out.print(j);
         }
+        int sp =2*(n-i);
+        for(int s=1;s<=sp;s++){
+            System.out.print(" ");
+        }
+        
         System.out.println();
        }
 
