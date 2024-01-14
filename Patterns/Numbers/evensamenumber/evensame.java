eae2954bd06d6451e925557c536da516e18e829b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-14 22:00:11 +0530

    added new code

--- Patterns/Numbers/evensamenumber/evensame.java
+++ Patterns/Numbers/evensamenumber/evensame.java
@@ -3,7 +3,9 @@ package Patterns.Numbers.evensamenumber;
 public class evensame {
     public static void main (String args[]){
        int n=5;
+       for(int i=1;i<=n;i++){
         
+       } 
     }
     
 }
