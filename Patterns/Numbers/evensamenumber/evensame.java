10b111a1de67837aca65a1ff8d66b1c4b39756da
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-14 22:12:36 +0530

    added new code

--- Patterns/Numbers/evensamenumber/evensame.java
+++ Patterns/Numbers/evensamenumber/evensame.java
@@ -2,6 +2,8 @@ package Patterns.Numbers.evensamenumber;
 import java.util.Scanner;
 public class evensame {
     public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        
        int n=5;
        int num=2;
        for(int i=1;i<=n;i++){
