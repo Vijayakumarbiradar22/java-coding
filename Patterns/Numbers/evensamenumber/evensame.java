d3ddcd9c60bcf94b0fefed23a180620afeff4764
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-14 22:13:47 +0530

    added new code

--- Patterns/Numbers/evensamenumber/evensame.java
+++ Patterns/Numbers/evensamenumber/evensame.java
@@ -5,7 +5,6 @@ public class evensame {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
         int n = sc.nextInt();
-       //int n=5;
        int num=2;
        for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
