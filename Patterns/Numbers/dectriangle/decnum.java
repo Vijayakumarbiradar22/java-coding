f4aef9dfb664cb6fef1ce57a57d321c0b950013a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-15 09:51:36 +0530

    added new code

--- Patterns/Numbers/dectriangle/decnum.java
+++ Patterns/Numbers/dectriangle/decnum.java
@@ -6,7 +6,7 @@ public class decnum {
         System.out.println("Enter the number of rows:");
         int n = sc.nextInt();
         for(int i=n;i>=1;i--){
-            for(int j=n;j<=i;j++){
+            for(int j=n;j>=i;j--){
                 System.out.print(j+" ");
             }
             System.out.println();
