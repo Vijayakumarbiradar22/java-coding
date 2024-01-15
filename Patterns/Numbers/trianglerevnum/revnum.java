e546777cd874ba783457d33b0814f478dad8b000
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-15 09:56:59 +0530

    added new code

--- Patterns/Numbers/trianglerevnum/revnum.java
+++ Patterns/Numbers/trianglerevnum/revnum.java
@@ -1,5 +1,18 @@
 package Patterns.Numbers.trianglerevnum;
-
+import java.util.Scanner;
 public class revnum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers of rows:");
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=i;j>=1;j--){
+                System.out.print(j+" ");
+            }
+            
+        }
+
+    }
     
 }
