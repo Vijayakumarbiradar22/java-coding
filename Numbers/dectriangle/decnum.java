8b44621eb677f4fd16dbcfa3daa96bd351fb93a6
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-15 09:41:38 +0530

    added new code

--- Numbers/dectriangle/decnum.java
+++ Numbers/dectriangle/decnum.java
@@ -1,8 +1,10 @@
 package Numbers.dectriangle;
-
+import java.util.Scanner;
 public class decnum {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         
+
     }
     
 }
