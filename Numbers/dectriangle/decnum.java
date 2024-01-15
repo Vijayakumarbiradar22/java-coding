a21b1b65f37669668b57f4c43205d083f4c9482d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-15 09:43:42 +0530

    added new code

--- Numbers/dectriangle/decnum.java
+++ Numbers/dectriangle/decnum.java
@@ -5,11 +5,11 @@ public class decnum {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
         int n = sc.nextInt();
-
         for(int i=n;i>=1;i--){
             for(int j=n;j<=i;j++){
                 System.out.print(j+" ");
             }
+            System.out.println();
         }
 
     }
