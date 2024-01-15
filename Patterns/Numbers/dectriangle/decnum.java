c49abcd563916a4969b520a18c20b9b2f3e4e492
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-15 09:47:14 +0530

    added new code

---
+++ Patterns/Numbers/dectriangle/decnum.java
@@ -0,0 +1,17 @@
+package Patterns.Numbers.dectriangle;
+import java.util.Scanner;
+public class decnum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n = sc.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=n;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
