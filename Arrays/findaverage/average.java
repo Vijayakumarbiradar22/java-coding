9c071912842bb832f89d450fcc9d6cbaa1d6601e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-16 21:40:07 +0530

    added new code

--- Arrays/findaverage/average.java
+++ Arrays/findaverage/average.java
@@ -5,6 +5,11 @@ public class average {
         double sum=0;
         int arr[]={1,3,5,7,9};
         int length=arr.length;
+
+        for(int i=0;i<length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println();
     }
     
 }
