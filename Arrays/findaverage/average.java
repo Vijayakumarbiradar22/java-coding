fe43ee39f937e79baf5f5444c56a783624ffaa57
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-16 21:39:05 +0530

    added new code

--- Arrays/findaverage/average.java
+++ Arrays/findaverage/average.java
@@ -2,7 +2,9 @@ package Arrays.findaverage;
 
 public class average {
     public static void main(String args[]){
-        
+        double sum=0;
+        int arr[]={1,3,5,7,9};
+        int length=arr.length;
     }
     
 }
