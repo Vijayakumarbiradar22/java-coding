0ebcd03a6bac91a8166b10bc69a753d3bb372965
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-17 21:43:51 +0530

    modified code

--- Arrays/arrayunion/union.java
+++ Arrays/arrayunion/union.java
@@ -3,20 +3,7 @@ import java.util.Scanner;
 import java.util.*;
 public class union {
     public static ArrayList<Integer> Unionarray(int arr1[],int arr2[],int size1,int size2){
-        TreeSet<Integer> set = new TreeSet<>();
-        for(int i:arr1){
-            set.add(i);
-        }
-
-        for(int i:arr2){
-            set.add(i);
-        }
-
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i:set){
-            list.add(i);
-        }
-        return list;
+        
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
