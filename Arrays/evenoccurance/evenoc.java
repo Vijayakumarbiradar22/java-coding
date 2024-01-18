6c34a49a2b3433566611c21ad67bb2922befecd3
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-18 21:22:17 +0530

    modified code

--- Arrays/evenoccurance/evenoc.java
+++ Arrays/evenoccurance/evenoc.java
@@ -2,18 +2,19 @@ package Arrays.evenoccurance;
 
 public class evenoc {
     public static int getOccurance(int arr[], int len){
-        for(int i=0;i<len;i++){
-            int count=0;
-            for(int j=0;j<len;j++){
-                if(arr[i]==arr[j]){
-                    count++;
-                }
-            }
-            if(count%2==0){
-              return arr[i];  
-            }
-        }
-        return -1;
+        
+        // for(int i=0;i<len;i++){
+        //     int count=0;
+        //     for(int j=0;j<len;j++){
+        //         if(arr[i]==arr[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count%2==0){
+        //       return arr[i];  
+        //     }
+        // }
+        // return -1;
     }
     public static void main(String rags[]){
         int[] arr = {5,3,4,5,4,1,5};
