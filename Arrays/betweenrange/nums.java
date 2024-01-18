0da0e1007968330b848a8b8f37885ee09391712f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-18 19:34:08 +0530

    modified code

--- Arrays/betweenrange/nums.java
+++ Arrays/betweenrange/nums.java
@@ -2,11 +2,17 @@ package Arrays.betweenrange;
 import java.util.Scanner;
 public class nums {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
        System.out.println("Enter the size of array:");
        int n = sc.nextInt();
-        int[] arr = new int[n];
-        
+       int[] arr = new int[n];
+       System.out.println("Enter the element:");
+       for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+       }
+       int count=0;
+       
+
 
 
         sc.close();
