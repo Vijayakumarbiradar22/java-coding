8a63a7f95aded9bab1cec85f1aa7c4f3fe70bb60
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-18 19:19:32 +0530

    modified code

--- Arrays/betweenrange/nums.java
+++ Arrays/betweenrange/nums.java
@@ -3,35 +3,12 @@ import java.util.Scanner;
 public class nums {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array:");
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        System.out.println("Enter the elements:");
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        System.out.println("Enter the start number:");
-        int start=sc.nextInt();
-        System.out.println("Enter the end number:");
-        int end=sc.nextInt();
-        //int arr[]= {1,12,14,20,25,37,65,72};
-        //int start=2;
-        //int end=40;
-        int count=0;
+       System.out.println("Enter the size of array:");
+       int n = sc.nextInt();
+        int[] arr = new int[n];
+        
 
-        for(int i=0;i<n;i++){
-            if(arr[i]>=start && arr[i]<=end){
-                count+=1;
-                System.out.print(arr[i]+" ");
-            }
-        }
-        System.out.println();
 
-        if(count>0){
-            System.out.println(count+" numbers available");
-        } else {
-            System.out.println("No numbers available");
-        }
         sc.close();
     }
     
