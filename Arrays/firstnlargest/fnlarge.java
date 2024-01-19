5f2d46eb1cdbd1f34d0fd644a3e49a31e20a7697
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-19 21:55:57 +0530

    modified code

--- Arrays/firstnlargest/fnlarge.java
+++ Arrays/firstnlargest/fnlarge.java
@@ -25,8 +25,8 @@ public class fnlarge {
         
         bubbleSort(arr,n);
 
-        for(int i=0;i<size;i++){
-            System.out.print(arr[i]+ " ");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
         }
         System.out.println();
 
