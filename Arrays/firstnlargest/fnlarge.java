96b693d3e3408ee9b2b40b7ce2a5ce666754a81e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-19 21:59:35 +0530

    modified code

--- Arrays/firstnlargest/fnlarge.java
+++ Arrays/firstnlargest/fnlarge.java
@@ -6,7 +6,9 @@ public class fnlarge {
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(arr[i]>arr[j]){
-                    
+                   temp = arr[i];
+                   arr[i] = arr[j];
+                   arr[j] = temp; 
                 }
             }
         }
