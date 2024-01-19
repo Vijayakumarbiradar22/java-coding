3d08c91512ec23eb051612a031002b517496f670
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-19 22:03:26 +0530

    modified code

--- Arrays/large1d/large.java
+++ Arrays/large1d/large.java
@@ -1,20 +1,25 @@
 package Arrays.large1d;
-
+import java.util.Scanner;
 public class large {
     public static void main(String args[]){
-        int[] arr = {2,6,4,2,3};
-        int max=arr[0];
-        int min=arr[0];
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]>max){
-                max=arr[i];
-            }
-            if(arr[i]<min){
-                min=arr[i];
-            }
-        }
-        System.out.println(max);
-        System.out.println(min);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+        System.out.println("Enter the elements:");
+        
+        // int[] arr = {2,6,4,2,3};
+        // int max=arr[0];
+        // int min=arr[0];
+        // for(int i=1;i<arr.length;i++){
+        //     if(arr[i]>max){
+        //         max=arr[i];
+        //     }
+        //     if(arr[i]<min){
+        //         min=arr[i];
+        //     }
+        // }
+        // System.out.println(max);
+        // System.out.println(min);
     }
     
 }
