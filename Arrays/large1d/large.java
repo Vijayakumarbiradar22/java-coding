e241a5d4f23eb2cfb4b191fa441749062eccc75c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-19 22:07:27 +0530

    modified code

--- Arrays/large1d/large.java
+++ Arrays/large1d/large.java
@@ -26,18 +26,7 @@ public class large {
 
         System.out.println(max);
         System.out.println(min);
-        // int max=arr[0];
-        // int min=arr[0];
-        // for(int i=1;i<arr.length;i++){
-        //     if(arr[i]>max){
-        //         max=arr[i];
-        //     }
-        //     if(arr[i]<min){
-        //         min=arr[i];
-        //     }
-        // }
-        // System.out.println(max);
-        // System.out.println(min);
+        
     }
     
 }
