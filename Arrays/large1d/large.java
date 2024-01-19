8f5d87153ae1a1476693aa55870f8575fdf7a311
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-19 22:04:20 +0530

    modified code

--- Arrays/large1d/large.java
+++ Arrays/large1d/large.java
@@ -5,7 +5,11 @@ public class large {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
         int n=sc.nextInt();
+        int[] arr = new int[n];
         System.out.println("Enter the elements:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
         
         // int[] arr = {2,6,4,2,3};
         // int max=arr[0];
