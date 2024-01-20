8d499852b13a0dc0220ab05d8808f7f243f0fe4e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-20 22:55:24 +0530

    modified code

--- Neetcode/ArraysandHashing/targetsum/target.java
+++ Neetcode/ArraysandHashing/targetsum/target.java
@@ -18,7 +18,7 @@ public class target {
     }
     public static void main(String rgs[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the array size:");
+        System.out.println("Enter the size of array:");
         int n=sc.nextInt();
         int[] nums=new int[n];
         System.out.println("Enter the elements of array:");
