e41a5200240ebc7b341e725572eaa7ea971df1e3
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-20 22:54:18 +0530

    modified code

--- Neetcode/ArraysandHashing/productexceptself/selfless.java
+++ Neetcode/ArraysandHashing/productexceptself/selfless.java
@@ -23,7 +23,7 @@ public class selfless {
         System.out.println("Enter the size of array:");
         int n= sc.nextInt();
         int nums[]=new int[n];
-        System.out.println("Enter the elements");
+        System.out.println("Enter the elements of array:");
         for(int i=0;i<n;i++){
             nums[i]=sc.nextInt();
         }
