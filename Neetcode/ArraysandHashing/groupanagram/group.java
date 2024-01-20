ae38eddb34bf6431f3d5c123725b3a9b6f9c60ad
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-20 22:51:03 +0530

    modified code

--- Neetcode/ArraysandHashing/groupanagram/group.java
+++ Neetcode/ArraysandHashing/groupanagram/group.java
@@ -23,7 +23,7 @@ public class group {
     //     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array:");
+        System.out.println("Enter the array size:");
         int n=sc.nextInt();
         String strs[]=new String[n];
         System.out.println("Enter the array of strings:");
