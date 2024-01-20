98bacffc68e2801e3c914f3eeadc468bfd328019
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-20 22:46:25 +0530

    modified code

--- Neetcode/ArraysandHashing/anagram/valid.java
+++ Neetcode/ArraysandHashing/anagram/valid.java
@@ -16,7 +16,7 @@ public class valid {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter String s:");
+        System.out.println("Enter the String s:");
         String s=sc.nextLine();
         System.out.println("Enter string t:");
         String t = sc.nextLine();
