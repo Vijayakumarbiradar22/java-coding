63bc2b55981b530c69f3b2bae5d8ebe7b27228f0
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-21 21:18:41 +0530

    modified code

--- Arrays/large2d/large.java
+++ Arrays/large2d/large.java
@@ -6,6 +6,16 @@ public class large {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter the number of rows:");
             int r=sc.nextInt();
+            System.out.println("Enter the number of cols:");
+            int c=sc.nextInt();
+            int[][] arr = new int[r][c];
+            System.out.println("Enter the array elements:");
+            for(int i=0;i<r;i++){
+                for(int j=0;j<c;j++){
+                    arr[r][c]=sc.nextInt();
+                }
+            }
+
             
     }
     
