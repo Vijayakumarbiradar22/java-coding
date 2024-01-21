ed162fd15a23cac8a471c2972a8b69b69ad2a7c7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-21 21:50:33 +0530

    modified code

--- Arrays/leftrotate/lrotate.java
+++ Arrays/leftrotate/lrotate.java
@@ -5,7 +5,7 @@ public class lrotate {
         int p=1;
         while(p<=rt){
             int last=arr[0];
-            for(int i=0;i<n;i++){
+            for(int i=0;i<n-1;i++){
                 arr[i]=arr[i+1];
             }
             arr[n-1]=last;
