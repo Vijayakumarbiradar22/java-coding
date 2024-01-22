8c1f7bacbfba3dd91abcd0049b615f633fe1c530
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-22 16:50:41 +0530

    modified code

--- Arrays/maxsubarray/sub.java
+++ Arrays/maxsubarray/sub.java
@@ -4,6 +4,8 @@ public class sub {
    
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
         
             
     }
