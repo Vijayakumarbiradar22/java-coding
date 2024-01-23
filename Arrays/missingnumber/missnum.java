b2cac430a9101f7d2088489af7f06d4c088befc2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-23 20:58:57 +0530

    modified code

--- Arrays/missingnumber/missnum.java
+++ Arrays/missingnumber/missnum.java
@@ -8,7 +8,13 @@ public class missnum {
            int n = sc.nextInt();
            int[] arr = new int[n];
            System.out.println("Enter the elements:");
-           
+           for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+           }
+
+           int result=findMissing(arr,n);
+           System.out.println(result);
+
     }
     
 }
