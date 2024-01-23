1ec55adb41ff5ba830f0b49c710dc78667327e01
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-23 21:17:53 +0530

    modified code

--- Arrays/repeatingele/repeat.java
+++ Arrays/repeatingele/repeat.java
@@ -14,7 +14,14 @@ public class repeat {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
         int n = sc.nextInt();
-        
+        int arr[]=new int[n];
+        System.out.println("Enter the elements:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int result = printRepeat(arr,n);
+        System.out.println(result);
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the size of array:");
         // int size=sc.nextInt();
