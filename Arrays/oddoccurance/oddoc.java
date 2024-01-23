7a53716291afe39a24790bd4f29fb28972a1ea42
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-23 21:06:14 +0530

    modified code

--- Arrays/oddoccurance/oddoc.java
+++ Arrays/oddoccurance/oddoc.java
@@ -1,25 +1,28 @@
 package Arrays.oddoccurance;
-
+import java.util.Scanner;
 public class oddoc {
     public static int getOccurance(int arr[], int len){
-        for(int i=0;i<len;i++){
-            int count=0;
-            for(int j=0;j<len;j++){
-                if(arr[i]==arr[j]){
-                    count++;
-                }
-            }
-                if(count%2!=0){
-                    return arr[i];
-                }
-            }
-                return -1; 
+        // for(int i=0;i<len;i++){
+        //     int count=0;
+        //     for(int j=0;j<len;j++){
+        //         if(arr[i]==arr[j]){
+        //             count++;
+        //         }
+        //     }
+        //         if(count%2!=0){
+        //             return arr[i];
+        //         }
+        //     }
+        //         return -1; 
     }
     public static void main(String args[]){
-        int arr[]={2,5,3,2,1,2,5,3,1};
-        int len=arr.length;
+        Scanner sc = new Scanner(System.in);
+        
 
-        System.out.println(getOccurance(arr,len));
+        // int arr[]={2,5,3,2,1,2,5,3,1};
+        // int len=arr.length;
+
+        // System.out.println(getOccurance(arr,len));
     }
     
 }
