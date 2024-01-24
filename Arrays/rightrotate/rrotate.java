e39b25ccfc25f711327d42a6697443a7c99d28d0
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-24 21:41:02 +0530

    modified code

--- Arrays/rightrotate/rrotate.java
+++ Arrays/rightrotate/rrotate.java
@@ -1,35 +1,38 @@
 package Arrays.rightrotate;
 import java.util.Scanner;
 public class rrotate {
-    public static void rightRotate(int arr[], int size, int rt){
-        int help=1;
-        while(help<=rt){
-          int first=arr[size-1];
-          for(int i=size-2;i>=0;i--){
-            arr[i+1]=arr[i];
-          }  
-          arr[0]=first;
-          help++;
-        }
-    }
+    // public static void rightRotate(int arr[], int size, int rt){
+    //     int help=1;
+    //     while(help<=rt){
+    //       int first=arr[size-1];
+    //       for(int i=size-2;i>=0;i--){
+    //         arr[i+1]=arr[i];
+    //       }  
+    //       arr[0]=first;
+    //       help++;
+    //     }
+    // }
     public static void main(String args[]){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the size of array:");
-            int size = sc.nextInt();
-            int[] arr = new int[size];
-            System.out.println("Enter the elements of array:");
-            for(int i=0;i<size;i++){
-                arr[i]=sc.nextInt();
-            }
-            System.out.println("Enter the rotation times:");
-            int rt= sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+            // Scanner sc = new Scanner(System.in);
+            // System.out.println("Enter the size of array:");
+            // int size = sc.nextInt();
+            // int[] arr = new int[size];
+            // System.out.println("Enter the elements of array:");
+            // for(int i=0;i<size;i++){
+            //     arr[i]=sc.nextInt();
+            // }
+            // System.out.println("Enter the rotation times:");
+            // int rt= sc.nextInt();
 
-            rightRotate(arr,size,rt);
+            // rightRotate(arr,size,rt);
 
-            for(int i=0;i<size;i++){
-                System.out.print(arr[i]+" ");
-            sc.close();
+            // for(int i=0;i<size;i++){
+            //     System.out.print(arr[i]+" ");
+            // sc.close();
         }
     }
     
-}
+
