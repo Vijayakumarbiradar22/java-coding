10ed0c0de2b085c63a607ec3444f5bf88afaccee
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-24 21:54:41 +0530

    modified code

--- Arrays/runningsum/run.java
+++ Arrays/runningsum/run.java
@@ -12,6 +12,8 @@ public class run {
      
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n = sc.nextInt();
         
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the size of array:");
