c3f1c7e1a324e0efd94a21ae37414e33f7709e64
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-24 21:54:12 +0530

    modified code

--- Arrays/runningsum/run.java
+++ Arrays/runningsum/run.java
@@ -1,29 +1,31 @@
 package Arrays.runningsum;
 import java.util.Scanner;
 public class run {
-    public static int[] findRunningSum(int num[],int n){
-        int[] result=new int[n];
-        result[0]=num[0];
-        for(int i=1;i<n;i++){
-            result[i]=num[i]+result[i-1];
-        }
-        return result;
-    }
+    // public static int[] findRunningSum(int num[],int n){
+    //     int[] result=new int[n];
+    //     result[0]=num[0];
+    //     for(int i=1;i<n;i++){
+    //         result[i]=num[i]+result[i-1];
+    //     }
+    //     return result;
+    // }
      
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array:");
-        int n=sc.nextInt();
-        int num[]=new int[n];
-        System.out.println("Enter the elements of array:");
-        for(int i=0;i<n;i++){
-            num[i]=sc.nextInt();
-        }
-         int[] output=findRunningSum(num,n);
-         for(int i=0;i<n;i++){
-            System.out.print(output[i]+" ");
-         }
-        sc.close();
+        
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the size of array:");
+        // int n=sc.nextInt();
+        // int num[]=new int[n];
+        // System.out.println("Enter the elements of array:");
+        // for(int i=0;i<n;i++){
+        //     num[i]=sc.nextInt();
+        // }
+        //  int[] output=findRunningSum(num,n);
+        //  for(int i=0;i<n;i++){
+        //     System.out.print(output[i]+" ");
+        //  }
+        // sc.close();
     }
     
 }
