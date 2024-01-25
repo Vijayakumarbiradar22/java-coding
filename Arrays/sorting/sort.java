2cbbbe9af4bcb839daac531b20c75831ed90df35
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-25 21:39:19 +0530

    modified code

--- Arrays/sorting/sort.java
+++ Arrays/sorting/sort.java
@@ -6,6 +6,8 @@ public class sort {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter the size of array:");
             int n = sc.nextInt();
+            int arr[]=new int[n];
+            System.out.println("Enter the elements:");
             
         
 
