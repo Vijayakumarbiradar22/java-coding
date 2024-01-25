44c66d4ebc5b00687b3e5656b3196946a281db8b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-25 21:42:24 +0530

    modified code

--- Arrays/sorting/sort.java
+++ Arrays/sorting/sort.java
@@ -1,6 +1,19 @@
 package Arrays.sorting;
 import java.util.Scanner;
 public class sort {
+    public static void bubbleSort(int arr[],int n){
+        int temp;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
     
     public static void main(String args[]){
             Scanner sc = new Scanner(System.in);
