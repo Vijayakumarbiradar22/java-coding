ba4673739a5b4bf4c9250c3fb4a24bc3af00b009
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-26 21:35:31 +0530

    modified code

--- Arrays/subarray/onlyodd.java
+++ Arrays/subarray/onlyodd.java
@@ -34,38 +34,6 @@ public class onlyodd {
         }
 
         sc.close();
-
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter the size of array:");
-        // int n=sc.nextInt();
-        // int arr[]=new int[n];
-        // System.out.println("Enter the elements of array:");
-        // for(int i=0;i<n;i++){
-        //     arr[i]=sc.nextInt();
-        // }
-        // //int[] arr = {1,2,3,4,5,6,7,8,9};
-        // int od=0;
-
-        // for(int i=0;i<n;i++){
-        //     if(arr[i]%2!=0){
-        //         od+=1;
-        //     }
-        // }
-
-        // int[] arr1=new int[od];
-        // int x=0;
-
-        // for(int i=0;i<n;i++){
-        //     if(arr[i]%2!=0){
-        //         arr1[x]=arr[i];
-        //         x++;
-        //     }
-        // }
-
-        // for(int i=0;i<arr1.length;i++){
-        //     System.out.print(arr1[i]+" ");
-        // }
-        // sc.close();
     }
     
 }
