3dcb90be1535c8065f760bfa3a0bddf1adf10c27
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-26 21:29:35 +0530

    modified code

--- Arrays/subarray/onlyodd.java
+++ Arrays/subarray/onlyodd.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class onlyodd {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n = sc.nextInt();
         
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the size of array:");
