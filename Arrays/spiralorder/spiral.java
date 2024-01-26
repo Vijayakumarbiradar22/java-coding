f2d0c324ffadde9c363e02cd6b18c6de9d775f1c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-26 21:13:20 +0530

    modified code

--- Arrays/spiralorder/spiral.java
+++ Arrays/spiralorder/spiral.java
@@ -9,7 +9,12 @@ public class spiral {
         int c = sc.nextInt();
         int[][] arr = new int[r][c];
         System.out.println("Enter the elements:");
-
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the no.of rows:");
         // int r=sc.nextInt();
