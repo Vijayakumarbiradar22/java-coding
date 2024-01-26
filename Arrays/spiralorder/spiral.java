dbcab2b02a9964625dc32616b414dbfbcf85302c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-26 21:11:07 +0530

    modified code

--- Arrays/spiralorder/spiral.java
+++ Arrays/spiralorder/spiral.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class spiral {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the numberof rows:");
+        System.out.println("Enter the number of rows:");
         int r = sc.nextInt();
+        System.out.println("Enter the number of cols:");
+        int c = sc.nextInt();
         
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the no.of rows:");
