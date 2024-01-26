66aa4bd05c2c3eb4873edacb5673b51ec74b1f6c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-26 21:09:31 +0530

    modified code

--- Arrays/spiralorder/spiral.java
+++ Arrays/spiralorder/spiral.java
@@ -3,51 +3,50 @@ import java.util.Scanner;
 public class spiral {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no.of rows:");
-        int r=sc.nextInt();
-        System.out.println("Enter the no.of cols");
-        int c=sc.nextInt();
-        int[][] mat = new int[r][c];
-        System.out.println("Enter the element:");
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                mat[i][j]=sc.nextInt();
-            }
-        }
+        System.out.println("Enter the numberof rows:");
+        int r = sc.nextInt();
+        
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the no.of rows:");
+        // int r=sc.nextInt();
+        // System.out.println("Enter the no.of cols");
+        // int c=sc.nextInt();
+        // int[][] mat = new int[r][c];
+        // System.out.println("Enter the element:");
+        // for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //         mat[i][j]=sc.nextInt();
+        //     }
+        // }
 
-        System.out.println("Spiral order is:");
-        int rowstart=0;
-        int rowend=r-1;
-        int colstart=0;
-        int colend=c-1;
+        // System.out.println("Spiral order is:");
+        // int rowstart=0;
+        // int rowend=r-1;
+        // int colstart=0;
+        // int colend=c-1;
 
-        while(rowstart<=rowend && colstart<=colend){
+        // while(rowstart<=rowend && colstart<=colend){
 
-            for(int col=colstart;col<=colend;col++){
-                System.out.print(mat[rowstart][col]+" ");
-            }
-            rowstart++;
+        //     for(int col=colstart;col<=colend;col++){
+        //         System.out.print(mat[rowstart][col]+" ");
+        //     }
+        //     rowstart++;
 
-            for(int row=rowstart;row<=rowend;row++){
-                System.out.print(mat[row][colend]+" ");
-            }
-            colend--;
+        //     for(int row=rowstart;row<=rowend;row++){
+        //         System.out.print(mat[row][colend]+" ");
+        //     }
+        //     colend--;
 
-            for(int col=colend;col>=colstart;col--){
-                System.out.print(mat[rowend][col]+" ");
-            }
-            rowend--;
+        //     for(int col=colend;col>=colstart;col--){
+        //         System.out.print(mat[rowend][col]+" ");
+        //     }
+        //     rowend--;
 
-            for(int row=rowend;row>=rowstart;row--){
-                System.out.print(mat[row][colstart]+" ");
-            }
-            colstart++;
+        //     for(int row=rowend;row>=rowstart;row--){
+        //         System.out.print(mat[row][colstart]+" ");
+        //     }
+        //     colstart++;
 
-        sc.close();
-    }
-
-
-
-    }
-    
+        // sc.close();
+    }   
 }
