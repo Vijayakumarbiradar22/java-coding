f8ab57b3b88891b614c0a1bb332165e1f4e2d080
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-27 21:47:03 +0530

    modified code

--- Arrays/subarrayeven/onlyeven.java
+++ Arrays/subarrayeven/onlyeven.java
@@ -10,6 +10,8 @@ public class onlyeven {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
+
+        int even=0;
         
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the array size:");
