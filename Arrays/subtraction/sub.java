7a0e4120ecbce8b212cb1403fa94a82b30face16
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-27 21:54:37 +0530

    modified code

--- Arrays/subtraction/sub.java
+++ Arrays/subtraction/sub.java
@@ -3,45 +3,44 @@ import java.util.Scanner;
 public class sub {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no.of rows:");
-        int r = sc.nextInt();
-        System.out.println("Enter the no.of cols:");
-        int c = sc.nextInt();
-        int[][] arr1=new int[r][c];
-        int[][] arr2=new int[r][c];
-        System.out.println("Enter the array 1:");
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                arr1[i][j]=sc.nextInt();
-            }
-        }
-        System.out.println(arr1.length);
+        System.out.println("Enter the number of rows");
+        int r=sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the no.of rows:");
+        // int r = sc.nextInt();
+        // System.out.println("Enter the no.of cols:");
+        // int c = sc.nextInt();
+        // int[][] arr1=new int[r][c];
+        // int[][] arr2=new int[r][c];
+        // System.out.println("Enter the array 1:");
+        // for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //         arr1[i][j]=sc.nextInt();
+        //     }
+        // }
+        // System.out.println(arr1.length);
 
-        System.out.println("Enter the array 2:");
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                arr2[i][j]=sc.nextInt();
-            }
-        }
-        System.out.println(arr2.length);
+        // System.out.println("Enter the array 2:");
+        // for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //         arr2[i][j]=sc.nextInt();
+        //     }
+        // }
+        // System.out.println(arr2.length);
 
-        int[][] arr3=new int[arr1.length][arr2.length];
+        // int[][] arr3=new int[arr1.length][arr2.length];
 
-        System.out.println("Array1 minus Array2 is:");
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                arr3[i][j]=0;
-                for(int k=0;k<arr3.length;k++){
-                    arr3[i][j]=arr1[i][j]-arr2[i][j];
-                }
-                System.out.print(arr3[i][j]+" ");
-                
-            }
-            System.out.println();
-        sc.close();
-        }
-        
-
+        // System.out.println("Array1 minus Array2 is:");
+        // for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //         arr3[i][j]=0;
+        //         for(int k=0;k<arr3.length;k++){
+        //             arr3[i][j]=arr1[i][j]-arr2[i][j];
+        //         }
+        //         System.out.print(arr3[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // sc.close();
+        // }
     }
-    
 }
