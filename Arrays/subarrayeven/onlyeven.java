a97c2b8858d0b5a6a80d9a6bceeda9ce4b3e7c42
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-27 21:49:28 +0530

    modified code

--- Arrays/subarrayeven/onlyeven.java
+++ Arrays/subarrayeven/onlyeven.java
@@ -30,7 +30,7 @@ public class onlyeven {
         }
 
         for(int i=0;i<arr1.length;i++){
-            
+          System.out.print(arr1[i]+" ");  
         }
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the array size:");
