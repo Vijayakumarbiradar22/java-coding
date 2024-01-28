f5a8f955a166af1821b6f382eda5af0602009e2f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-28 22:37:17 +0530

    modified code

--- Arrays/subarray/onlyodd.java
+++ Arrays/subarray/onlyodd.java
@@ -6,7 +6,7 @@ public class onlyodd {
         System.out.println("Enter the array size:");
         int n = sc.nextInt();
         int arr[]=new int[n];
-        System.out.println("Enter the elements of array:");
+        System.out.println("Enter the :");
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
