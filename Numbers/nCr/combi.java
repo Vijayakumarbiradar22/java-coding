c3a596dbc7abedd906f937e1bf772a5963885adb
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-29 21:54:23 +0530

    modified code

--- Numbers/nCr/combi.java
+++ Numbers/nCr/combi.java
@@ -4,8 +4,9 @@ public class combi {
     public static int fact(int n){
        int factorial=1;
        for(int i=2;i<=n;i++){
-        
+        factorial*=i;
        } 
+       return factorial;
     }
     // public static int fact(int n){
     //     int factorial=1;
