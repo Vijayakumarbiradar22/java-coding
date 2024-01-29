7d07f15c2700f7d4beae94cb28ecd22803a8899b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-29 21:47:38 +0530

    modified code

--- Numbers/leapyear/leap.java
+++ Numbers/leapyear/leap.java
@@ -5,7 +5,11 @@ public class leap {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter the Year:");
             int year = sc.nextInt();
-            
+            if(year%4==0 && year%100!=0 || year%400==0){
+                System.out.println("Leap year");
+            } else {
+                System.out.println("Not leap year.");
+            }
             // Scanner sc = new Scanner(System.in);
             // System.out.println("Enter the year:");
             // int year=sc.nextInt();
