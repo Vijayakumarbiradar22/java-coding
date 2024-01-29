e631467f5aea0d65f2821ed457a46ad94aaee345
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-29 21:54:42 +0530

    modified code

--- Numbers/nCr/combi.java
+++ Numbers/nCr/combi.java
@@ -8,13 +8,6 @@ public class combi {
        } 
        return factorial;
     }
-    // public static int fact(int n){
-    //     int factorial=1;
-    //     for(int i=2;i<=n;i++){
-    //         factorial*=i;
-    //     }
-    //     return factorial;
-    // }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter n:");
