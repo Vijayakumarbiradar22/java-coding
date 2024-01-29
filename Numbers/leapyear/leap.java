22f861c3d9325937d5f2d8ede407a7533df58386
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-29 21:48:45 +0530

    modified code

--- Numbers/leapyear/leap.java
+++ Numbers/leapyear/leap.java
@@ -10,14 +10,6 @@ public class leap {
             } else {
                 System.out.println("Not leap year.");
             }
-            // Scanner sc = new Scanner(System.in);
-            // System.out.println("Enter the year:");
-            // int year=sc.nextInt();
-            // if(year%4==0 && year%100!=0 || year%400==0 ){
-            //     System.out.println("Leap year");
-            // } else {
-            //     System.out.println("Not leap year");
-            // sc.close();
             sc.close();
         }
     }
