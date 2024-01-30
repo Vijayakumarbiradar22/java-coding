b0f07503d68a92da56078347825fe45cdf7144cd
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-30 22:20:44 +0530

    modified code

--- Arrays/repeatingele/repeat.java
+++ Arrays/repeatingele/repeat.java
@@ -14,7 +14,7 @@ public class repeat {
    
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array:");
+        System.out.println("Enter the array size:");
         int n = sc.nextInt();
         int arr[]=new int[n];
         System.out.println("Enter the elements:");
