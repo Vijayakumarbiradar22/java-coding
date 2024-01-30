69f0bf29e6312d05c7870e9e2f0ef11dcd1c7e75
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-30 22:21:28 +0530

    modified code

--- Arrays/sumclosezero/sumclose.java
+++ Arrays/sumclosezero/sumclose.java
@@ -22,7 +22,7 @@ public class sumclose {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array:");
+        System.out.println("Enter the :");
         int size=sc.nextInt();
         int arr[]=new int[size];
         System.out.println("Enter the elements of array:");
