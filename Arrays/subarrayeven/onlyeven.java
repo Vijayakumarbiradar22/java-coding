d1895c8cbe851a8b81065e0eb48988f16e83e86d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-31 21:21:20 +0530

    modified code

--- Arrays/subarrayeven/onlyeven.java
+++ Arrays/subarrayeven/onlyeven.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class onlyeven {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the array size:");
+        System.out.println("Enter the :");
         int n=sc.nextInt();
         int arr[]=new int[n];
         System.out.println("Enter the elements of array:");
