01f50a45959be26be205879b03959e19d516dd39
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-31 21:31:01 +0530

    modified code

--- Numbers/sumfl/sum.java
+++ Numbers/sumfl/sum.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class sum {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
         
         int num=1234;
         int rem,first=0;
