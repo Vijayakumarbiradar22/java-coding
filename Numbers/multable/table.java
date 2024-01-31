0826c7328ca4fb6968de055591c68edd4eb931f2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-31 21:39:13 +0530

    modified code

--- Numbers/multable/table.java
+++ Numbers/multable/table.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class table {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of multiple table:");
         int n=21;
         for(int i=1;i<=10;i++){
             System.out.println(n+"*"+i+"="+n*i);
