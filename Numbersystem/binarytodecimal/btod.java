1b341753710637044ea1ce48f7887707d5b081bb
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-31 21:44:50 +0530

    modified code

--- Numbersystem/binarytodecimal/btod.java
+++ Numbersystem/binarytodecimal/btod.java
@@ -14,8 +14,8 @@ public class btod {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the binary number:");
-        
-        String binary = "1011";
+        String binary = sc.nextLine();
+        // String binary = "1011";
         int decimalNumber = convertToDecimal(binary);
         System.out.println(decimalNumber);
     }
