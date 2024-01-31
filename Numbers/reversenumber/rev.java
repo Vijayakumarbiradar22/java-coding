8deb69a673899211047e180737e0cadb7a1910ad
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-31 21:36:46 +0530

    modified code

--- Numbers/reversenumber/rev.java
+++ Numbers/reversenumber/rev.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class rev {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=sc.nextInt();
         int rem,revNum=0;
-        int num=1234;
         int temp=num;
         while(num>0){
             rem=num%10;
