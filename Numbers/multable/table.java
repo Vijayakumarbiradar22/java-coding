4b338570240fa2e66ac119fc724ac521c85620a3
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-01-31 21:38:34 +0530

    modified code

--- Numbers/multable/table.java
+++ Numbers/multable/table.java
@@ -1,7 +1,8 @@
 package Numbers.multable;
-
+import java.util.Scanner;
 public class table {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         int n=21;
         for(int i=1;i<=10;i++){
             System.out.println(n+"*"+i+"="+n*i);
