498892aafbbe4efb000780c4f4f4e75865013300
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-01 21:50:45 +0530

    modified code

--- Bitmanupulation/singlenumber/singnum.java
+++ Bitmanupulation/singlenumber/singnum.java
@@ -12,17 +12,17 @@ public class singnum {
         return ans;
     }
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array:");
-        int n=sc.nextInt();
-        int nums[]=new int[n];
-        System.out.println("Enter the eleements:");
-        for(int i=0;i<n;i++){
-            nums[i]=sc.nextInt();
-        }
-        int res = singleNumber(nums);
-        System.out.println(res);
-        sc.close();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the size of array:");
+        // int n=sc.nextInt();
+        // int nums[]=new int[n];
+        // System.out.println("Enter the eleements:");
+        // for(int i=0;i<n;i++){
+        //     nums[i]=sc.nextInt();
+        // }
+        // int res = singleNumber(nums);
+        // System.out.println(res);
+        // sc.close();
     }
     
 }
