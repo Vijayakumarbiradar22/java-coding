59bb31f63acae8230ff7c6a1806aab291ed8abe0
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-01 21:52:33 +0530

    modified code

--- Bitmanupulation/singlenumber/singnum.java
+++ Bitmanupulation/singlenumber/singnum.java
@@ -13,6 +13,7 @@ public class singnum {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the size of array:");
         // int n=sc.nextInt();
