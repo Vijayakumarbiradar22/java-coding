c6c9bdc448bc928815b1621273f1990134ec3d25
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-02 21:57:35 +0530

    modified code

--- Stack/pushatbottom/pab.java
+++ Stack/pushatbottom/pab.java
@@ -15,13 +15,10 @@ public class pab {
         s.push(1);
         s.push(2);
         s.push(3);
-
         pushAtBottom(4,s);
-
         while(!s.isEmpty()){
             System.out.println(s.peek());
             s.pop();
         }
     }
-    
 }
