46212d46aa2cfbb16ca9f66cd855ddb0aa3aca7e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-02 21:59:18 +0530

    modified code

--- Stack/reverse/rev.java
+++ Stack/reverse/rev.java
@@ -5,13 +5,10 @@ public class rev {
         if(s.isEmpty()){
             return;
         }
-
         int top=s.pop();
         reverse(s);
         pushAtBottom(top,s);
-
     }
-
     public static void pushAtBottom(int data, Stack<Integer> s){
         if(s.isEmpty()){
             s.push(data);
@@ -26,9 +23,7 @@ public class rev {
         s.push(1);
         s.push(2);
         s.push(3);
-
         reverse(s);
-
         while(!s.isEmpty()){
             System.out.println(s.peek());
             s.pop();
