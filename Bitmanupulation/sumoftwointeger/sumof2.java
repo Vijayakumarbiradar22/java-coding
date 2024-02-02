471e1398f4d5da2a15b825be691bc827eebb94ad
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-02 21:13:02 +0530

    modified code

--- Bitmanupulation/sumoftwointeger/sumof2.java
+++ Bitmanupulation/sumoftwointeger/sumof2.java
@@ -14,7 +14,7 @@ public class sumof2 {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a value:");
+        System.out.println("Enter a value of a:");
         int a = sc.nextInt();
         System.out.println("Enter the value of b:");
         int b=sc.nextInt();
