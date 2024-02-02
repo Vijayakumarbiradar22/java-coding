78abe827033aa896a34119d49d1f2815e96fb90f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-02 22:07:06 +0530

    modified code

--- Patterns/Numbers/oddevenchange/change.java
+++ Patterns/Numbers/oddevenchange/change.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class change {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the rows:");
+        System.out.println("Enter the number of rows:");
         int n=sc.nextInt();
 
         for(int i=1;i<=n;i++){
