4b92010403de1d3131b85db013f1427451482e06
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-02 21:28:36 +0530

    modified code

--- Strings/replace/rep.java
+++ Strings/replace/rep.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class rep {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String:");
         String str="apple";
         String res="";
 
