5782454650593fa2a06fe99160e18c64f42cd06f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-03 22:22:46 +0530

    modified code

--- Patterns/Stars/starplus/sp.java
+++ Patterns/Stars/starplus/sp.java
@@ -2,6 +2,8 @@ package Patterns.Stars.starplus;
 import java.util.Scanner;
 public class sp {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
         // int n=5;
         // for(int i=1;i<=n;i++){
         //     for(int j=1;j<=n;j++){
