32e47138f6161c33994d929ad9cd956642346ae2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-03 22:20:20 +0530

    modified code

--- Patterns/Stars/cross/cr.java
+++ Patterns/Stars/cross/cr.java
@@ -14,22 +14,8 @@ public class cr {
                     System.out.print(" ");
                 }
             }
+            System.out.println();
         }
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter the total rows:");
-        // int n=sc.nextInt();
-
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=n;j++){
-        //         if(i==j || i+j==n-1){
-        //             System.out.print("*");
-        //         } else {
-        //             System.out.print(" ");
-        //         }
-        //     }
-        //     System.out.println();
-        // }
-        // sc.close();
     }
     
 }
