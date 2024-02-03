19741a22f655ef91ff9d825bdb2ffa002f68ff3e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-03 22:22:19 +0530

    modified code

--- Patterns/Stars/starplus/sp.java
+++ Patterns/Stars/starplus/sp.java
@@ -1,18 +1,18 @@
 package Patterns.Stars.starplus;
-
+import java.util.Scanner;
 public class sp {
     public static void main(String args[]){
-        int n=5;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(i==(n/2)+1 || j==(n/2)+1){
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+        // int n=5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         if(i==(n/2)+1 || j==(n/2)+1){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
     }
     
 }
