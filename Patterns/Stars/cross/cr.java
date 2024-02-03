503172d76d06e39c1d304a3d406a835341f155d6
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-03 22:19:14 +0530

    modified code

--- Patterns/Stars/cross/cr.java
+++ Patterns/Stars/cross/cr.java
@@ -8,7 +8,9 @@ public class cr {
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                
+                if(i==j || i+j==n-1){
+                    
+                }
             }
         }
         // Scanner sc = new Scanner(System.in);
