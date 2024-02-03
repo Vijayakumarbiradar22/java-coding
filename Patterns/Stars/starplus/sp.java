8ada1e7dc7482195e203961b1ab4bc6f20f3744d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-03 22:25:18 +0530

    modified code

--- Patterns/Stars/starplus/sp.java
+++ Patterns/Stars/starplus/sp.java
@@ -9,8 +9,10 @@ public class sp {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                if(i==(n/2)+1 || j==(n/2)+1){
-                
-               } 
+                System.out.print("*");
+               } else {
+                System.out.print(" ");
+               }
             }
         }
         // int n=5;
