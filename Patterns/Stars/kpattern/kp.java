22566a412d5b210afd1a314629b445b0f030dcda
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-04 21:17:21 +0530

    modified code

--- Patterns/Stars/kpattern/kp.java
+++ Patterns/Stars/kpattern/kp.java
@@ -2,24 +2,25 @@ package Patterns.Stars.kpattern;
 import java.util.Scanner;
 public class kp {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the total rows:");
-        int n=sc.nextInt();
+        
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the total rows:");
+        // int n=sc.nextInt();
 
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
 
-        for(int i=2;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        sc.close();
+        // for(int i=2;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // sc.close();
     }
     
 }
