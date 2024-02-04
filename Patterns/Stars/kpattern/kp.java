ebfc73192cc95a09bf3a664e35df50eb9557fd12
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-04 21:27:16 +0530

    modified code

--- Patterns/Stars/kpattern/kp.java
+++ Patterns/Stars/kpattern/kp.java
@@ -14,30 +14,11 @@ public class kp {
         }
 
         for(int i=2;i<=n;i++){
-            for(int j=1;i<=i;j++){
+            for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
-
-;        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter the total rows:");
-        // int n=sc.nextInt();
-
-        // for(int i=n;i>=1;i--){
-        //     for(int j=1;j<=i;j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
-
-        // for(int i=2;i<=n;i++){
-        //     for(int j=1;j<=i;j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
-        // sc.close();
         sc.close();
     }
     
