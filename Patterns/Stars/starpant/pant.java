fec5556ec429df6fa8805f5b019e08e885b93baf
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-05 21:46:40 +0530

    modified code

--- Patterns/Stars/starpant/pant.java
+++ Patterns/Stars/starpant/pant.java
@@ -2,25 +2,26 @@ package Patterns.Stars.starpant;
 import java.util.*;
 public class pant {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the total rows:");
-        int n=sc.nextInt();
-        int x=n;
-        int y=n;
+        Scanner sc  = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the total rows:");
+        // int n=sc.nextInt();
+        // int x=n;
+        // int y=n;
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<n*2;j++){
-                if(j>x&&j<y){
-                    System.out.print(" ");
-                } else {
-                    System.out.print("*");
-                }
-            }
-            x--;
-            y++;
-            System.out.println();
-        }
-        sc.close();
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<n*2;j++){
+        //         if(j>x&&j<y){
+        //             System.out.print(" ");
+        //         } else {
+        //             System.out.print("*");
+        //         }
+        //     }
+        //     x--;
+        //     y++;
+        //     System.out.println();
+        // }
+        // sc.close();
     }
     
 }
