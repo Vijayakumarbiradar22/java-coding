0fa256247c57fcba2b298c152e2c830f6887a2fb
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-05 21:55:42 +0530

    modified code

--- Patterns/Stars/inverthallowtri/invhaltri.java
+++ Patterns/Stars/inverthallowtri/invhaltri.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class invhaltri {
     public static void main(String args[]){
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the rows:");
+            System.out.println("Enter the no.of rows:");
             int n=sc.nextInt();
             for(int i=n;i>=1;i--){
                 for(int j=1;j<=2*n-1;j++){
