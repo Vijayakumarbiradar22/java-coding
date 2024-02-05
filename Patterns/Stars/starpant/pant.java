bee00c7c3a50eee0c47b2d9af8d8d4e9b71582c7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-05 21:51:22 +0530

    modified code

--- Patterns/Stars/starpant/pant.java
+++ Patterns/Stars/starpant/pant.java
@@ -21,25 +21,6 @@ public class pant {
             System.out.println();
         }
         sc.close();
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter the total rows:");
-        // int n=sc.nextInt();
-        // int x=n;
-        // int y=n;
-
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<n*2;j++){
-        //         if(j>x&&j<y){
-        //             System.out.print(" ");
-        //         } else {
-        //             System.out.print("*");
-        //         }
-        //     }
-        //     x--;
-        //     y++;
-        //     System.out.println();
-        // }
-        // sc.close();
     }
     
 }
