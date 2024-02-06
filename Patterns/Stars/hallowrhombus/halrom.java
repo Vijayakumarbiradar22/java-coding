248761d55ae826648caae964ecd49514727c195e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-06 20:07:45 +0530

    modified code

--- Patterns/Stars/hallowrhombus/halrom.java
+++ Patterns/Stars/hallowrhombus/halrom.java
@@ -19,7 +19,9 @@ public class halrom {
 
         for(int i=n-2;i>=1;i--){
            for(int j=1;j<=2*n-1;j++){
-            
+            if(j==n-i+1 || j==n+i-1 || i==n){
+                
+            }
            } 
         }
 
