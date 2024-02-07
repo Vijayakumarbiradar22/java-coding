fe7e432a3086dfcb20a7d4774d7a6cb2cece359b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-07 19:39:24 +0530

    changed code

--- Patterns/Stars/butterfly/fly.java
+++ Patterns/Stars/butterfly/fly.java
@@ -35,7 +35,7 @@ public class fly {
             }
 
             for(int j=1;j<=i;j++){
-                
+               System.out.print("*");
             }
         }
         // for(int i=1;i<=n;i++){
