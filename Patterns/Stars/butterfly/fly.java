ae584ad3894444680bc8035ffc63925ac419d565
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-07 19:31:04 +0530

    changed code

--- Patterns/Stars/butterfly/fly.java
+++ Patterns/Stars/butterfly/fly.java
@@ -1,46 +1,45 @@
 package Patterns.Stars.butterfly;
-
+import java.util.Scanner;
 public class fly {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         int n=4;
         //Upper part
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
 
-            int sp = 2*(n-i);
-            for(int s=1;s<=sp;s++){
-                System.out.print(" ");
-            }
+        //     int sp = 2*(n-i);
+        //     for(int s=1;s<=sp;s++){
+        //         System.out.print(" ");
+        //     }
 
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
 
-            System.out.println();
-        }
+        //     System.out.println();
+        // }
 
-            //lower part
+        //     //lower part
 
-            for(int i=n;i>=1;i--){
-                for(int j=1;j<=i;j++){
-                    System.out.print("*");
-                }
+        //     for(int i=n;i>=1;i--){
+        //         for(int j=1;j<=i;j++){
+        //             System.out.print("*");
+        //         }
                 
-                int sp=2*(n-i);
-                for(int s=1;s<=sp;s++){
-                    System.out.print(" ");
-                }
+        //         int sp=2*(n-i);
+        //         for(int s=1;s<=sp;s++){
+        //             System.out.print(" ");
+        //         }
 
-                for(int j=1;j<=i;j++){
-                    System.out.print("*");
-                }
+        //         for(int j=1;j<=i;j++){
+        //             System.out.print("*");
+        //         }
 
-                System.out.println();
-            }
-
-        
+        //         System.out.println();
+        //     }
     }
     
 }
