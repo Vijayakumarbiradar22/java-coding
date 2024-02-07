8a050355a0856274cad7ceaa097c0847d96bbfb2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-07 19:34:40 +0530

    changed code

--- Patterns/Stars/butterfly/fly.java
+++ Patterns/Stars/butterfly/fly.java
@@ -6,8 +6,11 @@ public class fly {
         System.out.println("Enter the number of rows:");
         int n= sc.nextInt();
 
-        
         //Upper part
+
+        for(int i=1;i<=n;i++){
+            
+        }
         // for(int i=1;i<=n;i++){
         //     for(int j=1;j<=i;j++){
         //         System.out.print("*");
