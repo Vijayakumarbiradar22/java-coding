4c3cd15faa6ff35c8d36cd9112d7a14139f03582
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-07 19:33:56 +0530

    changed code

--- Patterns/Stars/butterfly/fly.java
+++ Patterns/Stars/butterfly/fly.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class fly {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int n=4;
+        System.out.println("Enter the number of rows:");
+        int n= sc.nextInt();
+
+        
         //Upper part
         // for(int i=1;i<=n;i++){
         //     for(int j=1;j<=i;j++){
