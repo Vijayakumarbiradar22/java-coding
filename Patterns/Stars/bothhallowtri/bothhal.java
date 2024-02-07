54841c8262eb1aa0fa8c59afa1105f83a46561ff
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-07 19:46:48 +0530

    changed code

--- Patterns/Stars/bothhallowtri/bothhal.java
+++ Patterns/Stars/bothhallowtri/bothhal.java
@@ -8,9 +8,12 @@ public class bothhal {
         for(int i=n;i>=1;i--){
             for(int j=1;j<=2*n-1;j++){
                 if(j==n+i-1 || j==n-i+1 || i==n){
-                    
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
                 }
             }
+            
         }
         // System.out.println("Enter the rows:");
         // int n=sc.nextInt();
