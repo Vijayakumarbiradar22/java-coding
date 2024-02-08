c68a5bb7db474e49b8c302838fa545e5a627d7b5
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-08 21:34:09 +0530

    changed code

--- Patterns/Stars/combinedk/comk.java
+++ Patterns/Stars/combinedk/comk.java
@@ -3,42 +3,44 @@ import java.util.Scanner;
 public class comk {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
-        int n=sc.nextInt();
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            for(int s=1;s<=n-i;s++){
-                System.out.print(" ");
-            }
+        
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the number of rows:");
+        // int n=sc.nextInt();
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int s=1;s<=n-i;s++){
+        //         System.out.print(" ");
+        //     }
 
-            for(int s=1;s<=n-i;s++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        //     for(int s=1;s<=n-i;s++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
 
-        for(int i=2;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            for(int s=1;s<=n-i;s++){
-                System.out.print(" ");
-            }
+        // for(int i=2;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int s=1;s<=n-i;s++){
+        //         System.out.print(" ");
+        //     }
 
-            for(int s=1;s<=n-i;s++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        sc.close();
+        //     for(int s=1;s<=n-i;s++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // sc.close();
     }
     
 }
