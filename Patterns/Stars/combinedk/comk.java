ea8548dddc22624aa158eeac645fffd1f63c53f1
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-08 21:39:40 +0530

    changed code

--- Patterns/Stars/combinedk/comk.java
+++ Patterns/Stars/combinedk/comk.java
@@ -7,7 +7,9 @@ public class comk {
         int n = sc.nextInt();
 
         for(int i=n;i>=1;i--){
-            
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
         }
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the number of rows:");
