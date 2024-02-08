124cf3820dcf076d2effbdf9239caf993196e1fb
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-08 21:40:11 +0530

    changed code

--- Patterns/Stars/combinedk/comk.java
+++ Patterns/Stars/combinedk/comk.java
@@ -7,6 +7,9 @@ public class comk {
         int n = sc.nextInt();
 
         for(int i=n;i>=1;i--){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
