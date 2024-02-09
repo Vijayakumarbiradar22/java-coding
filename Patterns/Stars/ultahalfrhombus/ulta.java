97b763f300be4c1f9c637a374323eb0291d3338b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-09 22:01:20 +0530

    code changed

--- Patterns/Stars/ultahalfrhombus/ulta.java
+++ Patterns/Stars/ultahalfrhombus/ulta.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ulta {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no.of rows:");
+        System.out.println("Enter the number of rows:");
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int s=1;s<=n-i;s++){
@@ -25,6 +25,5 @@ public class ulta {
             System.out.println();
         }
         sc.close();
-    }
-    
+    } 
 }
