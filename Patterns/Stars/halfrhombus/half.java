02f968d73e73e07a7e17c341dc4e569b8e69baa4
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-09 21:40:20 +0530

    code changed

--- Patterns/Stars/halfrhombus/half.java
+++ Patterns/Stars/halfrhombus/half.java
@@ -12,7 +12,7 @@ public class half {
             System.out.println();
         }
 
-        for(int i=n;i>=1;i--){
+        for(int i=n-1;i>=1;i--){
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
