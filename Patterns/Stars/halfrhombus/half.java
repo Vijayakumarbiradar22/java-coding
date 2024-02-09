73398f266a75dd018ea5e9f86a61c48fab88574e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-09 21:38:01 +0530

    code changed

--- Patterns/Stars/halfrhombus/half.java
+++ Patterns/Stars/halfrhombus/half.java
@@ -5,6 +5,13 @@ public class half {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
         int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
         
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the no.of rows:");
