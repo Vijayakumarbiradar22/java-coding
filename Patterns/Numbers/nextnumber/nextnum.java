1c21252a5952bb42bcde3f406d2847d75349d931
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-10 21:55:27 +0530

    modified code

--- Patterns/Numbers/nextnumber/nextnum.java
+++ Patterns/Numbers/nextnumber/nextnum.java
@@ -5,7 +5,15 @@ public class nextnum {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number pof rows:");
         int n = sc.nextInt();
-        
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                
+            }
+        }
+
+        sc.close();
+
     }
     
 }
