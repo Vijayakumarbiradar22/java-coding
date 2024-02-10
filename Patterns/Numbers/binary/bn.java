46a7bd06c5159c7c7770d72ca268f5a6780ae55d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-10 21:40:14 +0530

    modified code

--- Patterns/Numbers/binary/bn.java
+++ Patterns/Numbers/binary/bn.java
@@ -5,7 +5,12 @@ public class bn {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
         int n = sc.nextInt();
-        
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                
+            }
+        }
         // Scanner sc = new Scanner(System.in);
         //     System.out.println("Enter the no.of rows:");
         //     int n=sc.nextInt();
