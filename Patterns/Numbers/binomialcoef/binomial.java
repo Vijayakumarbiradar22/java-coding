346a8302ebc3ee07dbb5d14084aad1d0df6b89c9
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-10 21:46:52 +0530

    modified code

--- Patterns/Numbers/binomialcoef/binomial.java
+++ Patterns/Numbers/binomialcoef/binomial.java
@@ -14,8 +14,9 @@ public class binomial {
         int n = sc.nextInt();
         for(int i=0;i<n;i++){
             for(int j=0;j<=i;j++){
-                System.out.println();
+                System.out.print(fact(i)/(fact(j)*(fact(i-j))));
             }
+            System.out.println();
         }
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the number of rows:");
