7d23b8943dba67a7807c669cf94f2542e7330d64
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-10 21:56:14 +0530

    modified code

--- Patterns/Numbers/nextnumber/nextnum.java
+++ Patterns/Numbers/nextnumber/nextnum.java
@@ -9,7 +9,9 @@ public class nextnum {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(num);
+                num++;
             }
+            
         }
 
         sc.close();
