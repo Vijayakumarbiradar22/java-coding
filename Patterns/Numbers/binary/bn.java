afa94802e03691f78d216728b868e3a6d959f9c6
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-10 21:41:38 +0530

    modified code

--- Patterns/Numbers/binary/bn.java
+++ Patterns/Numbers/binary/bn.java
@@ -8,9 +8,17 @@ public class bn {
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                
+                int sum=i+j;
+                if(sum%2==0){
+                    System.out.print("1");
+                } else {
+                   System.out.print("0"); 
+                }
             }
+            System.out.println();
         }
+
+        
         // Scanner sc = new Scanner(System.in);
         //     System.out.println("Enter the no.of rows:");
         //     int n=sc.nextInt();
