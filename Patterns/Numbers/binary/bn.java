0a7511890bd176d434b0f6d01722395eefdacde2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-10 21:39:45 +0530

    modified code

--- Patterns/Numbers/binary/bn.java
+++ Patterns/Numbers/binary/bn.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class bn {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n = sc.nextInt();
         
         // Scanner sc = new Scanner(System.in);
         //     System.out.println("Enter the no.of rows:");
