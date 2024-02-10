67016b1613e2f6714a8f9aea7e6e795d0bffbd5f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-10 21:54:40 +0530

    modified code

--- Patterns/Numbers/nextnumber/nextnum.java
+++ Patterns/Numbers/nextnumber/nextnum.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class nextnum {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number pof rows:");
+        int n = sc.nextInt();
         
     }
     
