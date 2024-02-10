5fddb91a1bb5b3ca670e7fbea5ec5cbd88004ae2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-10 22:04:22 +0530

    modified code

--- Patterns/Numbers/primenumber/prime.java
+++ Patterns/Numbers/primenumber/prime.java
@@ -5,7 +5,9 @@ public class prime {
        if(num<2){
         return false;
        } 
-       
+       for(int i=2;i<=Math.sqrt(num);i++){
+        
+       }
     }
     // public static boolean isPrime(int num){
     //     if(num<2){
