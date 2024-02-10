5d5bc12d2586a52675f200917bdb6dfe744a55f1
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-10 21:42:42 +0530

    modified code

--- Patterns/Numbers/binary/bn.java
+++ Patterns/Numbers/binary/bn.java
@@ -17,24 +17,7 @@ public class bn {
             }
             System.out.println();
         }
-
-        
-        // Scanner sc = new Scanner(System.in);
-        //     System.out.println("Enter the no.of rows:");
-        //     int n=sc.nextInt();
-        //     for(int i=1;i<=n;i++){
-        //         for(int j=1;j<=i;j++){
-        //             int sum=i+j;
-        //             if(sum%2==0){
-        //                 System.out.print("1");
-        //             } else {
-        //                 System.out.print("0");
-        //             }
-                    
-        //         }
-        //         System.out.println();
-        //     }
-        // }
+        sc.close();
     }
     
 }
