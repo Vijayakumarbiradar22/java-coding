74c33b6dd8aa57ca190ebde28f3a39e9df4bdf58
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-10 22:03:20 +0530

    modified code

--- Patterns/Numbers/primenumber/prime.java
+++ Patterns/Numbers/primenumber/prime.java
@@ -2,16 +2,19 @@ package Patterns.Numbers.primenumber;
 import java.util.Scanner;
 public class prime {
     public static boolean isPrime(int num){
-        if(num<2){
-            return false;
-        }
-        for(int i=2;i<=Math.sqrt(num);i++){
-            if(num%i==0){
-                return false;
-            }
-        }
-        return true;
+        
     }
+    // public static boolean isPrime(int num){
+    //     if(num<2){
+    //         return false;
+    //     }
+    //     for(int i=2;i<=Math.sqrt(num);i++){
+    //         if(num%i==0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 
     public static void primePattern(int rows){
         int currNum=1;
@@ -25,17 +28,6 @@ public class prime {
            } 
            System.out.println();
         }
-    //     int currNum=1;
-    //   for(int i=1;i<=rows;i++){
-    //         for(int j=1;j<=i;j++){
-    //             while(!isPrime(currNum)){
-    //                 currNum++;
-    //             }
-    //             System.out.print(currNum+" ");
-    //             currNum++;
-    //         }
-    //         System.out.println();
-    //     }
     }
 
     public static void main(String args[]){
