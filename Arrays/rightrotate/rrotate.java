31ec13c4224cc550bd969db08740dc9f6575a66d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-11 16:10:45 +0530

    changed code

--- Arrays/rightrotate/rrotate.java
+++ Arrays/rightrotate/rrotate.java
@@ -4,7 +4,11 @@ public class rrotate {
     public static void rightRotate(int arr[],int n,int rt){
         int p=1;
         while(p<=rt){
-            
+            int first=arr[n-1];
+
+            for(int i=n-2;i>=0;i--){
+                
+            }
         }
         
         // int help=1;
