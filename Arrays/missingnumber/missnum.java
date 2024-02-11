5fadcde0692906d05f718e30d44e7baa1174f571
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-11 16:04:03 +0530

    changed code

--- Arrays/missingnumber/missnum.java
+++ Arrays/missingnumber/missnum.java
@@ -8,6 +8,8 @@ public class missnum {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
         int n=sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the array elements:");
         
     }
 }
