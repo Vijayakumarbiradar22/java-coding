0107a518a92032bf9e8a26573cc192cba079bf6f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-11 16:04:32 +0530

    changed code

--- Arrays/missingnumber/missnum.java
+++ Arrays/missingnumber/missnum.java
@@ -10,6 +10,10 @@ public class missnum {
         int n=sc.nextInt();
         int[] arr = new int[n];
         System.out.println("Enter the array elements:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
         
     }
 }
