4de6ae06d46f46b2cb827375d7f61ffeefd23830
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-11 16:11:55 +0530

    changed code

--- Arrays/rightrotate/rrotate.java
+++ Arrays/rightrotate/rrotate.java
@@ -8,7 +8,8 @@ public class rrotate {
             for(int i=n-2;i>=0;i--){
                 arr[i+1]=arr[i];
             }
-            
+            arr[0]=first;
+            p++;
         }
         
         // int help=1;
