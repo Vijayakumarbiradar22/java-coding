c0b3b31fbc3621e7c843cd3798c4f83f654437f6
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-12 21:25:41 +0530

    code changed

--- Strings/reverse/rev.java
+++ Strings/reverse/rev.java
@@ -5,9 +5,7 @@ public class rev {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the string:");
         String str = sc.nextLine();
-
         StringBuilder sb = new StringBuilder(str);
-        
         for(int i=0;i<sb.length()/2;i++){
             int front=i;
             int back=sb.length()-1-i;
