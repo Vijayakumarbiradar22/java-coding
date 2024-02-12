64ec16a1668a1e9b28e8d1e301f3485ff87b9eb4
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-12 21:17:54 +0530

    code changed

--- Strings/reverse/rev.java
+++ Strings/reverse/rev.java
@@ -3,23 +3,25 @@ import java.util.Scanner;
 public class rev {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string:");
-        String str=sc.nextLine();
+        
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("Enter the string:");
+    //     String str=sc.nextLine();
 
-        StringBuilder sb = new StringBuilder(str);
+    //     StringBuilder sb = new StringBuilder(str);
 
-        for(int i=0;i<sb.length()/2;i++){
-            int front=i;
-            int back =sb.length()-1-i;
+    //     for(int i=0;i<sb.length()/2;i++){
+    //         int front=i;
+    //         int back =sb.length()-1-i;
 
-            char frontchar = sb.charAt(front);
-            char backchar = sb.charAt(back);
+    //         char frontchar = sb.charAt(front);
+    //         char backchar = sb.charAt(back);
 
-            sb.setCharAt(front, backchar);
-            sb.setCharAt(back, frontchar);
-        }
-        System.out.println(sb);
-    sc.close();
+    //         sb.setCharAt(front, backchar);
+    //         sb.setCharAt(back, frontchar);
+    //     }
+    //     System.out.println(sb);
+    // sc.close();
 
     }
     
