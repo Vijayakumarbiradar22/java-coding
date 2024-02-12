766f4c33f6adb5a66d223301fcf3482a3633749b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-12 21:09:34 +0530

    code changed

--- Strings/compare/com.java
+++ Strings/compare/com.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class com {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string 1:");
+        String name1=sc.nextLine();
         
         //  Scanner sc = new Scanner(System.in);
         //     System.out.println("Enter first string:");
