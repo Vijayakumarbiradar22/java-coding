0d0e3ee4a9f79ef6c7275bdabde34ae1966a4059
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-12 21:05:43 +0530

    code changed

--- Strings/compare/com.java
+++ Strings/compare/com.java
@@ -2,18 +2,20 @@ package Strings.compare;
 import java.util.Scanner;
 public class com {
     public static void main(String args[]){
-         Scanner sc = new Scanner(System.in);
-            System.out.println("Enter first string:");
-            String name1 = sc.nextLine();
-            System.out.println("Enter  second string:");
-            String name2 = sc.nextLine();
-            //do not use "==" for compare some code like following gives wrong result
-             //if(new String("vijay")==new String("vijay")){
-            if(name1.equals(name2)){
-                System.out.println("Strings are same");
-            } else {
-                System.out.println("Strings are not same");
-            }
-        sc.close();
+        Scanner sc = new Scanner(System.in);
+        
+        //  Scanner sc = new Scanner(System.in);
+        //     System.out.println("Enter first string:");
+        //     String name1 = sc.nextLine();
+        //     System.out.println("Enter  second string:");
+        //     String name2 = sc.nextLine();
+        //     //do not use "==" for compare some code like following gives wrong result
+        //      //if(new String("vijay")==new String("vijay")){
+        //     if(name1.equals(name2)){
+        //         System.out.println("Strings are same");
+        //     } else {
+        //         System.out.println("Strings are not same");
+        //     }
+        // sc.close();
     }  
 }
