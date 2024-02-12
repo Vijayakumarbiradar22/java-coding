0dc43dbfbf56c2606c35f22bf28fcead9de11a68
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-12 21:21:11 +0530

    code changed

--- Strings/reverse/rev.java
+++ Strings/reverse/rev.java
@@ -8,6 +8,9 @@ public class rev {
 
         StringBuilder sb = new StringBuilder(str);
         
+        for(int i=0;i<sb.length()/2;i++){
+            
+        }
     //     Scanner sc = new Scanner(System.in);
     //     System.out.println("Enter the string:");
     //     String str=sc.nextLine();
