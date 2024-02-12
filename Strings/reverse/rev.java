c57b7454f83ae8f3c1647ca49d440836346fb1a2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-12 21:26:06 +0530

    code changed

--- Strings/reverse/rev.java
+++ Strings/reverse/rev.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class rev {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string:");
+        System.out.println("Enter the string to reverse:");
         String str = sc.nextLine();
         StringBuilder sb = new StringBuilder(str);
         for(int i=0;i<sb.length()/2;i++){
