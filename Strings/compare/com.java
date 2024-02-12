fe394c5b3427bc12ac711173a4a3b6bd2b3b572b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-12 21:13:26 +0530

    code changed

--- Strings/compare/com.java
+++ Strings/compare/com.java
@@ -7,25 +7,11 @@ public class com {
         String name1=sc.nextLine();
         System.out.println("Enter the string 2:");
         String name2=sc.nextLine();
-
         if(name1.equals(name2)){
             System.out.println("Strings are same.");
         } else{
            System.out.println("Strings are not same."); 
         }
         sc.close();
-        //  Scanner sc = new Scanner(System.in);
-        //     System.out.println("Enter first string:");
-        //     String name1 = sc.nextLine();
-        //     System.out.println("Enter  second string:");
-        //     String name2 = sc.nextLine();
-        //     //do not use "==" for compare some code like following gives wrong result
-        //      //if(new String("vijay")==new String("vijay")){
-        //     if(name1.equals(name2)){
-        //         System.out.println("Strings are same");
-        //     } else {
-        //         System.out.println("Strings are not same");
-        //     }
-        // sc.close();
     }  
 }
