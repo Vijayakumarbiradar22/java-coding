12b68c5a00c38ee9bfc5f7052e11dc0504b172cb
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-12 21:25:03 +0530

    code changed

--- Strings/reverse/rev.java
+++ Strings/reverse/rev.java
@@ -20,26 +20,5 @@ public class rev {
         }
         System.out.println(sb);
         sc.close();
-        
-    //     Scanner sc = new Scanner(System.in);
-    //     System.out.println("Enter the string:");
-    //     String str=sc.nextLine();
-
-    //     StringBuilder sb = new StringBuilder(str);
-
-    //     for(int i=0;i<sb.length()/2;i++){
-    //         int front=i;
-    //         int back =sb.length()-1-i;
-
-    //         char frontchar = sb.charAt(front);
-    //         char backchar = sb.charAt(back);
-
-    //         sb.setCharAt(front, backchar);
-    //         sb.setCharAt(back, frontchar);
-    //     }
-    //     System.out.println(sb);
-    // sc.close();
-
-    }
-    
+    }   
 }
