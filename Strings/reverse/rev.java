a4ee25a349dcd59523e4687a9932a021db76de12
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-12 21:22:23 +0530

    code changed

--- Strings/reverse/rev.java
+++ Strings/reverse/rev.java
@@ -10,6 +10,9 @@ public class rev {
         
         for(int i=0;i<sb.length()/2;i++){
             int front=i;
+            int back=sb.length()-1-i;
+
+            char frontchar=sb.charAt(front);
             
         }
     //     Scanner sc = new Scanner(System.in);
