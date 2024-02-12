2a7982772aac0d54a39e96bb53c5631c250912a2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-12 21:27:20 +0530

    code changed

--- Strings/countconsonant/conso.java
+++ Strings/countconsonant/conso.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class conso {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the string:");
+            System.out.println("Enter the String:");
             String str = sc.nextLine();
             int count=0;
             for(int i=0;i<str.length();i++){
