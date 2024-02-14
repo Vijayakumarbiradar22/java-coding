2b01f6fcb2d5569fb0adf85a99a2afa8ddff391a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-14 21:21:04 +0530

    added new code

--- Arrays/transpose/trans.java
+++ Arrays/transpose/trans.java
@@ -23,6 +23,13 @@ public class trans {
             mat[j][i]=temp;
            } 
         }
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                System.out.print(mat[i][j]+" ");
+            }
+            
+        }
     }
     
 }
