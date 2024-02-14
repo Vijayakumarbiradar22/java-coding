9ee838ea8c76004713ce4ea54907863ce4921320
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-14 21:12:08 +0530

    added new code

--- Arrays/transpose/trans.java
+++ Arrays/transpose/trans.java
@@ -2,6 +2,7 @@ package Arrays.transpose;
 import java.util.Scanner;
 public class trans {
     public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
         
     }
     
