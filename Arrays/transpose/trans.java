c963e23064f43efac7c49a728abf0a5300c9bc56
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-14 21:13:06 +0530

    added new code

--- Arrays/transpose/trans.java
+++ Arrays/transpose/trans.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class trans {
     public static void main(String args[]){
       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the size of matrix:");
+       int n=sc.nextInt();
         
     }
     
