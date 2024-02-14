cd45f85f012a162e24a3a89516215c17ae3f0d31
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-14 21:15:03 +0530

    added new code

--- Arrays/transpose/trans.java
+++ Arrays/transpose/trans.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class trans {
     public static void main(String args[]){
       Scanner sc = new Scanner(System.in);
-       System.out.println("Enter the size of matrix:");
-       int n=sc.nextInt();
-        int mat[]=new int[n];
+       System.out.println("Enter the number of rows:");
+       int r=sc.nextInt();
+       System.out.println("Enter the number of columns:");
+       int c=sc.nextInt();
+        int mat[][]=new int[r][c];
         System.out.println("Enter the elements of matrix:");
-        
+        for(int i=0;i<r;i++){
+            for(int j=0;j<)
+        }
     }
     
 }
