f651ad380f81ef1a7347101b5e8463a60a3a0fc9
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-15 15:22:06 +0530

    added new code

---
+++ Arrays/recordday/rec.java
@@ -0,0 +1,9 @@
+package Arrays.recordday;
+import java.util.Scanner;
+public class rec {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+}
