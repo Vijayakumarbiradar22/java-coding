e4321d6b045ea5c4f66c508ac56440c311e540d2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-15 15:28:37 +0530

    added new code

--- Arrays/recordday/rec.java
+++ Arrays/recordday/rec.java
@@ -17,8 +17,14 @@ public class rec {
 
         int ans=0;
         int mx=-1;
-        
 
+        for(int i=0;i<n;i++){
+            if(array[i]>mx && array[i]>array[i+1]){
+                ans++;
+            }
+            mx=Integer.max(mx, array[i]);
+        }
+        System.out.println(ans);
     }
     
 }
