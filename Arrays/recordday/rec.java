faade478d0db78b5e82f6afc8919a6917684801c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-15 15:24:56 +0530

    added new code

--- Arrays/recordday/rec.java
+++ Arrays/recordday/rec.java
@@ -5,13 +5,18 @@ public class rec {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
         int n = sc.nextInt();
-        int array[]=new int[n];
-
+        int array[]=new int[n+1];
+        array[n]=-1;
         System.out.println("Enter the elements of array:");
         for(int i=0;i<n;i++){
             array[i]=sc.nextInt();
         }
+        if(n==1){
+            System.out.println("1");
+        }
+
         
+
     }
     
 }
