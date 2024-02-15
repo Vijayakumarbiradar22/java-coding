abd81449503cee9fe1f02bde42174e740772691a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-15 15:13:04 +0530

    added new code

--- Arrays/transpose/trans.java
+++ Arrays/transpose/trans.java
@@ -6,7 +6,7 @@ public class trans {
             for(int j=i;i<c;j++){
                 int temp=mat[i][j];
                 mat[i][j]=mat[j][i];
-                
+                mat[j][i]=temp;
             }
         }
     }
