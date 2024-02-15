fc804f020ce3c173cbb5d2e400408703763b8b84
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-15 15:22:36 +0530

    added new code

--- Arrays/recordday/rec.java
+++ Arrays/recordday/rec.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class rec {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n = sc.nextInt();
         
     }
     
