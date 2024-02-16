3f3caa617cc3755533ba70ac258a1d504b8e5100
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-16 23:10:14 +0530

    added new code

--- Patterns/Stars/mixofstarnum/mix.java
+++ Patterns/Stars/mixofstarnum/mix.java
@@ -2,6 +2,7 @@ package Patterns.Stars.mixofstarnum;
 import java.util.Scanner;
 public class mix {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         
     }
     
