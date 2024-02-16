b3c0c302fb92681ebd0f9c37e8e9922c5657e233
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-16 23:14:54 +0530

    added new code

--- Patterns/Stars/mixofstarnum/mix.java
+++ Patterns/Stars/mixofstarnum/mix.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class mix {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
+        System.out.println("Enter the nof rows:");
         int n = sc.nextInt();
         for(int i=0;i<n;i++){
             for(int j=1;j<=n;j++){
