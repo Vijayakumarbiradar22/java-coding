b5edbd4423f6eafca8b339ab1b0d0b5f0a9d6c10
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-17 21:49:08 +0530

    added new code

--- Patterns/Numbers/oddline/oddeven.java
+++ Patterns/Numbers/oddline/oddeven.java
@@ -8,6 +8,8 @@ public class oddeven {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 if(i%2==0){
+                  System.out.print(i);  
+                } else {
                     
                 }
             }
