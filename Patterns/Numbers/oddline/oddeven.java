2d0e4932d64de58aafbf5befb747d3b2e5501459
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-17 21:46:30 +0530

    added new code

--- Patterns/Numbers/oddline/oddeven.java
+++ Patterns/Numbers/oddline/oddeven.java
@@ -2,6 +2,7 @@ package Patterns.Numbers.oddline;
 import java.util.Scanner;
 public class oddeven {
     public static void main(String args[]){
+       System.out.println("Enter the number of rows:");
         
     }
     
