83ec38c1f4fa2780a88f0aec6f0646e6f5e0d789
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-17 21:53:38 +0530

    added new code

--- Patterns/Numbers/oddline/oddeven.java
+++ Patterns/Numbers/oddline/oddeven.java
@@ -6,13 +6,15 @@ public class oddeven {
        System.out.println("Enter the number of rows:");
         int n=sc.nextInt();
         int odd=1;
+        int even=2;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 if(i%2==0){
                   System.out.print(odd);
-                  odd+2;  
+                  odd+=2;  
                 } else {
-                   System.out.print(i+1); 
+                   System.out.print(even); 
+                   even+=2;
                 }
             }
             System.out.println();
