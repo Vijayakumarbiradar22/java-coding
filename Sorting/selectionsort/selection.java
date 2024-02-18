17ee9b4cfbd47bdebc8b18d0cad1394b4739ab86
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-18 19:15:03 +0530

    added new code

--- Sorting/selectionsort/selection.java
+++ Sorting/selectionsort/selection.java
@@ -15,6 +15,7 @@ public class selection {
             for(int j=i+1;j<=n;j++){
                 if(arr[j]<arr[i]){
                     int temp=arr[j];
+                    arr[j]=arr[i];
                     
                 }
             }
