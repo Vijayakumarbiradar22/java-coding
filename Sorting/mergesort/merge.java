021dfb8fa1409f87b99962aadd4fa4dcc92e9d1d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-19 21:50:18 +0530

    added new code

--- Sorting/mergesort/merge.java
+++ Sorting/mergesort/merge.java
@@ -38,33 +38,6 @@ public class merge {
         for(int i=0,j=si;i<merged.length;i++,j++){
            arr[j]=merged[i]; 
         }
-
-    //     int merged[]=new int[ei-si+1];
-
-    //     int indx1=si;
-    //     int indx2=mid+1;
-    //     int x=0;
-
-    //     while(indx1<=mid && indx2<=ei){
-    //         if(arr[indx1]<arr[indx2]){
-    //             merged[x++]=arr[indx1++];
-    //         } else {
-    //             merged[x++]=arr[indx2++];
-    //         }
-    //     }
-
-    //     while(indx1<=mid){
-    //         merged[x++]=arr[indx1++];
-    //     }
-
-    //     while(indx2<=ei){
-    //         merged[x++]=arr[indx2++];
-    //     }
-
-        
-    //     for(int i=0,j=si;i<merged.length;i++,j++){
-    //         arr[j]=merged[i];
-    //     }
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
