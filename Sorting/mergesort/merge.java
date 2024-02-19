1e3776daa7bdf8c1a9bedf6ac16b8510f8649396
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-19 21:33:27 +0530

    added new code

--- Sorting/mergesort/merge.java
+++ Sorting/mergesort/merge.java
@@ -50,7 +50,11 @@ public class merge {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        
+        devide(arr,0,n-1);
+
+        for(int i=0;i<n;i++){
+            
+        }
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the size of array:");
         // int n=sc.nextInt();
