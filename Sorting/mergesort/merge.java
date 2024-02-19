7dbcac2ee55ae6bd46dcf73229bd1ea51a9ade31
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-19 21:30:26 +0530

    added new code

--- Sorting/mergesort/merge.java
+++ Sorting/mergesort/merge.java
@@ -44,19 +44,22 @@ public class merge {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        System.out.println("Enter the elements:");
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
+        
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the size of array:");
+        // int n=sc.nextInt();
+        // int arr[]=new int[n];
+        // System.out.println("Enter the elements:");
+        // for(int i=0;i<n;i++){
+        //     arr[i]=sc.nextInt();
+        // }
 
-        devide(arr,0,n-1);
+        // devide(arr,0,n-1);
 
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
-        sc.close();   
+        // for(int i=0;i<n;i++){
+        //     System.out.print(arr[i]+" ");
+        // }
+        // System.out.println();
+        // sc.close();   
     } 
 }
