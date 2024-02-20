0aa556cc6b029a9cc87b0c749b0753e9f54f4d73
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-20 11:37:44 +0530

    added new code

--- Strings/longestsubstring/lenoflongsub.java
+++ Strings/longestsubstring/lenoflongsub.java
@@ -8,9 +8,20 @@ public class lenoflongsub {
         int left=0;
 
         for(int right=0;right<s.length();right++){
-            
+           if(!set.contains(s.charAt(right))){
+            set.add(s.charAt(right));
+            maxLength=Math.max(maxLength,right-left+1);
+           } else {
+            while(s.charAt(left)!=s.charAt(right)){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.remove(s.charAt(left));
+            left++;
+            set.add(s.charAt(right));
+           }
         }
-
+        return maxLength;
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
