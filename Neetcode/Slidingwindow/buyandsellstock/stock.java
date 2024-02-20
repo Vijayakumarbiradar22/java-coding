a197f82535a40068e6baa4f6329d72dde1d7f529
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-20 13:02:47 +0530

    added new code

--- Neetcode/Slidingwindow/buyandsellstock/stock.java
+++ Neetcode/Slidingwindow/buyandsellstock/stock.java
@@ -19,10 +19,10 @@ public class stock {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array:");
+        System.out.println("Enter the number of days:");
         int n=sc.nextInt();
         int[] prices = new int[n];
-        System.out.println("Enter the prices:");
+        System.out.println("Enter the prices on each day:");
         for(int i=0;i<n;i++){
             prices[i]=sc.nextInt();
         }
