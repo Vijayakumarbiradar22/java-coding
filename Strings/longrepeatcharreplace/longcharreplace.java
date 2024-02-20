f09e213aa87fce70ca80a85ddf0c6455ba1f7c13
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-20 11:58:37 +0530

    added new code

--- Strings/longrepeatcharreplace/longcharreplace.java
+++ Strings/longrepeatcharreplace/longcharreplace.java
@@ -2,7 +2,11 @@ package Strings.longrepeatcharreplace;
 import java.util.*;
 public class longcharreplace {
     public static void main(String args[]){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String s=sc.nextLine();
+        System.out.println("Enter the value of k:");
+        int k=sc.nextInt();
     }
     
 }
