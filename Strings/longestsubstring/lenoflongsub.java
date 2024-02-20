4f1097c2a21657bb8054222c6aa782c694e64e01
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-20 11:31:16 +0530

    added new code

--- Strings/longestsubstring/lenoflongsub.java
+++ Strings/longestsubstring/lenoflongsub.java
@@ -1,13 +1,14 @@
 package Strings.longestsubstring;
 import java.util.Scanner;
 public class lenoflongsub {
+    
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String:");
         String s=sc.nextLine();
         int result=lengthOflongestSubstring(s);
         System.out.println(result);
-        
+        sc.close();
     }
     
 }
