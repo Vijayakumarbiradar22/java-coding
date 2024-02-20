df8af1b8227d92f05f1930d0f73727a985a32244
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-20 12:02:51 +0530

    added new code

--- Strings/longrepeatcharreplace/longcharreplace.java
+++ Strings/longrepeatcharreplace/longcharreplace.java
@@ -1,7 +1,18 @@
 package Strings.longrepeatcharreplace;
 import java.util.*;
 public class longcharreplace {
-    
+    public static int characterReplacement(String s,int k){
+        int arr[]=new int[26];
+        int res=0;
+        int max=0;
+
+        int l=0;
+        for(int r=0;r<s.length();r++){
+            arr[s.charAt(r)-'A']++;
+            max=Math.max(max,arr[s.charAt(r)-'A']);
+            
+        }
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String:");
