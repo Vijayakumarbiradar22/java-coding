3f0b872b03f37ccd9e0b56bd1ae3491b8dfab511
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-20 12:23:11 +0530

    added new code

--- Neetcode/Slidingwindow/buyandsellstock/stock.java
+++ Neetcode/Slidingwindow/buyandsellstock/stock.java
@@ -5,6 +5,9 @@ public class stock {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
         int n=sc.nextInt();
+        int[] prices = new int[n];
+        System.out.println("Enter the prices:");
+        for()
     }
     
 }
