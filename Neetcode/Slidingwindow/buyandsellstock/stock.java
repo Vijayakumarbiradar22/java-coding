89ebc1adaefe015e68386dfd7ccb7fb7c240f38b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-20 12:21:02 +0530

    added new code

--- Neetcode/Slidingwindow/buyandsellstock/stock.java
+++ Neetcode/Slidingwindow/buyandsellstock/stock.java
@@ -2,6 +2,7 @@ package Neetcode.Slidingwindow.buyandsellstock;
 import java.util.*;
 public class stock {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         
     }
     
