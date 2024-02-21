03ae98a0d1969094eb57fa8314cdb5da4cf859b1
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-21 12:28:33 +0530

    added new code

--- Neetcode/Stack/minstack/min.java
+++ Neetcode/Stack/minstack/min.java
@@ -3,8 +3,7 @@ import java.util.*;
 public class min {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        
+
         sc.close();
-    }
-    
+    } 
 }
