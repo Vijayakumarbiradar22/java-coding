88e42bd3e93cd708e061ab1f9d1566c13e419e92
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-21 10:31:00 +0530

    added new code

--- Neetcode/Stack/validparentheses/paren.java
+++ Neetcode/Stack/validparentheses/paren.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class paren {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String s = sc.nextLine();
+        String result = isValid(s);
+        System.out.println(result);
         
     }
     
