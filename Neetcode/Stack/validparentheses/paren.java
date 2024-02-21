452b3d8d39ce806880f5a5e28374b06b965d0dbf
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-21 10:25:04 +0530

    added new code

--- Neetcode/Stack/validparentheses/paren.java
+++ Neetcode/Stack/validparentheses/paren.java
@@ -1,6 +1,9 @@
 package Neetcode.Stack.validparentheses;
 import java.util.Scanner;
 public class paren {
-    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
     
 }
