a6770602125b443e588a23bed7a96650828e6ee3
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-23 21:56:03 +0530

    added new code

--- Patterns/Numbers/palindromenum/palin.java
+++ Patterns/Numbers/palindromenum/palin.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class palin {
     public static void main(String args[]){
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the  rows:");
+            System.out.println("Enter the number of rows:");
             int n=sc.nextInt();
             for(int i=1;i<=n;i++){
                 for(int s=1;s<=n-i;s++){
