62ee7a4bd149e5661eebe46e0daf9bb73c12dde4
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-23 20:59:44 +0530

    added new code

--- Patterns/Stars/starplus/sp.java
+++ Patterns/Stars/starplus/sp.java
@@ -7,7 +7,9 @@ public class sp {
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                
+                if(i==(n/2)+1 || j==(n/2)+1){
+
+                }
             }
         }
         sc.close();
