7b890952475c9f78a64b206967b17b5050607ebf
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-24 15:11:18 +0530

    added new code

--- Neetcode/Stack/dailytemperatures/temp.java
+++ Neetcode/Stack/dailytemperatures/temp.java
@@ -1,7 +1,17 @@
 package Neetcode.Stack.dailytemperatures;
 import java.util.*;
 public class temp {
-    public static int[]
+    public static int[] DailyTemperature(int[] temperatures){
+        int[] results = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                results[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        return results;
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of days:");
