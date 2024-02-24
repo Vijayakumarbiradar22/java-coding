86127a5c6e7b3566dc0cb061f8fcaedf4356dd2a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-24 14:57:18 +0530

    added new code

--- Neetcode/Stack/dailytemperatures/temp.java
+++ Neetcode/Stack/dailytemperatures/temp.java
@@ -2,7 +2,8 @@ package Neetcode.Stack.dailytemperatures;
 import java.util.*;
 public class temp {
     public static void main(String args[]){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the ");
     }
     
 }
