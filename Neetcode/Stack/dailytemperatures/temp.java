a6aae58cd15ab96e9be744636ec8f419645bface
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-24 15:02:19 +0530

    added new code

--- Neetcode/Stack/dailytemperatures/temp.java
+++ Neetcode/Stack/dailytemperatures/temp.java
@@ -3,7 +3,12 @@ import java.util.*;
 public class temp {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the ");
+        System.out.println("Enter the number of days:");
+        int n=sc.nextInt();
+        int
+        System.out.println("Enter the temperatures:");
+
+        sc.close();
     }
     
 }
