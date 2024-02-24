5148a51b865088fd30d3b9bae99c198f192cf888
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-24 15:06:30 +0530

    added new code

--- Neetcode/Stack/dailytemperatures/temp.java
+++ Neetcode/Stack/dailytemperatures/temp.java
@@ -8,7 +8,11 @@ public class temp {
         int temperatures[]=new int[n];
         System.out.println("Enter the temperatures:");
         for(int i=0;i<n;i++){
-            
+           temperatures[i]=sc.nextInt(); 
+        }
+        int result[]=DailyTemperature(temperatures);
+        for(int i:result){
+            System.out.print(result[i]+" ");
         }
         sc.close();
     }
