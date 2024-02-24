b36b0fcd460adc8c1db7310fb5c384c2babdaf6e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-24 19:38:14 +0530

    added new code

--- Neetcode/Stack/generateparantheses/parentheses.java
+++ Neetcode/Stack/generateparantheses/parentheses.java
@@ -2,7 +2,9 @@ package Neetcode.Stack.generateparantheses;
 import java.util.*;
 public class parentheses {
     public static void main(String args[]){
-        
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the number of parentheses:"); 
+       
     }
     
 }
