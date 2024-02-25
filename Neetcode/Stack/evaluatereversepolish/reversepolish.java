f998ca853fca026e12d7efed936ee28e4d5e7145
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-25 09:54:41 +0530

    added new code

--- Neetcode/Stack/evaluatereversepolish/reversepolish.java
+++ Neetcode/Stack/evaluatereversepolish/reversepolish.java
@@ -6,6 +6,10 @@ public class reversepolish {
         int n=tokens.length;
         for(int i=0;i<n;i++){
             if(tokens[i].length()==1 && tokens[i].charAt(0)<48){
+              long integer2=stack.pop();
+              long integer1=stack.pop();
+              char operator=tokens[i].charAt(0);
+              long resolvedAns=resolves(integer1,integer2,operator);
                 
             }
         }
