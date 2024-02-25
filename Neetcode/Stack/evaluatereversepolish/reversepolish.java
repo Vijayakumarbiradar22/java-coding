4dc676964e4a43ce23973624f86d33b6abd0e0b5
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-25 09:45:59 +0530

    added new code

--- Neetcode/Stack/evaluatereversepolish/reversepolish.java
+++ Neetcode/Stack/evaluatereversepolish/reversepolish.java
@@ -1,12 +1,17 @@
 package Neetcode.Stack.evaluatereversepolish;
 import java.util.*;
 public class reversepolish {
+    
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
         int n=sc.nextInt();
         String tokens[]=new String[n];
         System.out.println("Enter the tokens:");
+        for(int i=0;i<n;i++){
+            tokens[i]=sc.nextLine();
+        }
+        String stackres=evlRPN(tokens);
         
         sc.close();
     }
