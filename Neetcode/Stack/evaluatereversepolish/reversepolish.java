44dc8c98e99d912f4ca4f18dc883f13e87b73a51
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-25 09:37:25 +0530

    added new code

--- Neetcode/Stack/evaluatereversepolish/reversepolish.java
+++ Neetcode/Stack/evaluatereversepolish/reversepolish.java
@@ -2,7 +2,9 @@ package Neetcode.Stack.evaluatereversepolish;
 import java.util.*;
 public class reversepolish {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         
+        sc.close();
     }
     
 }
