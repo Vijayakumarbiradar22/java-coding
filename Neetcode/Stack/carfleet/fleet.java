f87b433ec52aaefb80cc6ff6faa16ecbccd57e17
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-26 07:41:18 +0530

    added new code

--- Neetcode/Stack/carfleet/fleet.java
+++ Neetcode/Stack/carfleet/fleet.java
@@ -1,5 +1,13 @@
 package Neetcode.Stack.carfleet;
-
+import java.util.*;
 public class fleet {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the target:");
+        int target=sc.nextInt();
+        
+
+        sc.close();
+    }
     
 }
