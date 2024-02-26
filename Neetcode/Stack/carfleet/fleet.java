3a13da352a233a606af28876e885fb3583ffa59e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-26 07:53:32 +0530

    added new code

--- Neetcode/Stack/carfleet/fleet.java
+++ Neetcode/Stack/carfleet/fleet.java
@@ -1,6 +1,9 @@
 package Neetcode.Stack.carfleet;
 import java.util.*;
 public class fleet {
+    public static int carFleet(int target,int[] position,int[] speed){
+        
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the target:");
