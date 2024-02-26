30e307f4260903b38fefd115703216e6a37096f0
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-26 07:57:47 +0530

    added new code

--- Neetcode/Stack/carfleet/fleet.java
+++ Neetcode/Stack/carfleet/fleet.java
@@ -2,7 +2,20 @@ package Neetcode.Stack.carfleet;
 import java.util.*;
 public class fleet {
     public static int carFleet(int target,int[] position,int[] speed){
-        
+        int res=0;
+        double[] timeArr = new double[target];
+        for(int i=0;i<position.length;i++){
+            timeArr[position[i]]=(double)(target-position[i])/speed[i];
+        }
+        double prev=0.0;
+        for(int i=target-1;i>=0;i--){
+            double cur=timeArr[i];
+            if(cur>prev){
+                prev=cur;
+                res++;
+            }
+        }
+        return res;
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
@@ -24,5 +37,4 @@ public class fleet {
         System.out.println(result);
         sc.close();
     }
-    
 }
