d3ae3fbd16c031f18e25abb2eb3fc96b50c20fe9
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-27 21:58:07 +0530

    added new code

--- Recursion/decreasenum/decnum.java
+++ Recursion/decreasenum/decnum.java
@@ -2,11 +2,8 @@ package Recursion.decreasenum;
 import java.util.Scanner;
 public class decnum {
     public static void decreaseNumber(int n){
-        if(n==0){
-            return;
-        }
-        System.out.println(n);
-        decreaseNumber(n-1);
+       System.out.println(n);
+       
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
