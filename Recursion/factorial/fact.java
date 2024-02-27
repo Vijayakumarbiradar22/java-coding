1c2e256d664e370eabb9b24adf927a2f25888d69
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-27 21:51:34 +0530

    added new code

--- Recursion/factorial/fact.java
+++ Recursion/factorial/fact.java
@@ -11,6 +11,8 @@ public class fact {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n=sc.nextInt();
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter the number:");
         // int n = sc.nextInt();
