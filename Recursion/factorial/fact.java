ff26c5dd8072eb847a8a6ecf7f6faad1dfd82d34
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-27 21:50:43 +0530

    added new code

--- Recursion/factorial/fact.java
+++ Recursion/factorial/fact.java
@@ -1,21 +1,22 @@
 package Recursion.factorial;
 import java.util.*;
 public class fact {
-    public static int findFact(int n){
-        if(n==0 || n==1){
-            return 1;
-        }
-        else{
-            return n*findFact(n-1);
-        }
+    // public static int findFact(int n){
+    //     if(n==0 || n==1){
+    //         return 1;
+    //     }
+    //     else{
+    //         return n*findFact(n-1);
+    //     }
     }
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number:");
-        int n = sc.nextInt();
-        int result = findFact(n);
-        System.out.println(result);
-        sc.close();
+        
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the number:");
+        // int n = sc.nextInt();
+        // int result = findFact(n);
+        // System.out.println(result);
+        // sc.close();
     }
     
 }
