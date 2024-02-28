ca1bfa49fe848d08c1782c8234a4c7e7d0bf7d44
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-28 09:39:49 +0530

    added new code

--- Arrays/searchinarray/search.java
+++ Arrays/searchinarray/search.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class search {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array;");
+        int n=sc.nextInt();
+        int nums[]=new int[n];
         
     }
     
