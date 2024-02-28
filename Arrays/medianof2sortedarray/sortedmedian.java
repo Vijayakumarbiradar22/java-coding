2b7aca211451aabe36041d444b93e365dae6c74c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-28 15:27:58 +0530

    added new code

--- Arrays/medianof2sortedarray/sortedmedian.java
+++ Arrays/medianof2sortedarray/sortedmedian.java
@@ -8,6 +8,11 @@ public class sortedmedian {
 
         int merged[] = new int[n1+n2];
         int k=0;
+
+        for(int i=0;i<n1;i++){
+            merged[k++]=nums1[i];
+        }
+        
     }
     public static void main(String args[]){
       Scanner sc = new Scanner(System.in);
