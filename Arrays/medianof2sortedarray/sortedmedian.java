f4e1a4d0f2b1719c4bb4d6132f75a5e764dc3bff
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-28 15:22:04 +0530

    added new code

--- Arrays/medianof2sortedarray/sortedmedian.java
+++ Arrays/medianof2sortedarray/sortedmedian.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class sortedmedian {
     public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the size of array1:");
+      int n1=sc.nextInt();
         
     }
     
