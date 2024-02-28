bc6995d03be58e4fb7f8d1fbe01606da194ddd88
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-28 09:35:26 +0530

    added new code

---
+++ Arrays/searchinarray/search.java
@@ -0,0 +1,9 @@
+package Arrays.searchinarray;
+import java.util.Scanner;
+public class search {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+}
