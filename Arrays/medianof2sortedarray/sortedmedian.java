d61be9cd77f2d594fd5bc0764619f41737e6b992
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-28 15:35:44 +0530

    added new code

--- Arrays/medianof2sortedarray/sortedmedian.java
+++ Arrays/medianof2sortedarray/sortedmedian.java
@@ -5,19 +5,15 @@ public class sortedmedian {
     public static double findMedianSortedArray(int[] nums1,int[] nums2){
         int n1=nums1.length;
         int n2=nums2.length;
-
         int merged[] = new int[n1+n2];
         int k=0;
-
         for(int i=0;i<n1;i++){
             merged[k++]=nums1[i];
         }
         for(int i=0;i<n2;i++){
             merged[k++]=nums2[i];
         }
-
         Arrays.sort(merged);
-
         int total=merged.length;
         if(total%2==1){
             return (double) merged[total/2];
