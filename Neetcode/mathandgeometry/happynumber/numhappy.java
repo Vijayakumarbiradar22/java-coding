56b243c5cf98613cae1c3674a5ab07e04a120873
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-29 11:45:33 +0530

    added new code

--- Neetcode/mathandgeometry/happynumber/numhappy.java
+++ Neetcode/mathandgeometry/happynumber/numhappy.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class numhappy {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter the number:");
+        int n=sc.nextInt();
+        isHappy(n);
         sc.close();
     }
 }
