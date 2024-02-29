ebca3376143fc175eff08639e44cc9ed99dc18fc
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-29 12:00:56 +0530

    added new code

--- Neetcode/mathandgeometry/plusone/oneplus.java
+++ Neetcode/mathandgeometry/plusone/oneplus.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class oneplus {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+        int digits[]=new int[n];
+        
         sc.close();
     }
     
