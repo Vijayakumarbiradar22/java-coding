7fe3b1b843759d468d9ec4eb6cc5e44aa28a260b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-29 12:03:30 +0530

    added new code

--- Neetcode/mathandgeometry/plusone/oneplus.java
+++ Neetcode/mathandgeometry/plusone/oneplus.java
@@ -1,11 +1,20 @@
 package Neetcode.mathandgeometry.plusone;
 import java.util.*;
 public class oneplus {
+    
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
         int n=sc.nextInt();
         int digits[]=new int[n];
+        System.out.println("Enter the elements :");
+        for(int i=0;i<n;i++){
+            digits[i]=sc.nextInt();
+        }
+        int result[]=plusOne(digits);
+        for(int i=0;i<n;i++){
+            System.out.print(digits[i]+" ");
+        }
         
         sc.close();
     }
