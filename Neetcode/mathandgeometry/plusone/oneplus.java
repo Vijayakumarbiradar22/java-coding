a14f111d2fadbb824f588d68808032fa50d5880f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-29 11:58:53 +0530

    added new code

---
+++ Neetcode/mathandgeometry/plusone/oneplus.java
@@ -0,0 +1,11 @@
+package Neetcode.mathandgeometry.plusone;
+import java.util.*;
+public class oneplus {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+
+        sc.close();
+    }
+    
+}
