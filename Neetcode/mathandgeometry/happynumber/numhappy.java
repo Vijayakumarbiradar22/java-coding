ca8810a745569ea0342c380c9eab8f2ccf368aab
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-29 11:48:07 +0530

    added new code

--- Neetcode/mathandgeometry/happynumber/numhappy.java
+++ Neetcode/mathandgeometry/happynumber/numhappy.java
@@ -4,7 +4,14 @@ public class numhappy {
     public static boolean isHappy(int n){
        int slow=n;
        int fast=n;
-        
+
+       do{
+        slow=square(slow);
+        fast=square(square(fast));
+        if(slow==1){
+            return true;
+        }
+       }
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
