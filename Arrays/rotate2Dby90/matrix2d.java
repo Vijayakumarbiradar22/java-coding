03778555484ff5efc632b05d9a2bb2fe1519cc97
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-29 10:28:48 +0530

    added new code

---
+++ Arrays/rotate2Dby90/matrix2d.java
@@ -0,0 +1,9 @@
+package Arrays.rotate2Dby90;
+import java.util.Scanner;
+public class matrix2d {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+}
