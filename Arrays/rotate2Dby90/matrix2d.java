b69283e7159a35428e7c22781446afbc0a01acab
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-02-29 10:31:46 +0530

    added new code

--- Arrays/rotate2Dby90/matrix2d.java
+++ Arrays/rotate2Dby90/matrix2d.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class matrix2d {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int r=sc.nextInt();
+        System.out.println("Enter the number of cols:");
+        int c=sc.nextInt();
         
     }
     
