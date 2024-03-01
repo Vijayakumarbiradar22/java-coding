3ad83cf7bb15b165354845b034a7dff0a56062f3
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-01 17:37:08 +0530

    modified code

---
+++ Neetcode/mathandgeometry/xpowern/power.java
@@ -0,0 +1,10 @@
+package Neetcode.mathandgeometry.xpowern;
+import java.util.*;
+public class power {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of x:");
+        double x=sc.nextDouble();
+    }
+    
+}
