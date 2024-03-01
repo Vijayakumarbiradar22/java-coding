1269be595ad09aa7e66e0aa9100b80773b2812ff
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-01 18:02:47 +0530

    modified code

--- Neetcode/mathandgeometry/multiplynumber/multiply.java
+++ Neetcode/mathandgeometry/multiplynumber/multiply.java
@@ -5,7 +5,12 @@ public class multiply {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String1:");
         String num1=sc.nextLine();
-        
+        System.out.println("Enter the String2:");
+        String num2=sc.nextLine();
+        String result = multiply(num1,num2);
+        System.out.println(result);
+
+        sc.close();
     }
     
 }
