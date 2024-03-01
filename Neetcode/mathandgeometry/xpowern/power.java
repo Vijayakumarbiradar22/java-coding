58f463c1c80e2dc2b661e488a6653cd750c81e6a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-01 17:44:56 +0530

    modified code

--- Neetcode/mathandgeometry/xpowern/power.java
+++ Neetcode/mathandgeometry/xpowern/power.java
@@ -1,6 +1,21 @@
 package Neetcode.mathandgeometry.xpowern;
 import java.util.*;
 public class power {
+    public static double myPower(double x,int n){
+        if(n<0){
+            n=-n;
+            x=1/x;
+        }
+        double pow=1;
+        while(n!=0){
+            if((n&1)!=0){
+                pow*=x;
+            }
+            x*=x;
+            n>>>=1;
+        }
+        return pow;
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of x:");
@@ -8,7 +23,7 @@ public class power {
         System.out.println("Enter the value of n:");
         int n=sc.nextInt();
         double result = myPower(x,n);
-
+        
         sc.close();
     }
 }
