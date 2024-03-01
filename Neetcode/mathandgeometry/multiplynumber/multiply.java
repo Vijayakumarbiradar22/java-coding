31aac86df7a72f1d0cdfa90db7b5d5165b61c822
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-01 18:00:06 +0530

    modified code

---
+++ Neetcode/mathandgeometry/multiplynumber/multiply.java
@@ -0,0 +1,9 @@
+package Neetcode.mathandgeometry.multiplynumber;
+import java.util.*;
+public class multiply {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+    
+}
