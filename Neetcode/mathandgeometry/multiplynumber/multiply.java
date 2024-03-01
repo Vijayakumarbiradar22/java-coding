c32fd92a8b16d40766c2b1d10ca2e4e104e366e4
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-01 18:00:44 +0530

    modified code

--- Neetcode/mathandgeometry/multiplynumber/multiply.java
+++ Neetcode/mathandgeometry/multiplynumber/multiply.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class multiply {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String1:");
+        String num1=sc.nextLine();
         
     }
     
