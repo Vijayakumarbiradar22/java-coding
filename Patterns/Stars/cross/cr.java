6e01af905e9e571b5ade2268d7d9589ae72e3a70
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-02 20:00:22 +0530

    modified code

--- Patterns/Stars/cross/cr.java
+++ Patterns/Stars/cross/cr.java
@@ -3,20 +3,24 @@ import java.util.Scanner;
 public class cr {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
-        int n = sc.nextInt();
+        
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(i==j || i+j==n-1){
-                  System.out.print("*");  
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
         sc.close();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the number of rows:");
+        // int n = sc.nextInt();
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         if(i==j || i+j==n-1){
+        //           System.out.print("*");  
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        // sc.close();
     }
     
 }
