d0202272e770663d1876ca3ded123e8eaaea0bf8
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-02 19:37:24 +0530

    modified code

--- Patterns/Stars/butterfly/fly.java
+++ Patterns/Stars/butterfly/fly.java
@@ -3,44 +3,47 @@ import java.util.Scanner;
 public class fly {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no.of rows:");
-        int n= sc.nextInt();
 
-        //Upper part
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-
-            int sp=2*(n-i);
-            for(int s=1;s<=sp;s++){
-                System.out.print(" ");
-            }
-
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-            //Lower part
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-
-            int sp=2*(n-i);
-            for(int s=1;s<=sp;s++){
-                System.out.print(" ");
-            }
-
-            for(int j=1;j<=i;j++){
-               System.out.print("*");
-            }
-            System.out.println();
-        }
         sc.close();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the no.of rows:");
+        // int n= sc.nextInt();
+
+        // //Upper part
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+
+        //     int sp=2*(n-i);
+        //     for(int s=1;s<=sp;s++){
+        //         System.out.print(" ");
+        //     }
+
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        //     //Lower part
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+
+        //     int sp=2*(n-i);
+        //     for(int s=1;s<=sp;s++){
+        //         System.out.print(" ");
+        //     }
+
+        //     for(int j=1;j<=i;j++){
+        //        System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // sc.close();
     }
     
 }
