ea58ed266bb0de726619d22d72c376d111b55825
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-02 20:01:41 +0530

    modified code

--- Patterns/Stars/cross/cr.java
+++ Patterns/Stars/cross/cr.java
@@ -7,7 +7,9 @@ public class cr {
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                
+                if(i==j && i+j==n-1){
+                    System.out.print("*");
+                }
             }
         }
 
