21dfc2fb6c57af9e83d6c69edf391bddf8fa8b65
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-03 21:28:32 +0530

    modified code

--- Patterns/Stars/hallowrhombus/halrom.java
+++ Patterns/Stars/hallowrhombus/halrom.java
@@ -3,56 +3,36 @@ import java.util.Scanner;
 public class halrom {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the no.of rows:");
-        int n = sc.nextInt();
-
-        for(int i=1;i<n;i++){
-           for(int j=1;j<=2*n-1;j++){
-            if(j==n+i-1 || j==n-i+1 || i==n){
-                System.out.print("*");
-            } else {
-                System.out.print(" ");
-            }
-           } 
-           System.out.println();
-        }
-
-        for(int i=n-2;i>=1;i--){
-           for(int j=1;j<=2*n-1;j++){
-            if(j==n-i+1 || j==n+i-1 || i==n){
-                System.out.print("*");
-            } else {
-                System.out.print(" ");
-            }
-           } 
-           System.out.println();
-        }
-
-        sc.close();
+        System.out.println("Enter the number of rows:");
+        int n=sc.nextInt();
+        
         // Scanner sc = new Scanner(System.in);
-        //     System.out.println("Enter the rows:");
-        //     int n=sc.nextInt();
-        //     for(int i=1;i<n;i++){
-        //         for(int j=1;j<=2*n-1;j++){
-        //             if(j==n+i-1 || j==n-i+1 || i==n){
-        //                 System.out.print("*");
-        //             } else {
-        //                 System.out.print(" ");
-        //             }
-        //         }
-        //         System.out.println();
-        //     }
+        // System.out.println("Enter the no.of rows:");
+        // int n = sc.nextInt();
 
-        //     for(int i=n-2;i>=1;i--){
-        //         for(int j=1;j<=2*n-1;j++){
-        //             if(j==n+i-1 || j==n-i+1 || i==n){
-        //                 System.out.print("*");
-        //             } else {
-        //                 System.out.print(" ");
-        //             }
-        //         }
-        //         System.out.println();
+        // for(int i=1;i<n;i++){
+        //    for(int j=1;j<=2*n-1;j++){
+        //     if(j==n+i-1 || j==n-i+1 || i==n){
+        //         System.out.print("*");
+        //     } else {
+        //         System.out.print(" ");
         //     }
+        //    } 
+        //    System.out.println();
+        // }
+
+        // for(int i=n-2;i>=1;i--){
+        //    for(int j=1;j<=2*n-1;j++){
+        //     if(j==n-i+1 || j==n+i-1 || i==n){
+        //         System.out.print("*");
+        //     } else {
+        //         System.out.print(" ");
+        //     }
+        //    } 
+        //    System.out.println();
+        // }
+
+        // sc.close();
         }
     }
     
