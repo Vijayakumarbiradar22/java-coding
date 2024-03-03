2eafd6681a06cd3912a7d4f41b8b6385b2de2601
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-03 21:36:29 +0530

    modified code

--- Patterns/Stars/hallowrhombus/halrom.java
+++ Patterns/Stars/hallowrhombus/halrom.java
@@ -5,7 +5,7 @@ public class halrom {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
         int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<n;i++){
             for(int j=1;j<=2*n-1;j++){
                 if(j==n+i-1 || j==n-i+1 || i==n){
                     System.out.print("*");
@@ -16,7 +16,7 @@ public class halrom {
             System.out.println();
         }
 
-        for(int i=n-1;i>=1;i--){
+        for(int i=n-2;i>=1;i--){
             for(int j=1;j<=2*n-1;j++){
                 if(j==n+i-1 || j==n-i+1 || i==n){
                     System.out.print("*");
@@ -27,33 +27,6 @@ public class halrom {
             System.out.println();
         }
         sc.close();
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter the no.of rows:");
-        // int n = sc.nextInt();
-
-        // for(int i=1;i<n;i++){
-        //    for(int j=1;j<=2*n-1;j++){
-        //     if(j==n+i-1 || j==n-i+1 || i==n){
-        //         System.out.print("*");
-        //     } else {
-        //         System.out.print(" ");
-        //     }
-        //    } 
-        //    System.out.println();
-        // }
-
-        // for(int i=n-2;i>=1;i--){
-        //    for(int j=1;j<=2*n-1;j++){
-        //     if(j==n-i+1 || j==n+i-1 || i==n){
-        //         System.out.print("*");
-        //     } else {
-        //         System.out.print(" ");
-        //     }
-        //    } 
-        //    System.out.println();
-        // }
-
-        // sc.close();
         }
     }
     
