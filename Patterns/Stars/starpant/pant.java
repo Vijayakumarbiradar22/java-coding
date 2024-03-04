f1137dadc8a0fb2d92cd4a780be7fb15105b2ac3
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-04 21:56:00 +0530

    added new code

--- Patterns/Stars/starpant/pant.java
+++ Patterns/Stars/starpant/pant.java
@@ -16,28 +16,7 @@ public class pant {
                 }
             }
         }
-
         sc.close();
-
-        // Scanner sc  = new Scanner(System.in);
-        // System.out.println("Enter the number of rows:");
-        // int n = sc.nextInt();
-        // int x=n;
-        // int y=n;
-
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<n*2;j++){
-        //         if(j>x&&j<y){
-        //            System.out.print(" "); 
-        //         } else {
-        //             System.out.print("*");
-        //         }
-        //     }
-        //     x--;
-        //     y++;
-        //     System.out.println();
-        // }
-        // sc.close();
     }
     
 }
