5c23fd135791fc701fd280d04a46636bd9adb40a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-04 21:42:09 +0530

    added new code

--- Patterns/Stars/hallowsquare/halsq.java
+++ Patterns/Stars/hallowsquare/halsq.java
@@ -7,7 +7,9 @@ public class halsq {
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
            for(int j=1;j<=n;j++){
-            
+            if(i==1 || j==1 || i==n || j==n){
+                System.out.print("*");
+            }
            } 
         }
 
