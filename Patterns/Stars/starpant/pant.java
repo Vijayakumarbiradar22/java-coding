308c2f9d420c1c340e0ba9c9ccf5d915e48ea2d6
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-04 21:52:47 +0530

    added new code

--- Patterns/Stars/starpant/pant.java
+++ Patterns/Stars/starpant/pant.java
@@ -5,7 +5,11 @@ public class pant {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
         int n=sc.nextInt();
-        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<n*2;j++){
+                
+            }
+        }
 
         sc.close();
 
