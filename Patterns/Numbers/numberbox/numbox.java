f451abe0f9a047b2dee0853fbaec6b2f7153b1ff
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-05 22:18:43 +0530

    added new code

--- Patterns/Numbers/numberbox/numbox.java
+++ Patterns/Numbers/numberbox/numbox.java
@@ -8,7 +8,10 @@ public class numbox {
         // int n=4;
         int min=0;
         for(int i=1;i<=2*n-1;i++){
-            
+            for(int j=1;j<=2*n-1;j++){
+                int a=i;
+                
+            }
         }
         // for(int i=1;i<=2*n-1;i++){
         //     for(int j=1;j<=2*n-1;j++){
