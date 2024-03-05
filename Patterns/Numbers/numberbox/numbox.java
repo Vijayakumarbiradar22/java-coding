1898693b3e122599e0e4df8f57915df3675de74e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-05 22:14:58 +0530

    added new code

--- Patterns/Numbers/numberbox/numbox.java
+++ Patterns/Numbers/numberbox/numbox.java
@@ -2,6 +2,8 @@ package Patterns.Numbers.numberbox;
 import java.util.Scanner;
 public class numbox {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
         int n=4;
         int min=0;
         for(int i=1;i<=2*n-1;i++){
