45a8e603985075873dff0439223535d173ac1dce
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-05 22:15:18 +0530

    added new code

--- Patterns/Numbers/numberbox/numbox.java
+++ Patterns/Numbers/numberbox/numbox.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class numbox {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
         
         int n=4;
         int min=0;
