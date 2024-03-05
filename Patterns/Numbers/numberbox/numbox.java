b5cc3b60ae32eb861604b9b37abf14db1fa8f8f4
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-05 22:21:41 +0530

    added new code

--- Patterns/Numbers/numberbox/numbox.java
+++ Patterns/Numbers/numberbox/numbox.java
@@ -16,16 +16,6 @@ public class numbox {
                 if(a<b) min=a;
                 else min=b;
                 System.out.print(n+1-min+" ");
-            
-        // for(int i=1;i<=2*n-1;i++){
-        //     for(int j=1;j<=2*n-1;j++){
-        //         int a=i;
-        //         if(i>n) a=2*n-i;
-        //         int b=j;
-        //         if(j>n) b=2*n-j;
-        //         if(a<b) min=a;
-        //         else min=b;
-        //         System.out.print(n+1-min+ " ");
             }
             System.out.println();
             sc.close();
