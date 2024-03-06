6369e23c5ed433e30242b0ecac43a556711ab59f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-06 11:42:37 +0530

    added new code

--- LinkedList/directmethod/lists.java
+++ LinkedList/directmethod/lists.java
@@ -2,7 +2,7 @@ package LinkedList.directmethod;
 import java.util.*;
 public class lists {
     public static void main(String args[]){
-        LinkedList<String> list =new LinkedList<>();
+        LinkedList<String> list = new LinkedList<>();
         list.addFirst("10");
         list.addLast("20");
         list.addLast("30");
@@ -12,12 +12,26 @@ public class lists {
         list.removeFirst();
         list.removeLast();
 
-        System.out.println(list.size());
-
         for(int i=0;i<list.size();i++){
             System.out.print(list.get(i)+"->");
         }
         System.out.println("NULL");
+        // LinkedList<String> list =new LinkedList<>();
+        // list.addFirst("10");
+        // list.addLast("20");
+        // list.addLast("30");
+        // list.addLast("40");
+        // list.addLast("50");
+
+        // list.removeFirst();
+        // list.removeLast();
+
+        // System.out.println(list.size());
+
+        // for(int i=0;i<list.size();i++){
+        //     System.out.print(list.get(i)+"->");
+        // }
+        // System.out.println("NULL");
     }
     
 }
