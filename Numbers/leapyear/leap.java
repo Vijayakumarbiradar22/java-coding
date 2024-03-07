268588ac2aee6b974427172bba9dcdba79cf9d12
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-07 21:58:47 +0530

    added new code

--- Numbers/leapyear/leap.java
+++ Numbers/leapyear/leap.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class leap {
     public static void main(String args[]){
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the Year:");
+            System.out.println("Enter the Year to find leap:");
             int year = sc.nextInt();
             if(year%4==0 && year%100!=0 || year%400==0){
                 System.out.println("Leap year");
