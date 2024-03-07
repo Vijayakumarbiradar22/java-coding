196f0053f99cdfdd8294330ca97e0e068518c615
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-07 21:44:36 +0530

    added new code

--- Numbers/primenumber/prime.java
+++ Numbers/primenumber/prime.java
@@ -14,7 +14,7 @@ public class prime {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the start number:");
+        System.out.println("Enter start number:");
         int start=sc.nextInt();
         System.out.println("Enter the end number:");
         int end = sc.nextInt();
