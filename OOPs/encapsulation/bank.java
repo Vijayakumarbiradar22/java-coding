d84924bc5b9f6717fb8280a71201f42570a1cdb2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-08 21:42:38 +0530

    added new add

--- OOPs/encapsulation/bank.java
+++ OOPs/encapsulation/bank.java
@@ -19,7 +19,7 @@ public class bank {
        Account account1 = new Account();
        account1.name="Vijay Bank";
        account1.email="vijaybank22@gmail.com";
-       account1.setPassword("vgb22");
+       account1.setPassword("vijay22");
        System.out.println(account1.getPassword()); 
     }
     
