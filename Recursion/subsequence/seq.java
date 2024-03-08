2427b4ba67788fc878e82221c57111ebe67c13f9
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-08 21:50:53 +0530

    added new add

--- Recursion/subsequence/seq.java
+++ Recursion/subsequence/seq.java
@@ -16,7 +16,7 @@ public class seq {
     }
     public static void main(String args[]){
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the string value:");
+            System.out.println("Enter a string value:");
             String str = sc.nextLine();        
             subseq(str, 0, "");
             sc.close();
