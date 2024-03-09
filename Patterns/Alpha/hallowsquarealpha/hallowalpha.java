57e076bbba35bad6b72e812176a010ef550e4d4a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-09 21:49:13 +0530

    added new code

--- Patterns/Alpha/hallowsquarealpha/hallowalpha.java
+++ Patterns/Alpha/hallowsquarealpha/hallowalpha.java
@@ -8,7 +8,9 @@ public class hallowalpha {
         int alpha=65;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                
+                if(j==1 || j==n || i==1 || i==n){
+                    
+                }
             }
         }
 
