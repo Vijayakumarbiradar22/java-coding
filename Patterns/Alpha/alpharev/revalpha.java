9db9c53dbc7c1792cddee5c8d0e8e0e69a4ff3b5
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-09 21:56:25 +0530

    added new code

--- Patterns/Alpha/alpharev/revalpha.java
+++ Patterns/Alpha/alpharev/revalpha.java
@@ -2,7 +2,9 @@ package Patterns.Alpha.alpharev;
 import java.util.Scanner;
 public class revalpha {
     public static void main(String args[]){
-        
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
     }
     
 }
