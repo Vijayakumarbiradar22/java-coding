44754fa89e8ce55b537a1b0f1751ee9c3b409532
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-09 21:47:52 +0530

    added new code

--- Patterns/Alpha/hallowsquarealpha/hallowalpha.java
+++ Patterns/Alpha/hallowsquarealpha/hallowalpha.java
@@ -2,7 +2,12 @@ package Patterns.Alpha.hallowsquarealpha;
 import java.util.Scanner;
 public class hallowalpha {
     public static void main(String args[]){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n=sc.nextInt();
+
+
+        sc.close();
     }
     
 }
