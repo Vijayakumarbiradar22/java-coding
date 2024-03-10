2cde0a17ec78d916ece90ba9b82176cb57fab8b1
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-10 19:33:14 +0530

    added new code

--- Patterns/Numbers/pascaltriangle/pascal.java
+++ Patterns/Numbers/pascaltriangle/pascal.java
@@ -10,6 +10,8 @@ public class pascal {
     // }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n=sc.nextInt();
         
 
         sc.close();
