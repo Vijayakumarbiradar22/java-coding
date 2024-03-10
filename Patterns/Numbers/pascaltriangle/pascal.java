92e4b7486469f88c4da3f2877f4a6b52ec96ec96
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-10 19:40:23 +0530

    added new code

--- Patterns/Numbers/pascaltriangle/pascal.java
+++ Patterns/Numbers/pascaltriangle/pascal.java
@@ -8,13 +8,6 @@ public class pascal {
         }
         return factorial;
     }
-    // public static int fact(int n){
-    //     int factorial=1;
-    //     for(int i=2;i<=n;i++){
-    //         factorial=factorial*i;
-    //     }
-    //     return factorial;
-    // }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
