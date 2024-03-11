a6f0ea0c9ac1f43a4e8ceb4173a4deee18e8c02b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-11 19:36:10 +0530

    added new code

--- Numbers/armstrongno/arm.java
+++ Numbers/armstrongno/arm.java
@@ -25,28 +25,5 @@ public class arm {
            System.out.println(num+" is not an Armstrong number"); 
         }
         sc.close();
-        // int count=0;
-        // double rem,sum=0;
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter number to check Armstrong:");
-        // int num=sc.nextInt();
-        // int original=num;
-        // while(num!=0){
-        //     num=num/10;
-        //     count++;
-        // }
-       
-        // num=original;
-        // while(num!=0){
-        //     rem=num%10;
-        //     sum=sum+Math.pow(rem,count);
-        //     num=num/10;
-        // }
-        // if(original==sum){
-        //     System.out.println(original+" is Armstrong number.");
-        // } else {
-        //     System.out.println(original+" is not Armstrong number.");
-        // }
-        // sc.close();
     }
 }
