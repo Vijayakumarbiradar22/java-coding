ab644871bc4eaff1d0f7856bdc0ace6314daafd7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-12 19:31:33 +0530

    modified code

--- Numbers/reversenumber/rev.java
+++ Numbers/reversenumber/rev.java
@@ -8,11 +8,11 @@ public class rev {
         int rem,revNum=0;
         int temp=num;
         while(num!=0){
-            rem=num/10;
+            rem=num%10;
             revNum=revNum*10+rem;
             num=num/10;
         }
-        System.out.println(revNum);
+        System.out.println("Reverse of the "+temp+" is"+revNum);
         if(revNum==temp){
             System.out.println(temp+" is palindrome number");
         } else {
