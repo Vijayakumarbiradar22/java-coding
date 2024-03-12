7bb7140742abbd53181933d8ad43214c46c0812b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-12 19:16:42 +0530

    modified code

--- Numbers/primenumber/prime.java
+++ Numbers/primenumber/prime.java
@@ -1,30 +1,35 @@
 package Numbers.primenumber;
 import java.util.Scanner;
 public class prime {
-    public static boolean isPrime(int num){
-        if(num<2){
-            return false;
-        }
-        for(int i=2;i<=Math.sqrt(num);i++){
-            if(num%i==0){
-                return false;
-            }
-        }
-        return true;
-    }
+    // public static boolean isPrime(int num){
+    //     if(num<2){
+    //         return false;
+    //     }
+    //     for(int i=2;i<=Math.sqrt(num);i++){
+    //         if(num%i==0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter start number:");
+        System.out.println("Enter the start number:");
         int start=sc.nextInt();
-        System.out.println("Enter end number:");
-        int end = sc.nextInt();
-        for(int i=start;i<end;i++){
-            while(!isPrime(i)){
-                System.out.print(i+" ");
-            }
-        }
-        sc.close();
-
+        System.out.println("Enter the end number:");
+        int end=sc.nextInt();
+        
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter start number:");
+        // int start=sc.nextInt();
+        // System.out.println("Enter end number:");
+        // int end = sc.nextInt();
+        // for(int i=start;i<end;i++){
+        //     while(!isPrime(i)){
+        //         System.out.print(i+" ");
+        //     }
+        // }
+        // sc.close();
     }
     
 }
