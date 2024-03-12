7e12dbe670986a050dfa189da237f4438297660b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-12 19:20:31 +0530

    modified code

--- Numbers/primenumber/prime.java
+++ Numbers/primenumber/prime.java
@@ -1,6 +1,18 @@
 package Numbers.primenumber;
 import java.util.Scanner;
 public class prime {
+    public static boolean isPrime(int num){
+        if(num<2){
+            return false;
+        }
+
+        for(int i=2;i<Math.sqrt(num);i++){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
     // public static boolean isPrime(int num){
     //     if(num<2){
     //         return false;
@@ -18,7 +30,11 @@ public class prime {
         int start=sc.nextInt();
         System.out.println("Enter the end number:");
         int end=sc.nextInt();
-        
+        for(int i=start;i<end;i++){
+            if(!isPrime(i)){
+                System.out.print(i+" ");
+            }
+        }
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter start number:");
         // int start=sc.nextInt();
@@ -31,5 +47,4 @@ public class prime {
         // }
         // sc.close();
     }
-    
 }
