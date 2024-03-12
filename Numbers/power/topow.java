f7ea1c6307ef0f1d8490f21ba4a3417618f0b0ab
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-12 19:12:28 +0530

    modified code

--- Numbers/power/topow.java
+++ Numbers/power/topow.java
@@ -13,28 +13,9 @@ public class topow {
         if(n%2==0){
             return findPower(x, n/2) * findPower(x, n/2);
         } else {
-            
+           return x*findPower(x, n/2) * findPower(x, n/2); 
         }
     }
-
-    // public static int findPower(int x, int n){
-    //     if(n==0){
-    //         return 1;
-    //     }
-
-    //     if(x==0){
-    //         return 0;
-    //     }
-
-    //     if(n%2==0){
-    //         return findPower(x,n/2) * findPower(x,n/2);
-    //     }
-    //     else {
-    //         return findPower(x,n/2) * findPower(x,n/2) *x;
-    //     }
-        
-
-    // }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the x:");
