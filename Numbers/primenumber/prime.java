2ac1ac26843e31886436fa66a40fb45332a92fb3
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-12 19:23:30 +0530

    modified code

--- Numbers/primenumber/prime.java
+++ Numbers/primenumber/prime.java
@@ -6,24 +6,13 @@ public class prime {
             return false;
         }
 
-        for(int i=2;i<Math.sqrt(num);i++){
+        for(int i=2;i<=Math.sqrt(num);i++){
             if(num%i==0){
                 return false;
             }
         }
         return true;
     }
-    // public static boolean isPrime(int num){
-    //     if(num<2){
-    //         return false;
-    //     }
-    //     for(int i=2;i<=Math.sqrt(num);i++){
-    //         if(num%i==0){
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the start number:");
@@ -31,21 +20,10 @@ public class prime {
         System.out.println("Enter the end number:");
         int end=sc.nextInt();
         for(int i=start;i<end;i++){
-            if(!isPrime(i)){
+            if(isPrime(i)){
                 System.out.print(i+" ");
             }
         }
         sc.close();
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter start number:");
-        // int start=sc.nextInt();
-        // System.out.println("Enter end number:");
-        // int end = sc.nextInt();
-        // for(int i=start;i<end;i++){
-        //     while(!isPrime(i)){
-        //         System.out.print(i+" ");
-        //     }
-        // }
-        // sc.close();
     }
 }
