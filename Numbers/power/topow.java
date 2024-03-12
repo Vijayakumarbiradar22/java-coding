9dcfd2d889f07b7ac1ff0b2890a77fea3ae4287b
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-12 19:08:33 +0530

    modified code

--- Numbers/power/topow.java
+++ Numbers/power/topow.java
@@ -1,32 +1,38 @@
 package Numbers.power;
 import java.util.Scanner;
 public class topow {
-    public static int findPower(int x, int n){
-        if(n==0){
-            return 1;
-        }
 
-        if(x==0){
-            return 0;
-        }
+    // public static int findPower(int x, int n){
+    //     if(n==0){
+    //         return 1;
+    //     }
 
-        if(n%2==0){
-            return findPower(x,n/2) * findPower(x,n/2);
-        }
-        else {
-            return findPower(x,n/2) * findPower(x,n/2) *x;
-        }
+    //     if(x==0){
+    //         return 0;
+    //     }
+
+    //     if(n%2==0){
+    //         return findPower(x,n/2) * findPower(x,n/2);
+    //     }
+    //     else {
+    //         return findPower(x,n/2) * findPower(x,n/2) *x;
+    //     }
         
 
-    }
+    // }
     public static void main(String args[]){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter x:");
-            int x = sc.nextInt();
-            System.out.println("Enter n:");
-            int n = sc.nextInt();
-            int result = findPower(x,n);
-            System.out.println(result);
-        sc.close();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the x:");
+        int x=sc.nextInt();
+
+        
+        //     Scanner sc = new Scanner(System.in);
+        //     System.out.println("Enter x:");
+        //     int x = sc.nextInt();
+        //     System.out.println("Enter n:");
+        //     int n = sc.nextInt();
+        //     int result = findPower(x,n);
+        //     System.out.println(result);
+        // sc.close();
     }
 }
