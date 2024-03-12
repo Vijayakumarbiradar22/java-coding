bb250dc1610bb06ca0d71a3b68dc09c005adc0e7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-12 19:10:04 +0530

    modified code

--- Numbers/power/topow.java
+++ Numbers/power/topow.java
@@ -1,6 +1,9 @@
 package Numbers.power;
 import java.util.Scanner;
 public class topow {
+    public static int findPower(int x, int n){
+        
+    }
 
     // public static int findPower(int x, int n){
     //     if(n==0){
@@ -24,7 +27,10 @@ public class topow {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the x:");
         int x=sc.nextInt();
-        
+        System.out.println("Enter the n:");
+        int n=sc.nextInt();
+        int result = findPower(x,n);
+        System.out.println(result);
 
         sc.close();
         //     Scanner sc = new Scanner(System.in);
