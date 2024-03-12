9157b48527ab3d1a3637be7508580449b38ec4de
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-12 19:29:19 +0530

    modified code

--- Numbers/reversenumber/rev.java
+++ Numbers/reversenumber/rev.java
@@ -5,6 +5,19 @@ public class rev {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number:");
         int num=sc.nextInt();
+        int rem,revNum=0;
+        int temp=num;
+        while(num!=0){
+            rem=num/10;
+            revNum=revNum*10+rem;
+            num=num/10;
+        }
+        System.out.println(revNum);
+        if(revNum==temp){
+            System.out.println(temp+" is palindrome number");
+        } else {
+            System.out.println(temp+" is not palindrome number");
+        }
         // System.out.println("Enter the number:");
         // int num=sc.nextInt();
         // int rem,revNum=0;
