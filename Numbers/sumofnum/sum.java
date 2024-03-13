8d0d5d1f3a9ef7a75dff3ad51b333661d7c62431
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-13 19:42:27 +0530

    added new code

--- Numbers/sumofnum/sum.java
+++ Numbers/sumofnum/sum.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class sum {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=sc.nextInt();
         
 
         sc.close();
