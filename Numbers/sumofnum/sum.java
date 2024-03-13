9bdb3dc150f77680bef66a59a6c0c5ced576d21a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-13 19:40:07 +0530

    added new code

--- Numbers/sumofnum/sum.java
+++ Numbers/sumofnum/sum.java
@@ -2,17 +2,19 @@ package Numbers.sumofnum;
 import java.util.Scanner;
 public class sum {
     public static void main(String args[]){
-        int rem,sum=0;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the number:");
-            int num=sc.nextInt();
-            while(num>0){
-                rem = num%10;
-                sum=sum+rem;
-                num=num/10;
-            }
-        System.out.println(sum);
-            sc.close();
+        Scanner sc = new Scanner(System.in);
+        
+        // int rem,sum=0;
+        //     Scanner sc = new Scanner(System.in);
+        //     System.out.println("Enter the number:");
+        //     int num=sc.nextInt();
+        //     while(num>0){
+        //         rem = num%10;
+        //         sum=sum+rem;
+        //         num=num/10;
+        //     }
+        // System.out.println(sum);
+        //     sc.close();
     }
     
 }
