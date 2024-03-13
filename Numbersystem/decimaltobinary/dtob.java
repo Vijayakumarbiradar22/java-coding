2baf1a83dff42829364aa9ec5d9d61933852f60a
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-13 19:46:16 +0530

    added new code

--- Numbersystem/decimaltobinary/dtob.java
+++ Numbersystem/decimaltobinary/dtob.java
@@ -12,7 +12,7 @@ public class dtob {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number:");
+        System.out.println("Enter the decimal number:");
         int num =  sc.nextInt();
         String binary = convertBinary(num);
         System.out.println(binary);
