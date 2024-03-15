a3ed1b783c86b0f52ea76a71d477be2b2638843e
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-15 20:02:15 +0530

    modified code

--- Patterns/Stars/starpant/pant.java
+++ Patterns/Stars/starpant/pant.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class pant {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
+        System.out.println("Enter the no.of rows:");
         int n=sc.nextInt();
         int x=n;
         int y=n;
