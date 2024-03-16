2f78e2681c445525b32f057b7b305a3ac01c7fa6
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-16 19:55:07 +0530

    modified code

--- Recursion/increasenum/numin.java
+++ Recursion/increasenum/numin.java
@@ -1,21 +1,20 @@
 package Recursion.increasenum;
 import java.util.Scanner;
 public class numin {
-    public static void printNum(int n){
+    public static void increaseNum(int n){
         if(n==5){
             System.out.println(n);
             return;
         }
 
         System.out.println(n);
-        printNum(n+1);
+        increaseNum(n+1);
     }
     public static void main(String args[]){
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter the number:");
             int n = sc.nextInt();
-            //int n=1;
-            printNum(n);
+            increaseNum(n);
             sc.close();
     }
     
