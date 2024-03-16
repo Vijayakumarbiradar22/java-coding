ebd367341db133e6099a56e7044333e0f1a9bda4
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-16 19:58:05 +0530

    modified code

--- Recursion/stringreverse/revstr.java
+++ Recursion/stringreverse/revstr.java
@@ -12,7 +12,7 @@ public class revstr {
     }
     public static void main(String args[]){
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the string to reverse:");
+            System.out.println("Enter the string:");
             String str = sc.nextLine();
             printRev(str,str.length()-1);
         }
