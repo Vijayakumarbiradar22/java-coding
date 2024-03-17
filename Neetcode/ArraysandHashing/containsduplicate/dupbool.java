6702947f4b87c4291167187bae989d33d8151251
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-17 21:38:17 +0530

    added new code

--- Neetcode/ArraysandHashing/containsduplicate/dupbool.java
+++ Neetcode/ArraysandHashing/containsduplicate/dupbool.java
@@ -13,7 +13,7 @@ public class dupbool {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the array size:");
+        System.out.println("Enter the size of array:");
         int n=sc.nextInt();
         int nums[]= new int[n];
         System.out.println("Enter the elements of array:");
