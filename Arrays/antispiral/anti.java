be2b56fed7214363f3d3cce82841f7fb41ad6745
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-19 20:34:02 +0530

    modified code

--- Arrays/antispiral/anti.java
+++ Arrays/antispiral/anti.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class anti {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-           System.out.println("Enter the no.of rows:");
+           System.out.println("Enter the number of rows:");
            int r = sc.nextInt();
             System.out.println("Enter the no.of cols:");
             int c = sc.nextInt();
