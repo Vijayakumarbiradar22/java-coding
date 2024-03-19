740d22d3eccf1607ff8192074f867530dcccd484
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-19 20:32:22 +0530

    modified code

--- Arrays/addition/add.java
+++ Arrays/addition/add.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class add {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
+        System.out.println("Enter number of rows:");
         int r =sc.nextInt();
         System.out.println("Enter the number of cols:");
         int c = sc.nextInt();
