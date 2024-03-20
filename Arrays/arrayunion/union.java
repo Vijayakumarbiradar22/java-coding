753fa5c83250998eab1e00560b903702d76c8694
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-20 20:35:58 +0530

    modified code

--- Arrays/arrayunion/union.java
+++ Arrays/arrayunion/union.java
@@ -26,7 +26,7 @@ public class union {
         System.out.println("Enter size of array1:");
         int size1=sc.nextInt();
         int arr1[]=new int[size1];
-        System.out.println("Enter the elements of array1:");
+        System.out.println("Enter elements of array1:");
         for(int i=0;i<size1;i++){
             arr1[i]=sc.nextInt();
         }
