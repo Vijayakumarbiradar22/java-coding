cacb2ff436ccd3e97b3672d537e94de00a169658
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-21 22:00:13 +0530

    added new code

--- HashMap/operation/map.java
+++ HashMap/operation/map.java
@@ -10,23 +10,19 @@ public class map {
         System.out.println(map);
         map.put("China",180);
         System.out.println(map);
-
         //search
         if(map.containsKey("China")){
             System.out.println("Key Present in the map");
         } else {
             System.out.println(" Key is not present in the map");
         }
-
         System.out.println(map.get("China"));
         System.out.println(map.get("Srilanka"));
-
         //entryset through
         for(Map.Entry<String,Integer>e:map.entrySet()){
             System.out.println(e.getKey());
             System.out.println(e.getValue());
         }
-
         //keyset through
         Set<String> keys = map.keySet();
         for(String key:keys){
