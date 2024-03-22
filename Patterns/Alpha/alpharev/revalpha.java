dae500e17505b5230907486e1e32a95e7d811089
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-22 07:42:44 +0530

    modified code

--- Patterns/Alpha/alpharev/revalpha.java
+++ Patterns/Alpha/alpharev/revalpha.java
@@ -5,10 +5,13 @@ public class revalpha {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter no.of rows:");
         int n=sc.nextInt();
-        int alpha=64;
+        char alpha=65;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=2*n-1;j++){
+                System.out.print(alpha);
+                alpha++;
             }
+            
         }
         sc.close();
     }
