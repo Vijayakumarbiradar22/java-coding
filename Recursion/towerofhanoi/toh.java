b4d28f3c36076aecaac30daa8d293a4e6563fc5c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-23 22:07:30 +0530

    modified code

--- Recursion/towerofhanoi/toh.java
+++ Recursion/towerofhanoi/toh.java
@@ -14,7 +14,7 @@ public class toh {
     }
     public static void main(String args[]){
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the number of discs:");
+            System.out.println("Enter the total number of discs:");
             int n= sc.nextInt();
             towerofhanoi(n,"S","H","D");
             
