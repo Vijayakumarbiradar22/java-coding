c940801a175b91eab8362f20afca4728d2f59f61
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-23 22:05:01 +0530

    modified code

--- Recursion/stackheight/sh.java
+++ Recursion/stackheight/sh.java
@@ -17,7 +17,7 @@ public class sh {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the x:");
+        System.out.println("Enter the value x:");
         int x= sc.nextInt();
         System.out.println("Enter the n:");
         int n = sc.nextInt();
