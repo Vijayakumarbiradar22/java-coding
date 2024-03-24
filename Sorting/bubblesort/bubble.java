7dd9d8587407603c9623481cb326de73d0e2e306
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-24 10:03:27 +0530

    modified code

--- Sorting/bubblesort/bubble.java
+++ Sorting/bubblesort/bubble.java
@@ -16,7 +16,8 @@ public class bubble {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array:");
-        
+        int size  = sc.nextInt();
+        int arr[]=new int[size];
 
         sc.close();
         //     Scanner sc = new Scanner(System.in);
