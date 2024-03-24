74f9ed42e0bd4ae077dc9e5703f192db446131cb
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-24 10:02:47 +0530

    modified code

--- Sorting/bubblesort/bubble.java
+++ Sorting/bubblesort/bubble.java
@@ -1,32 +1,37 @@
 package Sorting.bubblesort;
 import java.util.Scanner;
 public class bubble {
-    public static void bubbleSort(int array[], int size){
-        int temp;
-        for(int i=0;i<size;i++){
-            for(int j=i+1;j<size;j++){
-                if(array[i]>array[j]){
-                    temp=array[i];
-                    array[i]=array[j];
-                    array[j]=temp;
-                }
-            }
-        }
-    }
+    // public static void bubbleSort(int array[], int size){
+    //     int temp;
+    //     for(int i=0;i<size;i++){
+    //         for(int j=i+1;j<size;j++){
+    //             if(array[i]>array[j]){
+    //                 temp=array[i];
+    //                 array[i]=array[j];
+    //                 array[j]=temp;
+    //             }
+    //         }
+    //     }
+    // }
     public static void main(String args[]){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the size of array:");
-            int size=sc.nextInt();
-            int[] array = new int[size];
-            System.out.println("Enter the elements of array:");
-            for(int i=0;i<size;i++){
-                array[i]=sc.nextInt();
-            }
-            bubbleSort(array,size);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        
 
-            for(int i=0;i<size;i++){
-                System.out.print(array[i]+" ");
-            }
         sc.close();
+        //     Scanner sc = new Scanner(System.in);
+        //     System.out.println("Enter the size of array:");
+        //     int size=sc.nextInt();
+        //     int[] array = new int[size];
+        //     System.out.println("Enter the elements of array:");
+        //     for(int i=0;i<size;i++){
+        //         array[i]=sc.nextInt();
+        //     }
+        //     bubbleSort(array,size);
+
+        //     for(int i=0;i<size;i++){
+        //         System.out.print(array[i]+" ");
+        //     }
+        // sc.close();
     }
 }
