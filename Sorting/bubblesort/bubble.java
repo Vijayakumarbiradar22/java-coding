38836301bf8bea4e5b219fd745a887e476cad0e3
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-24 10:05:46 +0530

    modified code

--- Sorting/bubblesort/bubble.java
+++ Sorting/bubblesort/bubble.java
@@ -22,9 +22,11 @@ public class bubble {
         for(int i=0;i<size;i++){
             arr[i]=sc.nextInt();
         }
+        bubbleSort(arr,size);
 
-        
-
+        for(int i=0;i<size;i++){
+            System.out.print(arr[i]+" ");
+        }
         sc.close();
         //     Scanner sc = new Scanner(System.in);
         //     System.out.println("Enter the size of array:");
