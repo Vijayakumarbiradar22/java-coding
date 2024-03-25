83a75b9e927d46756379c1103dd6de11911f6086
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-25 22:50:35 +0530

    modified code

--- Strings/reversebyword/revbyword.java
+++ Strings/reversebyword/revbyword.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class revbyword {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the string:");
+            System.out.println("Enter the string to reverse by word:");
             String str=sc.nextLine();
             String ns[]=str.split(" ");
             String nstr="";
