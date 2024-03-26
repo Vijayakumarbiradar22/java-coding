83ba475d21c528044e64581d61814cb023d680e3
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-26 16:22:07 +0530

    modified code

--- Patterns/Alpha/alpharev/revalpha.java
+++ Patterns/Alpha/alpharev/revalpha.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class revalpha {
     public static void main(String args[]){
        Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the number of rows:");
+       int rows=sc.nextInt();
        
 
 
