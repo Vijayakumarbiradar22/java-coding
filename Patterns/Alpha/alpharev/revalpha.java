23a77706a6f22bb7959b90ea30855870d98f2f79
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-26 16:25:32 +0530

    modified code

--- Patterns/Alpha/alpharev/revalpha.java
+++ Patterns/Alpha/alpharev/revalpha.java
@@ -9,11 +9,13 @@ public class revalpha {
        for(int i=0;i<rows;i++){
         for(int j=0;j<2*rows-1;j++){
             System.out.print(ch);
-            
+            if(j<rows-1){
+                ch++;
+            } else {
+                ch--;
+            }
         }
        }
-
-
        sc.close();
     }
 }
