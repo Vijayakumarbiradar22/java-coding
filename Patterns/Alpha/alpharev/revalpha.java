6c83d560ec2f8ca39b0d3f34c2e7df84474701b7
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-26 16:21:02 +0530

    modified code

--- Patterns/Alpha/alpharev/revalpha.java
+++ Patterns/Alpha/alpharev/revalpha.java
@@ -2,21 +2,10 @@ package Patterns.Alpha.alpharev;
 import java.util.Scanner;
 public class revalpha {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter no.of rows:");
-        int n=sc.nextInt();
-        char alpha=65;
-        for(int i=1;i<=n;i++){
-            for(char j=alpha;j<='E';j++){
-                System.out.print(j);
-            }
+       Scanner sc = new Scanner(System.in);
+       
 
-            for(char j='D';j>='A';j--){
-                System.out.print(j);
-            }
-            
-            System.out.println();
-        }
-        sc.close();
+
+       sc.close();
     }
 }
