36e4701485466d6f7264e2f3b098d9459cb1d8c9
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-27 20:51:40 +0530

    modified code

--- Searchalgo/binarysearch/binary.java
+++ Searchalgo/binarysearch/binary.java
@@ -24,7 +24,7 @@ public class binary {
         System.out.println("Enter the size of array:");
         int n=sc.nextInt();
         int nums[]=new int[n];
-        System.out.println("Enter the elements:");
+        System.out.println("Enter the elements of array:");
         for(int i=0;i<n;i++){
             nums[i]=sc.nextInt();
         }
