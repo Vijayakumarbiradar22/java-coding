7a9adb08ac9a02029e996084e7b4c05ad384513d
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-28 21:59:26 +0530

    modified code

--- Stack/reverse/rev.java
+++ Stack/reverse/rev.java
@@ -20,10 +20,10 @@ public class rev {
     }
     public static void main(String args[]){
         Stack<Integer> s = new Stack<>();
-        s.push(1);
         s.push(2);
-        s.push(3);
         s.push(4);
+        s.push(6);
+        s.push(8);
         reverse(s);
         while(!s.isEmpty()){
             System.out.println(s.peek());
