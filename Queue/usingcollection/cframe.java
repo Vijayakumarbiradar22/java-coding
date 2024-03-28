240dd7bd8566fff7fb9c9bd8602884aa58fa370f
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-28 22:07:46 +0530

    modified code

--- Queue/usingcollection/cframe.java
+++ Queue/usingcollection/cframe.java
@@ -7,11 +7,11 @@ public class cframe {
         q.add(2);
         q.add(3);
         q.add(4);
+        q.add(5);
 
         while(!q.isEmpty()){
             System.out.println(q.peek());
             q.remove();
         }
     }
-    
 }
