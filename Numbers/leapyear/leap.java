02495a0bee642631f571d653650bbcf26a1e7f82
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-31 23:33:03 +0530

    modified code

--- Numbers/leapyear/leap.java
+++ Numbers/leapyear/leap.java
@@ -8,7 +8,7 @@ public class leap {
             if(year%4==0 && year%100!=0 || year%400==0){
                 System.out.println("Entered year is Leap year");
             } else {
-                System.out.println("Not leap year.");
+                System.out.println("Entered year is not leap year.");
             }
             sc.close();
         }
