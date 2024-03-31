69642af79f73ebcfb44edb89e30cdf35035447d2
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-31 23:34:21 +0530

    modified code

--- Arrays/rightrotate/rrotate.java
+++ Arrays/rightrotate/rrotate.java
@@ -17,7 +17,7 @@ public class rrotate {
         System.out.println("Enter the array size:");
         int n=sc.nextInt();
         int[] arr = new int[n];
-        System.out.println("Enter the elements:");
+        System.out.println("Enter the array elements:");
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
