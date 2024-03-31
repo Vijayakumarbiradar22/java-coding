eece44f17a86e11ab9e178e39fb489c342769d6c
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-03-31 23:37:06 +0530

    modified code

--- Arrays/oddoccurance/oddoc.java
+++ Arrays/oddoccurance/oddoc.java
@@ -21,7 +21,7 @@ public class oddoc {
         System.out.println("Enter the size of array:");
         int n=sc.nextInt();
         int[] arr = new int[n];
-        System.out.println("Enter the elements:");
+        System.out.println("Enter the elements of array:");
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
