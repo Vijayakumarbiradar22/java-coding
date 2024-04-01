d739567a328a09120739010435b830d18c3ccb98
Vijayakumarbiradar22 <vijaya.20cs104@sode-edu.in>
2024-04-01 17:53:47 +0530

    modified code

--- Arrays/countinversion/inversion.java
+++ Arrays/countinversion/inversion.java
@@ -14,7 +14,7 @@ public class inversion {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size of array:");
+        System.out.println("Enter the size of array:");
         int size=sc.nextInt();
         int arr[]=new int[size];
         System.out.println("Enter elements of array:");
